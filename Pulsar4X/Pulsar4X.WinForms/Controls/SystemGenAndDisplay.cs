6070ea1f36214ccc013cdc779581655bbd7b0147
Charl van Deventer <charlvdeventer@gmail.com>
2012-08-28 15:36:40 +0200

    A very basic viewmodel for F9

--- Pulsar4X/Pulsar4X.WinForms/Controls/SystemGenAndDisplay.cs
+++ Pulsar4X/Pulsar4X.WinForms/Controls/SystemGenAndDisplay.cs
@@ -6,14 +6,28 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Pulsar4X.ViewModels;
 
 namespace Pulsar4X.WinForms.Controls
 {
     public partial class SystemGenAndDisplay : UserControl
     {
+        public StarSystemViewModel VM { get; set; }
         public SystemGenAndDisplay()
         {
             InitializeComponent();
+
+            VM = new StarSystemViewModel();
+
+            NameListBox.DataSource = new BindingSource(VM.StarSystems, null);
+            NameListBox.DataBindings.Add(new Binding("SelectedItem", VM.CurrentStarSystem, null));
+            NameListBox.DisplayMember = "Name";
+
+            AgetextBox.DataBindings.Add(new Binding("Text", VM.CurrentStarSystem.Stars[0].Age, null));
+
+            StarsDataGridView.DataSource = VM.CurrentStarSystem.Stars;
+
+            StarADataGridView.DataSource = new BindingSource(VM.CurrentStar.Planets, null);
         }
 
     }
---
+++ Pulsar4X/Pulsar4X.WinForms/ViewModels/StarSystemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pulsar4X.Stargen;
+using Pulsar4X.Entities;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Pulsar4X.ViewModels
+{
+    public class StarSystemViewModel : INotifyPropertyChanged
+    {
+        public StarSystem CurrentStarSystem { get; set; }
+        public ObservableCollection<StarSystem> StarSystems { get; set; }
+
+        public Star CurrentStar { get; set; }
+        public Planet CurrentPlanet { get; set; }
+
+        public StarSystemViewModel()
+        {
+            // Just gen a Starsystem
+            var ssf = new StarSystemFactory();
+            var ss = ssf.Create("Test");
+            StarSystems = new ObservableCollection<StarSystem>();
+            StarSystems.Add(ss);
+            CurrentStarSystem = ss;
+            CurrentStar = CurrentStarSystem.Stars.First();
+            CurrentPlanet = CurrentStar.Planets.First();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
