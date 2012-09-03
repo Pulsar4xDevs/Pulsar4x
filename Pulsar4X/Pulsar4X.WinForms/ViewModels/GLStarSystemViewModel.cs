c2d9b2dd7b18f57af21926ae8926ea334b0196dc
Charl van Deventer <charlvdeventer@gmail.com>
2012-09-03 12:56:39 +0200

    Added a basic VM for GL

---
+++ Pulsar4X/Pulsar4X.WinForms/ViewModels/GLStarSystemViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pulsar4X.Entities;
+using System.ComponentModel;
+using Pulsar4X.Stargen;
+using System.Linq.Expressions;
+
+namespace Pulsar4X.ViewModels
+{
+    public class GLStarSystemViewModel: INotifyPropertyChanged
+    {
+
+        public BindingList<StarSystem> StarSystems { get; set; }
+        private StarSystem _currentstarsystem;
+        public StarSystem CurrentStarSystem
+        {
+            get
+            {
+                return _currentstarsystem;
+            }
+            set
+            {
+                if (_currentstarsystem != value)
+                {
+                    _currentstarsystem = value;
+                    OnPropertyChanged(() => CurrentStarSystem);
+                }
+            }
+        }
+
+        public GLStarSystemViewModel()
+        {
+            // For now, just create a new starsystem
+            var ssf = new StarSystemFactory(false);
+            CurrentStarSystem = ssf.Create("FooBar");
+        }
+
+
+        private void OnPropertyChanged(Expression<Func<object>> property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(BindingHelper.Name(property)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
