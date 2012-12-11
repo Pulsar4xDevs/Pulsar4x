4aa36e61a2000dd37e42031d7747cf4841312d7b
Greg Nott <gvnott@gmail.com>
2012-12-11 23:39:55 +1100

    Added aditional error checking to Economics screen

--- Pulsar4X/Pulsar4X.UI/Handlers/Economics.cs
+++ Pulsar4X/Pulsar4X.UI/Handlers/Economics.cs
@@ -6,11 +6,16 @@ using System.Windows.Forms;
 using WeifenLuo.WinFormsUI.Docking;
 using Pulsar4X.UI.ViewModels;
 using Pulsar4X.Entities;
+using log4net.Config;
+using log4net;
 
 namespace Pulsar4X.UI.Handlers
 {
     public class Economics
     {
+        // System Map Logger:
+        public static readonly ILog logger = LogManager.GetLogger(typeof(Economics));
+
         /// <summary>
         /// Panel that contains the list of populated systems.
         /// </summary>
@@ -101,93 +106,113 @@ namespace Pulsar4X.UI.Handlers
 
         private void SetupSummaryDataGrid()
         {
-            // Add coloums:
-            Padding newPadding = new Padding(2, 0, 2, 0);
-            using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
+            try
             {
-                col.HeaderText = "Item";
-                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-                col.DefaultCellStyle.Padding = newPadding;
-                m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
-            }
-            using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
-            {
-                col.HeaderText = "Amount";
-                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-                col.DefaultCellStyle.Padding = newPadding;
-                m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
-            }
-            using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
-            {
-                col.HeaderText = "Installation";
-                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-                col.DefaultCellStyle.Padding = newPadding;
-                m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
-            }
-            using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
-            {
-                col.HeaderText = "Number or Level";
-                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-                col.DefaultCellStyle.Padding = newPadding;
-                m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
-            }
 
-            // Add Rows:
-            for (int i = 0; i < 33; ++i)
-            {
-                using (DataGridViewRow row = new DataGridViewRow())
+                // Add coloums:
+                Padding newPadding = new Padding(2, 0, 2, 0);
+                using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
                 {
-                    // setup row height. note that by default they are 22 pixels in height!
-                    row.Height = 18;
-                    m_oSummaryPanel.SummaryDataGrid.Rows.Add(row);
+                    col.HeaderText = "Item";
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                    col.DefaultCellStyle.Padding = newPadding;
+                    if (col != null)
+                    {
+                        m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
+                    }
                 }
+                using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
+                {
+                    col.HeaderText = "Amount";
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    col.DefaultCellStyle.Padding = newPadding;
+                    if (col != null)
+                    {
+                        m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
+                    }
+                }
+                using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
+                {
+                    col.HeaderText = "Installation";
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                    col.DefaultCellStyle.Padding = newPadding;
+                    if (col != null)
+                    {
+                        m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
+                    }
+                }
+                using (DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn())
+                {
+                    col.HeaderText = "Number or Level";
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                    col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    col.DefaultCellStyle.Padding = newPadding;
+                    if (col != null)
+                    {
+                        m_oSummaryPanel.SummaryDataGrid.Columns.Add(col);
+                    }
+                }
+
+                // Add Rows:
+                for (int i = 0; i < 33; ++i)
+                {
+                    using (DataGridViewRow row = new DataGridViewRow())
+                    {
+                        // setup row height. note that by default they are 22 pixels in height!
+                        row.Height = 18;
+                        m_oSummaryPanel.SummaryDataGrid.Rows.Add(row);
+                    }
+                }
+
+                // Setup item Colomn:
+                m_oSummaryPanel.SummaryDataGrid.Rows[1].Cells[0].Value = "Species";
+                m_oSummaryPanel.SummaryDataGrid.Rows[7].Cells[0].Value = "Population";
+                m_oSummaryPanel.SummaryDataGrid.Rows[8].Cells[0].Value = "   Agriculture and Enviromental (5.0%)";
+                m_oSummaryPanel.SummaryDataGrid.Rows[9].Cells[0].Value = "   Service Industries (75.0%)";
+                m_oSummaryPanel.SummaryDataGrid.Rows[10].Cells[0].Value = "   Manufacturing (20.0%)";
+                m_oSummaryPanel.SummaryDataGrid.Rows[11].Cells[0].Value = "Anual Growth Rate";
+
+                m_oSummaryPanel.SummaryDataGrid.Rows[14].Cells[0].Value = "Current Infrastructure";
+
+                m_oSummaryPanel.SummaryDataGrid.Rows[31].Cells[0].Value = "Tectonics";
+
+                // Setup Installation Colomn
+                m_oSummaryPanel.SummaryDataGrid.Rows[0].Cells[2].Value = "Military Academy";
+                m_oSummaryPanel.SummaryDataGrid.Rows[1].Cells[2].Value = "Deep Space Tracking Station";
+                m_oSummaryPanel.SummaryDataGrid.Rows[2].Cells[2].Value = "Maintenance Facility Maximum Ship Size";
+
+                m_oSummaryPanel.SummaryDataGrid.Rows[4].Cells[2].Value = "Shipyards / Slipways";
+                m_oSummaryPanel.SummaryDataGrid.Rows[5].Cells[2].Value = "Maintenance Facilities";
+                m_oSummaryPanel.SummaryDataGrid.Rows[6].Cells[2].Value = "Construction Factories";
+                m_oSummaryPanel.SummaryDataGrid.Rows[7].Cells[2].Value = "Ordnance Factories";
+                m_oSummaryPanel.SummaryDataGrid.Rows[8].Cells[2].Value = "Fighter Factories";
+                m_oSummaryPanel.SummaryDataGrid.Rows[9].Cells[2].Value = "Fuel Refineries";
+                m_oSummaryPanel.SummaryDataGrid.Rows[10].Cells[2].Value = "Mines";
+                m_oSummaryPanel.SummaryDataGrid.Rows[11].Cells[2].Value = "Automated Mines";
+                m_oSummaryPanel.SummaryDataGrid.Rows[12].Cells[2].Value = "Research Labs";
+                m_oSummaryPanel.SummaryDataGrid.Rows[13].Cells[2].Value = "Ground Force Training Facilities";
+                m_oSummaryPanel.SummaryDataGrid.Rows[14].Cells[2].Value = "Financial Centre";
+                m_oSummaryPanel.SummaryDataGrid.Rows[15].Cells[2].Value = "Mass Driver";
+                m_oSummaryPanel.SummaryDataGrid.Rows[16].Cells[2].Value = "Sector Command";
+                m_oSummaryPanel.SummaryDataGrid.Rows[17].Cells[2].Value = "Spaceport";
+                m_oSummaryPanel.SummaryDataGrid.Rows[18].Cells[2].Value = "Terraforming Installation";
+
+                m_oSummaryPanel.SummaryDataGrid.Rows[20].Cells[2].Value = "Fuel Reserves";
+                m_oSummaryPanel.SummaryDataGrid.Rows[21].Cells[2].Value = "Maintenance Supplies";
+                m_oSummaryPanel.SummaryDataGrid.Rows[23].Cells[2].Value = "EM Signature of Colony";
+                m_oSummaryPanel.SummaryDataGrid.Rows[25].Cells[2].Value = "Economic Production Modifier";
+                m_oSummaryPanel.SummaryDataGrid.Rows[26].Cells[2].Value = "Manufacturing Efficiency Modifier";
+                m_oSummaryPanel.SummaryDataGrid.Rows[27].Cells[2].Value = "Political Status Production Modifier";
+                m_oSummaryPanel.SummaryDataGrid.Rows[28].Cells[2].Value = "Political Status Wealth/Trade Modifier";
+                m_oSummaryPanel.SummaryDataGrid.Rows[29].Cells[2].Value = "Political Status Modifier";
             }
-
-            // Setup item Colomn:
-            m_oSummaryPanel.SummaryDataGrid.Rows[1].Cells[0].Value = "Species";
-            m_oSummaryPanel.SummaryDataGrid.Rows[7].Cells[0].Value = "Population";
-            m_oSummaryPanel.SummaryDataGrid.Rows[8].Cells[0].Value = "   Agriculture and Enviromental (5.0%)";
-            m_oSummaryPanel.SummaryDataGrid.Rows[9].Cells[0].Value = "   Service Industries (75.0%)";
-            m_oSummaryPanel.SummaryDataGrid.Rows[10].Cells[0].Value = "   Manufacturing (20.0%)";
-            m_oSummaryPanel.SummaryDataGrid.Rows[11].Cells[0].Value = "Anual Growth Rate";
-
-            m_oSummaryPanel.SummaryDataGrid.Rows[14].Cells[0].Value = "Current Infrastructure";
-
-            m_oSummaryPanel.SummaryDataGrid.Rows[31].Cells[0].Value = "Tectonics";
-
-            // Setup Installation Colomn
-            m_oSummaryPanel.SummaryDataGrid.Rows[0].Cells[2].Value = "Military Academy";
-            m_oSummaryPanel.SummaryDataGrid.Rows[1].Cells[2].Value = "Deep Space Tracking Station";
-            m_oSummaryPanel.SummaryDataGrid.Rows[2].Cells[2].Value = "Maintenance Facility Maximum Ship Size";
-
-            m_oSummaryPanel.SummaryDataGrid.Rows[4].Cells[2].Value = "Shipyards / Slipways";
-            m_oSummaryPanel.SummaryDataGrid.Rows[5].Cells[2].Value = "Maintenance Facilities";
-            m_oSummaryPanel.SummaryDataGrid.Rows[6].Cells[2].Value = "Construction Factories";
-            m_oSummaryPanel.SummaryDataGrid.Rows[7].Cells[2].Value = "Ordnance Factories";
-            m_oSummaryPanel.SummaryDataGrid.Rows[8].Cells[2].Value = "Fighter Factories";
-            m_oSummaryPanel.SummaryDataGrid.Rows[9].Cells[2].Value = "Fuel Refineries";
-            m_oSummaryPanel.SummaryDataGrid.Rows[10].Cells[2].Value = "Mines";
-            m_oSummaryPanel.SummaryDataGrid.Rows[11].Cells[2].Value = "Automated Mines";
-            m_oSummaryPanel.SummaryDataGrid.Rows[12].Cells[2].Value = "Research Labs";
-            m_oSummaryPanel.SummaryDataGrid.Rows[13].Cells[2].Value = "Ground Force Training Facilities";
-            m_oSummaryPanel.SummaryDataGrid.Rows[14].Cells[2].Value = "Financial Centre";
-            m_oSummaryPanel.SummaryDataGrid.Rows[15].Cells[2].Value = "Mass Driver";
-            m_oSummaryPanel.SummaryDataGrid.Rows[16].Cells[2].Value = "Sector Command";
-            m_oSummaryPanel.SummaryDataGrid.Rows[17].Cells[2].Value = "Spaceport";
-            m_oSummaryPanel.SummaryDataGrid.Rows[18].Cells[2].Value = "Terraforming Installation";
-
-            m_oSummaryPanel.SummaryDataGrid.Rows[20].Cells[2].Value = "Fuel Reserves";
-            m_oSummaryPanel.SummaryDataGrid.Rows[21].Cells[2].Value = "Maintenance Supplies";
-            m_oSummaryPanel.SummaryDataGrid.Rows[23].Cells[2].Value = "EM Signature of Colony";
-            m_oSummaryPanel.SummaryDataGrid.Rows[25].Cells[2].Value = "Economic Production Modifier";
-            m_oSummaryPanel.SummaryDataGrid.Rows[26].Cells[2].Value = "Manufacturing Efficiency Modifier";
-            m_oSummaryPanel.SummaryDataGrid.Rows[27].Cells[2].Value = "Political Status Production Modifier";
-            m_oSummaryPanel.SummaryDataGrid.Rows[28].Cells[2].Value = "Political Status Wealth/Trade Modifier";
-            m_oSummaryPanel.SummaryDataGrid.Rows[29].Cells[2].Value = "Political Status Modifier";
+            catch
+            {
+                logger.Error("Something whent wrong Creating Colums for Economics summary screen...");
+            }
         }
 
         public void RefreshSummaryCells()
