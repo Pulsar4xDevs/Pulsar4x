69d3d7fcdc228e10377b29f9d52a48167a16a5fd
PsiDog <starsail3b@yahoo.com>
2012-12-14 20:38:33 -0500

    Fixed empty BasicNewtonian class.

--- Pulsar4X/Pulsar4X.Lib/Entities/Components/BasicNewtonian.cs
+++ Pulsar4X/Pulsar4X.Lib/Entities/Components/BasicNewtonian.cs
@@ -1,12 +1,62 @@
 using System;
+using Pulsar4X.Entities;
+using Pulsar4X.Entities.Components;
+using log4net;
 
-namespace Pulsar4X
+
+namespace Pulsar4X.Entities.Components
 {
-	public class BasicNewtonian
+	public enum NewtonianType
 	{
+		Engine,
+		Weapon,
+		BeamFireCon,
+		MissileFireCon,
+		Sensor,
+		Shield,
+		Fuel,
+		Engineering,
+		Other,
+		TypeCount
+	};
+
+	/// <summary>
+	/// A class with all methods and properties that all internal newtonian ship components will share
+	/// </summary>
+	public abstract class BasicNewtonian
+	{
+		public string name {get; set;}
+
+		public BuildCost unitCost;
+
+		/// <summary>
+		/// Displacement in tons (meter cubed) for one component.
+		/// Typicaly equals 10x mass for storage compartments or 5x mass otherwise
+		/// </summary>
+		public long volumeUnit { get; set; }
+
+		public long unitCrew { get; set; }
+
+		/// <summary>
+		/// Mass in tons for a single component.
+		/// </summary>
+		public long unitMass { get; set; }
+
+		/// <summary>
+		/// MJ of energy required for a 100% destruction chance.
+		/// </summary>
+		public double integrity { get; set; }
+
+		/// <summary>
+		/// NewtonianType of Component
+		/// </summary>
+		public NewtonianType Type { get; set; }
+
 		public BasicNewtonian ()
 		{
+
 		}
 	}
+
 }
 
