ffccde32e5ebbb19b303744a9a13fd1ef26387b8
PsiDog <starsail3b@yahoo.com>
2012-12-14 20:44:15 -0500

    Fixed empty class upload

--- Pulsar4X/Pulsar4X.Lib/Entities/Components/BuildCost.cs
+++ Pulsar4X/Pulsar4X.Lib/Entities/Components/BuildCost.cs
@@ -1,9 +1,89 @@
 using System;
+using Pulsar4X.Entities;
+using Pulsar4X.Entities.Components;
+using log4net;
 
-namespace Pulsar4X
+
+namespace Pulsar4X.Entities.Components
 {
+	/// <summary>
+	/// The total cost to construct anything is the total mineral cost plus a new
+	/// conventional labor premium for labor intensive tasks like ship refiting or prefab PDC assembly.
+	/// </summary>
 	public class BuildCost
 	{
+
+		/// <summary>
+		/// Gets or sets the duranium cost.
+		/// </summary>
+		public double Duranium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Neutronium cost.
+		/// </summary>
+		public double Neutronium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Corbomite cost.
+		/// </summary>
+		public double Corbomite{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Tritanium cost.
+		/// </summary>
+		public double Tritanium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Boronide cost.
+		/// </summary>
+		public double Boronide{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Mercassium cost.
+		/// </summary>
+		public double Mercassium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Vendarite cost.
+		/// </summary>
+		public double Vendarite{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Sorium cost.
+		/// </summary>
+		public double Sorium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Uridium cost.
+		/// </summary>
+		public double Uridium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Corundium cost.
+		/// </summary>
+		public double Corundium{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the Gallacite cost.
+		/// </summary>
+		public double Gallacite{ get; set; }
+
+		/// <summary>
+		/// Gets or sets any non-mineral credit/time cost.
+		/// </summary>
+		public double ConventionalLabor{ get; set; }
+
+		/// <summary>
+		/// Gets the total build point cost.
+		/// </summary>
+		public double TotalCost {
+			get {
+				return (Duranium + Neutronium + Corbomite + Tritanium + Boronide + Mercassium
+					+ Vendarite + Sorium + Uridium + Corundium + Gallacite + ConventionalLabor); 
+			}
+		}
+
+
 		public BuildCost ()
 		{
 		}
--- Pulsar4X/Pulsar4X.Lib/Entities/NewtonianShip.cs
+++ Pulsar4X/Pulsar4X.Lib/Entities/NewtonianShip.cs
@@ -1,11 +1,76 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Pulsar4X.Entities;
+using Pulsar4X.Entities.Components;
+using log4net;
+using System.ComponentModel;
 
-namespace Pulsar4X
+namespace Pulsar4X.Entities
 {
+	public class NewtonianShipClass
+	{
+		public string className { get; set; }
+
+		public NewtonianEngine shipEngine;
+
+		public BindingList<BasicNewtonian> ComponentList;
+		public List<int> ComponentCount;
+
+		public BindingList<NewtonianShip> ShipsInClass;
+
+		public int NextBuildNumber { get; set; }
+
+
+		public NewtonianShipClass (string Name)
+		{
+			className = Name;
+			ComponentList = new BindingList<BasicNewtonian>();
+			ShipsInClass = new BindingList<NewtonianShip>();
+			ComponentCount = new List<int>();
+			NextBuildNumber = 1;
+		}
+
+		public void AddComponent (BasicNewtonian component, int numberAdded)
+		{
+			if (!ComponentList.Contains (component)) {
+				ComponentList.Add (component);
+				ComponentCount.Add (numberAdded);
+			} else {
+				int index = ComponentList.IndexOf (component);
+				ComponentCount[index] += numberAdded;
+			}
+		}
+
+		public void removeCoponent (BasicNewtonian component, int numberTaken)
+		{
+			if (ComponentList.Contains (component)) {
+				int index = ComponentList.IndexOf(component);
+				if (numberTaken == 0 || numberTaken >= ComponentCount [index]) {
+					ComponentList.RemoveAt (index);
+					ComponentCount.RemoveAt (index);
+				} else {
+					ComponentCount [index] -= numberTaken;
+				}
+			}
+		}
+
+	}
+
 	public class NewtonianShip
 	{
-		public NewtonianShip ()
+		public NewtonianShipClass shipClass;
+
+		public string ShipName { get; set; }
+
+		public List<int> WorkingComponents;
+
+		public NewtonianShip ( NewtonianShipClass parentClass)
 		{
+			parentClass.ShipsInClass.Add(this);
+			ShipName = parentClass.className + " " + parentClass.NextBuildNumber;
+			WorkingComponents = new List<int>(parentClass.ComponentCount);
 		}
 	}
 }
