0b802a2b09716075bf22a7c75292453dcc2206fa
Greg Nott <gvnott@gmail.com>
2012-12-15 19:53:09 +1100

    Refactored F9 data panel to use less designer generated code.
    The SGaD_DataPanel now has the Split container and two data grids
    created and configured entierly by hand written code, instead of
    designer code. This was done to try and resolve bugs reported by
    dizzyfoxkit

--- Pulsar4X/Pulsar4X.UI/Panels/SGaD_DataPanel.cs
+++ Pulsar4X/Pulsar4X.UI/Panels/SGaD_DataPanel.cs
@@ -12,6 +12,8 @@ namespace Pulsar4X.UI.Panels
 {
     public partial class SGaD_DataPanel : DockContent
     {
+
+        private SplitContainer m_oSplitContainer1;
         /// <summary>
         /// Returs the Planets Data Grid View
         /// </summary>
@@ -22,7 +24,6 @@ namespace Pulsar4X.UI.Panels
                 return m_oPlanetsDataGridView;
             }
         }
-
         /// <summary>
         /// Returns the Star Data Grid View
         /// </summary>
@@ -38,13 +39,27 @@ namespace Pulsar4X.UI.Panels
         {
             InitializeComponent();
 
-            // Setup Splitter so star data grid canot be made too small.
-            splitContainer1.Panel1MinSize = 108;
+            // Create Controls:
+            m_oSplitContainer1 = new SplitContainer();
+            m_oPlanetsDataGridView = new DataGridView();
+            m_oStarDataGridView = new DataGridView();
+
+            // Add controls to panel:
+            this.Controls.Add(m_oSplitContainer1);
+            m_oSplitContainer1.Panel1.Controls.Add(m_oStarDataGridView);
+            m_oSplitContainer1.Panel2.Controls.Add(m_oPlanetsDataGridView);
+
+            // Configure controls:
+            m_oSplitContainer1.Dock = DockStyle.Fill;
+            m_oSplitContainer1.Orientation = Orientation.Horizontal;
+            m_oSplitContainer1.Panel1MinSize = 108;
+            m_oStarDataGridView.Dock = DockStyle.Fill;
+            m_oPlanetsDataGridView.Dock = DockStyle.Fill;
         }
 
         public void SetSplitterDistance(int a_iDist)
         {
-            splitContainer1.SplitterDistance = a_iDist;
+            m_oSplitContainer1.SplitterDistance = a_iDist;
         }
     }
 }
