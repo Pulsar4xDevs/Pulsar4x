476d6d7ecfdbbef406a9a957c67a651424e74fb5
Nathan Hittinger <nhdump@gmail.com>
2012-12-16 12:19:55 -0600

    General Component TN part 2
    Ship class has been updated, now all that remains is for it to be
    tested.

--- Pulsar4X/Pulsar4X.Lib/Entities/Ship.cs
+++ Pulsar4X/Pulsar4X.Lib/Entities/Ship.cs
@@ -22,17 +22,64 @@ namespace Pulsar4X.Entities
         public string ClassNotes { get; set; }
         public string Notes { get; set; }
 
-        public int CurrentFuel { get; set; }
-        public int FuelCapacity { get; set; }
-        public int CurrentCrew { get; set; }
-        public int MaxCrew { get; set; }
-        public int MaxLifeSupport { get; set; }
-        public int DamageControlRating { get; set; }
+        public int MaxLifeSupport { get; set; } //here is life support again.
+
+        public int PlanetaryProtectionValue { get; set; }
 
         /// <summary>
         /// The ship will have an armor layering.
         /// </summary>
         public ArmorTN ShipArmor { get; set; }
+
+        /// <summary>
+        /// Crew Quarters, Small Crew Quarters, Tiny Crew Quarters.
+        /// </summary>
+        public BindingList<GeneralComponentTN> CrewQuarters { get; set; }
+
+        /// <summary>
+        /// How many crew are on the ship. Crew aren't necessarily in their quarters when those are destroyed, though the crew requirement for quarters will be at risk.
+        /// </summary>
+        public int CurrentCrew;
+
+        /// <summary>
+        /// How many crew/POWs are in cryo stasis.
+        /// </summary>
+        public int CurrentCryo;
+
+        /// <summary>
+        /// How long has the ship been out on patrol.
+        /// </summary>
+        public int CurrentDeployment;
+
+        /// <summary>
+        /// Fuel Tanks, Small Fuel Tanks, Tiny Fuel Tanks, Large Fuel Tanks, Very Large Fuel Tanks, Ultra Large Fuel Tanks.
+        /// </summary>
+        public BindingList<GeneralComponentTN> FuelTanks { get; set; }
+
+        /// <summary>
+        /// How much fuel in total does the ship have. it will be divided equally among all tanks for convenience.
+        /// </summary>
+        public int CurrentFuel { get; set; }
+
+        /// <summary>
+        /// Engineering bay, Small Engineering Bay, Tiny Engineering Bay, Fighter Engineering Bay.
+        /// </summary>
+        public BindingList<GeneralComponentTN> EngineeringBays { get; set; }
+
+        /// <summary>
+        /// How much maintenance supply is available to this ship, as with fuel it is divided equally among all bays.
+        /// </summary>
+        public int CurrentMSP { get; set; }
+
+        /// <summary>
+        /// Bridge, Flag Bridge, Damage Control, Improved Damage Control, Advanced Damage Control, Maintenance bay, Recreational Facility, Orbital Habitat.
+        /// </summary>
+        public BindingList<GeneralComponentTN> OtherGeneralComponents { get; set; }
+
+        /// <summary>
+        /// What is this ship's current damage control rating. Ebays and damage control both contribute to this value.
+        /// </summary>
+        public int CurrentDamageControlRating { get; set; }
 
         /// <summary>
         /// Ships can potentially have multiple engines, though they must all be of the same type.
--- Pulsar4X/Pulsar4X.Lib/Entities/ShipClass.cs
+++ Pulsar4X/Pulsar4X.Lib/Entities/ShipClass.cs
@@ -21,17 +21,64 @@ namespace Pulsar4X.Entities
         /// <summary>
         /// In 50 ton increments
         /// </summary>
-        public decimal Size { get; set; }
+        public float SizeHS { get; set; }
+        public float SizeTons { get; set; }
+
         public int RequiredRank { get; set; }
-        public int CrewSize { get; set; }
-        public int MaxLifeSupport { get; set; }
-        public int DamageControlRating { get; set; }
-        public int FuelCapacity { get; set; }
+
+        public int MaxLifeSupport { get; set; } //not sure what life support is supposed to be? deployment time?
+
 
         /// <summary>
         /// Armor statistics that matter to the class itself.
         /// </summary>
         public ArmorDefTN ShipArmorDef { get; set; }
+
+        /// <summary>
+        /// Crew Quarters, Small Crew Quarters, Tiny Crew Quarters.
+        /// Every ship has a required crew amount, though for some small sensor only craft it might be 0.
+        /// </summary>
+        public BindingList<GeneralComponentDefTN> CrewQuarters { get; set; }
+        public BindingList<ushort> CrewQuartersCount { get; set; }
+        public int TotalCrewQuarters { get; set; }
+        public int TotalRequiredCrew { get; set; }
+        public int SpareCrewQuarters { get; set; }
+        public int SpareCryoBerths { get; set; }
+        public int MaxDeploymentTime { get; set; }
+        public float TonsPerMan { get; set; }
+        public float CapPerHS { get; set; }
+        public float AccomHSRequirement { get; set; }
+        public float AccomHSAvailable { get; set; }
+
+
+        /// <summary>
+        /// Fuel Tanks, Small Fuel Tanks, Tiny Fuel Tanks, Large Fuel Tanks, Very Large Fuel Tanks, Ultra Large Fuel Tanks.
+        /// Should fuel compression be a tank type or an option?
+        /// </summary>
+        public BindingList<GeneralComponentDefTN> FuelTanks { get; set; }
+        public BindingList<ushort> FuelTanksCount { get; set; }
+        public int TotalFuelCapacity { get; set; }
+
+        /// <summary>
+        /// Engineering bay, Small Engineering Bay, Tiny Engineering Bay, Fighter Engineering Bay.
+        /// Ebays give 3 benefits, they increase MSP, they give a minor boost to damage control and they reduce failure rate for military vessels.
+        /// </summary>
+        public BindingList<GeneralComponentDefTN> EngineeringBays { get; set; }
+        public BindingList<ushort> EngineeringBaysCount { get; set; }
+        public int TotalMSPCapacity { get; set; }
+        public float MaintenanceLife { get; set; }
+        public float AnnualFailureRate { get; set; }
+        public float InitialFailureRate { get; set; }
+        public float YearOneFailureTotal { get; set; }
+        public float YearFiveFailureTotal { get; set; }
+        public int MaxDamageControlRating { get; set; }
+
+        /// <summary>
+        /// Bridge, Flag Bridge, Damage Control, Improved Damage Control, Advanced Damage Control, Maintenance bay, Recreational Facility, Orbital Habitat.
+        /// Each gives a fairly specialist benefit that not all ships will need, excepting the bridge of course.
+        /// </summary>
+        public BindingList<GeneralComponentDefTN> OtherComponents { get; set; }
+        public BindingList<ushort> OtherComponentsCount { get; set; }
 
         /// <summary>
         /// each ship class can only have one type of engine, though several copies may be present.
