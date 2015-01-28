Pulsar4xDevs/Pulsar4x

commit 804d12cc7b54aed10f6d3a400f5f945d0eed731b

Refactor work on SystemContact for a better tomorrow.

Pulsar4X/Pulsar4X.Lib/Entities/SystemContact.cs
@@ -24,19 +24,9 @@ namespace Pulsar4X.Entities
         public SystemPosition LastPosition;
 
         /// <summary>
-        /// If this contact is a planetary population it will be here.
+        /// Bascking entity of this contact.
         /// </summary>
-        public Population Pop { get; set; }
-
-        /// <summary>
-        /// if the contact is a taskgroup it will be stored here.
-        /// </summary>
-        public TaskGroupTN TaskGroup { get; set; }
-
-        /// <summary>
-        /// If the contact is a missile, it goes here.
-        /// </summary>
-        public OrdnanceGroupTN MissileGroup { get; set; }
+        public StarSystemEntity Entity;
 
         /// <summary>
         /// Distance between this contact and the other contacts in the system in AU.
@@ -64,68 +54,20 @@ namespace Pulsar4X.Entities
         /// </summary>
         public CEState ContactElementCreated { get; set; }
 
-
         /// <summary>
         /// Creates a new system contact.
         /// </summary>
         /// <param name="Fact">Faction of contact.</param>
-        /// <param name="body">Type of contact.</param>
-        public SystemContact(Faction Fact, Population pop)
+        /// <param name="entity">Backing entity of the contact.</param>
+        public SystemContact(Faction Fact, StarSystemEntity entity)
         {
             Id = Guid.NewGuid();
             faction = Fact;
-            Position.X = pop.Planet.Position.X;
-            Position.Y = pop.Planet.Position.Y;
-            LastPosition.X = Position.X;
-            LastPosition.Y = Position.Y;
+            Position = entity.Position;
+            LastPosition = Position;
 
-            Pop = pop;
-            SSEntity = StarSystemEntityType.Population;
+            Entity = entity;
 
-            DistanceTable = new BindingList<float>();
-            DistanceUpdate = new BindingList<int>();
-
-            ContactElementCreated = CEState.NotCreated;
-        }
-
-        /// <summary>
-        /// Creates a new system contact.
-        /// </summary>
-        /// <param name="Fact">Faction of contact.</param>
-        /// <param name="TG">Type of contact.</param>
-        public SystemContact(Faction Fact, TaskGroupTN TG)
-        {
-            Id = Guid.NewGuid();
-            faction = Fact;
-            Position.X = TG.Position.X;
-            Position.Y = TG.Position.Y;
-            LastPosition.X = Position.X;
-            LastPosition.Y = Position.Y;
-
-            TaskGroup = TG;
-            SSEntity = StarSystemEntityType.TaskGroup;
-            DistanceTable = new BindingList<float>();
-            DistanceUpdate = new BindingList<int>();
-
-            ContactElementCreated = CEState.NotCreated;
-        }
-
-        /// <summary>
-        /// Creates a new system contact.
-        /// </summary>
-        /// <param name="Fact">Faction of contact.</param>
-        /// <param name="MG">Type of contact.</param>
-        public SystemContact(Faction Fact, OrdnanceGroupTN MG)
-        {
-            Id = Guid.NewGuid();
-            faction = Fact;
-            Position.X = MG.Position.X;
-            Position.Y = MG.Position.Y;
-            LastPosition.X = Position.X;
-            LastPosition.Y = Position.Y;
-
-            MissileGroup = MG;
-            SSEntity = StarSystemEntityType.Missile;
             DistanceTable = new BindingList<float>();
             DistanceUpdate = new BindingList<int>();
 

Pulsar4X/Pulsar4X.Lib/Entities/Population.cs
@@ -255,7 +255,7 @@ namespace Pulsar4X.Entities
         }
     }
 
-    public class Population : GameEntity
+    public class Population : StarSystemEntity
     {
 
         /// <summary>

Pulsar4X/Pulsar4X.Lib/Entities/StarSystem.cs
@@ -30,17 +30,17 @@ namespace Pulsar4X.Entities
         /// <summary>
         /// A list of TaskGroups currently inside this system.
         /// </summary>
-        public List<TaskGroupTN> TaskGroups { get; set; }
+        public BindingList<TaskGroupTN> TaskGroups { get; set; }
 
         /// <summary>
         /// A list of Populations currently inside this system.
         /// </summary>
-        public List<Population> Populations { get; set; }
+        public BindingList<Population> Populations { get; set; }
 
         /// <summary>
         /// A list of OrdnanceGroups (Missile Groups) currently inside this system.
         /// </summary>
-        public List<OrdnanceGroupTN> OrdnanceGroups { get; set; }
+        public BindingList<OrdnanceGroupTN> OrdnanceGroups { get; set; }
 
         /// <summary>
         /// Global List of all contacts within the system.
@@ -88,6 +88,15 @@ namespace Pulsar4X.Entities
             SystemContactList = new BindingList<SystemContact>();
             FactionDetectionLists = new BindingList<FactionSystemDetection>();
 
+            TaskGroups = new BindingList<TaskGroupTN>();
+            Populations = new BindingList<Population>();
+            OrdnanceGroups = new BindingList<OrdnanceGroupTN>();
+
+            // Subscribe to change events.
+            TaskGroups.ListChanged += ContactsChanged;
+            Populations.ListChanged += ContactsChanged;
+            OrdnanceGroups.ListChanged += ContactsChanged;
+
             ContactCreateList = new BindingList<SystemContact>();
             ContactDeleteList = new BindingList<SystemContact>();
         }
@@ -101,9 +110,6 @@ namespace Pulsar4X.Entities
         {
             Waypoint NewWP = new Waypoint(Title, this, X, Y, FactionID);
             Waypoints.Add(NewWP);
-            //logger.Info("Waypoint added.");
-            //logger.Info(Position.XAU.ToString());
-            //logger.Info(Position.YAU.ToString());
         }
 
         /// <summary>
@@ -112,9 +118,6 @@ namespace Pulsar4X.Entities
         /// <param name="Remove"></param>
         public void RemoveWaypoint(Waypoint Remove)
         {
-            //logger.Info("Waypoint Removed.");
-            //logger.Info(Remove.Position.X.ToString());
-            //logger.Info(Remove.Position.Y.ToString());
             if (Waypoints.Count == 1)
                 Waypoints.Clear();
             else
@@ -133,18 +136,6 @@ namespace Pulsar4X.Entities
             JumpPoint NewJP = new JumpPoint(this, parentStar, XOffsetAU, YOffsetAU);
             JumpPoints.Add(NewJP);
             return NewJP;
-        }
-
-        public void AddTaskGroup(TaskGroupTN TaskGroup)
-        {
-            TaskGroups.Add(TaskGroup);
-            contactsChanged = true;
-        }
-
-        public void RemoveTaskGroup(TaskGroupTN TaskGroup)
-        {
-            TaskGroups.Remove(TaskGroup);
-            contactsChanged = true;
         }
 
         /// <summary>
@@ -248,21 +239,29 @@ namespace Pulsar4X.Entities
         public int GetProtectionLevel(Faction fact)
         {
             int PPV = 0;
-            foreach (SystemContact Contact in SystemContactList)
+            foreach (TaskGroupTN TaskGroup in TaskGroups)
             {
-                if (Contact.SSEntity == StarSystemEntityType.TaskGroup)
+                if (TaskGroup.TaskGroupFaction == fact)
                 {
-                    if (Contact.TaskGroup.TaskGroupFaction == fact)
+                    foreach (ShipTN Ship in TaskGroup.Ships)
                     {
-                        foreach (ShipTN Ship in Contact.TaskGroup.Ships)
-                        {
-                            PPV = PPV + Ship.ShipClass.PlanetaryProtectionValue;
-                        }
+                        PPV = PPV + Ship.ShipClass.PlanetaryProtectionValue;
                     }
                 }
             }
 
             return PPV;
+        }
+
+        /// <summary>
+        /// Event raised when TaskGroups, Populations, or OrdnanceGroups are
+        /// added/removed from the system.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContactsChanged(object sender, ListChangedEventArgs e)
+        {
+            contactsChanged = true;
         }
     }
 }

commit 95922f8e50f52989cac1817f7ae700c4cb8c8149

Fix for display bug introduced in commit 804d12cc7b54aed10f6d3a400f5f945d0eed731b

Pulsar4X/Pulsar4X.Lib/Entities/SystemContact.cs
@@ -72,6 +72,7 @@ namespace Pulsar4X.Entities
             DistanceUpdate = new BindingList<int>();
 
             ContactElementCreated = CEState.NotCreated;
+            SSEntity = entity.SSEntity;
         }
 
         /// <summary>