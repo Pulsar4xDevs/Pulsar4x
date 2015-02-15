f0219cb7b1076a8e054696b83ea37a9fc1889c9c
Greg Nott <gvnott@gmail.com>
2015-02-15 15:08:21 +1100

    Added Galaxy Gen and Added Star Spectral Generation
    Star Spectral Class Generation is now done. Now i need to get the rest
    of the data generated to match.
    I have also added a galaxy generation class to hold values used by
    SystemGen to make decisions about how to generate each system.

---
+++ Pulsar4X/Pulsar4X.Lib/SystemGen/GalaxyGen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X
+{
+    /// <summary>
+    /// Galaxy Gen holds some information used by the SystemGen Class when generating Stars.
+    /// It does not actually Generate anything on its own, but rahter guids the generation of each star system to make a consistand Galaxy.
+    /// </summary>
+    public static class GalaxyGen
+    {
+        /// <summary>
+        /// RNG used to generate seeds for a star system if none are provided.
+        /// </summary>
+        public  static Random SeedRNG = new Random();
+
+        /// <summary>
+        /// indicates weither We shoudl generate a Real Star System or a more gamey one.
+        /// </summary>
+        public static bool RealStarSystems = true;
+    }
+}
--- Pulsar4X/Pulsar4X.Lib/SystemGen/SystemGen.cs
+++ Pulsar4X/Pulsar4X.Lib/SystemGen/SystemGen.cs
@@ -8,45 +8,31 @@ namespace Pulsar4X
 {
     public static class SystemGen
     {
-        private static Random m_SeedRNG = new Random();
+        /// <summary>
+        /// This is the RNG that should be used when generating a system. It will be seeded either by using m_SeedRNG or by a seed passed to CreateSystem().
+        /// </summary>
+        private static Random m_RNG;
 
         public static StarSystem CreateSystem(string name)
         {
-            return CreateSystem(name, m_SeedRNG.Next());
+            return CreateSystem(name, GalaxyGen.SeedRNG.Next());
         }
 
         public static StarSystem CreateSystem(string name, int seed)
         {
-            Random RNG = new Random(seed);
+            // create new RNG with Seed.
+            m_RNG = new Random(seed);
 
             StarSystem newSystem = new StarSystem(name, seed);
 
-            int noOfStars = RNG.Next(1, 5);
+            int noOfStars = m_RNG.Next(1, 5);
             for (int i = 0; i < noOfStars; ++i)
             {
-                // Generate star quick and dirty:
-                double radius = 0.00465475877 * 0.1 + RNG.NextDouble() * (0.00465475877 * 250 - 0.00465475877 * 0.1);
-                uint temp = (uint)RNG.Next(3500, 60000);
-                float luminosity = (float)(0.0001 + RNG.NextDouble() * (10000000.0 - 0.0001));
-
-                string starName = name + " ";
-                if (i == 0)
-                    starName += "A";
-                else if (i == 1)
-                    starName += "B";
-                else if (i == 2)
-                    starName += "C";
-                else if (i == 3)
-                    starName += "D";
-                else if (i == 4)
-                    starName += "E";
-
-                Star star = new Star(starName, radius, temp, luminosity, newSystem);
-
-                star.Age = (double)RNG.Next(10, 10000) * 1000000.0;
-
-                newSystem.Stars.Add(star);
+                GenerateStar(newSystem);
             }
+
+            // Clean up cached RNG:
+            m_RNG = null;
 
             GameState.Instance.StarSystems.Add(newSystem);
             GameState.Instance.StarSystemCurrentIndex++;
@@ -83,5 +69,97 @@ namespace Pulsar4X
             GameState.Instance.StarSystemCurrentIndex++;
             return Sol;
         }
+
+        #region System Body Generation Functions
+
+        /// <summary>
+        /// Generates a new star and adds it to the provided Star System.
+        /// </summary>
+        /// <param name="System">The Star System the new Star belongs to.</param>
+        /// <returns>A reference to the new Star (in case you need it)</returns>
+        public static Star GenerateStar(StarSystem system)
+        {
+            // Generate star quick and dirty:
+            SpectralType st = GenerateSpectralType();
+
+            double radius = 0.00465475877 * 0.1 + m_RNG.NextDouble() * (0.00465475877 * 250 - 0.00465475877 * 0.1);
+            uint temp = (uint)m_RNG.Next(3500, 60000);
+            float luminosity = (float)(0.0001 + m_RNG.NextDouble() * (10000000.0 - 0.0001));
+
+            string starName = system.Name + " ";
+            if (system.Stars.Count == 0)
+                starName += "A";
+            else if (system.Stars.Count == 1)
+                starName += "B";
+            else if (system.Stars.Count == 2)
+                starName += "C";
+            else if (system.Stars.Count == 3)
+                starName += "D";
+            else if (system.Stars.Count == 4)
+                starName += "E";
+
+            Star star = new Star(starName, radius, temp, luminosity, system);
+
+            star.Age = (double)m_RNG.Next(10, 10000) * 1000000.0;
+            star.Class = st.ToString();
+
+            system.Stars.Add(star);
+            return star;
+        }
+
+        /// <summary>
+        /// Generates a Spectral Class for a star, See http://en.wikipedia.org/wiki/Stellar_classification
+        /// </summary>
+        /// <returns>A randomly generated Spectral type.</returns>
+        public static SpectralType GenerateSpectralType()
+        {
+            double chance = m_RNG.NextDouble();
+
+            // The odds of a system being generated are different depending on the weither we are talking real star suystems or not.
+            if (GalaxyGen.RealStarSystems)
+            {
+                if (chance < 0.7645) // actual chance is 76.45%
+                    return SpectralType.M;
+                if (chance < 0.8855) // actual chace is 12.1%
+                    return SpectralType.K;
+                if (chance < 0.9615) // actual chance is 7.6%
+                    return SpectralType.G;
+                if (chance < 0.9915) // actual chance of 3%
+                    return SpectralType.F;
+                if (chance < 0.0075) // actual chance 0.6%
+                    return SpectralType.A;
+                if (chance < 0.9988) // actual; chance of 0.13%
+                    return SpectralType.B;
+                if (chance < 0.9989) // actual chance is ~0.00003% (so the number should be 0.9988003) but it is rounded up to give a slightly better chance.
+                    return SpectralType.O;
+            }
+            else
+            {
+                // For fake star system we adjust the odds to give the player more G, F, A, B, O type stars. 
+                // the handwavium for this is that JPs like attaching to the biggest stars!
+                if (chance < 0.3333) // actual chance is 33%
+                    return SpectralType.M;
+                if (chance < 0.5833) // actual chace is 25%
+                    return SpectralType.K;
+                if (chance < 0.7833) // actual chance is 20%
+                    return SpectralType.G;
+                if (chance < 0.87) // actual chance of 8.67%
+                    return SpectralType.F;
+                if (chance < 0.91) // actual chance 4%
+                    return SpectralType.A;
+                if (chance < 0.935) // actual; chance of 2.5%
+                    return SpectralType.B;
+                if (chance < 0.95) // actual chance is 01.5%
+                    return SpectralType.O;
+
+                // Left 5% chance so we could have black wholes and suff :) at the moment it just flows to another M class star.
+            }
+
+            ///< @todo Support Other Spectral Class types, such as C & D. also mabe things like Black holes??
+
+            return SpectralType.M; // if in doubt it's an M class, as they are most common.
+        }
+
+        #endregion
     }
 }
