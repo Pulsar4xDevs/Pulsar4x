a25838015e8be9dcd30a9a3b634c098c353991a9
Greg Nott <gvnott@gmail.com>
2015-02-18 00:43:18 +1100

    Added a Atmosphere class.
    as a starting point.

---
+++ Pulsar4X/Pulsar4X.Lib/Entities/StarSystem/Atmosphere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X.Entities.StarSystem
+{
+    
+    /// <summary>
+    /// The Atmosphere of a Planet or Moon.
+    /// @todo Make this a generic component.
+    /// </summary>
+    class Atmosphere
+    {
+        /// <summary>
+        /// Atmospheric Presure
+        /// Units to be decided.
+        /// </summary>
+        public float Pressure { get; set; }
+        
+        /// <summary>
+        /// Weather or not the planet has abundent water.
+        /// </summary>
+        public bool Hydrosphere { get; set; }
+
+        /// <summary>
+        /// The percentage of the bodies sureface covered by water.
+        /// </summary>
+        public short HydrosphereExtent { get; set; }
+
+        /// <summary>
+        /// A measure of the greenhouse factor provided by this Atmosphere.
+        /// </summary>
+        public float GreenhouseFactor { get; set; }
+    }
+}
