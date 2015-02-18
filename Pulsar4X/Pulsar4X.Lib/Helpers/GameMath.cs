237f5dde06a0249bf4ed7ff2b6eee41e151ebc19
Rod-Serling <jthomson145@gmail.com>
2015-02-18 16:47:39 -0500

    Added WeightedList math helper.
    Great for selecting a random value from a weighted list!

---
+++ Pulsar4X/Pulsar4X.Lib/Helpers/GameMath.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X.Helpers.GameMath
+{
+    /// <summary>
+    /// Weighted list used for selecting values with a random number generator.
+    /// </summary>
+    /// <remarks>
+    /// This is a weighted list. Eeight input values do not need to add up to 1.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// WeightedList<string> fruitList = new WeightList<string>();
+    /// fruitList.Add(0.2, "Apple");
+    /// fruitList.Add(0.5, "Banana");
+    /// fruitList.Add(0.3, "Tomatoe");
+    /// 
+    /// fruitSelection = fruitList.Select(0.1)
+    /// print(fruitSelection); // "Apple"
+    /// 
+    /// fruitSelection = fruitList.Select(0.69)
+    /// print(fruitSelection); // "Banana"
+    /// 
+    /// string fruitSelection = fruitList.Select(0.7)
+    /// print(fruitSelection); // "Tomatoe"
+    /// </code>
+    /// </example>
+    /// <example>
+    /// <code>
+    /// WeightedList<string> fruitList = new WeightList<string>();
+    /// fruitList.Add(4, "Apple");
+    /// fruitList.Add(6, "Banana");
+    /// fruitList.Add(10, "Tomatoe");
+    /// 
+    /// fruitSelection = fruitList.Select(0.19)
+    /// print(fruitSelection); // "Apple"
+    /// 
+    /// fruitSelection = fruitList.Select(0.2)
+    /// print(fruitSelection); // "Banana"
+    /// 
+    /// string fruitSelection = fruitList.Select(0.5)
+    /// print(fruitSelection); // "Tomatoe"
+    /// </code>
+    /// </example>
+    /// 
+    public class WeightedList<T>
+    {
+        private class WeightedValue<T>
+        {
+            public double Weight { get; set; }
+            public T Value { get; set; }
+        }
+
+        List<WeightedValue<T>> m_valueList;
+
+        /// <summary>
+        /// Total weights of the list.
+        /// </summary>
+        public double TotalWeight { get { return m_totalWeight; } }
+        double m_totalWeight;
+
+        public WeightedList()
+        {
+            m_valueList = new List<WeightedValue<T>>();
+        }
+
+        /// <summary>
+        /// Adds a value to the weighted list.
+        /// </summary>
+        /// <param name="weight">Weight of this value in the list.</param>
+        public void Add(double weight, T value)
+        {
+            WeightedValue<T> listEntry = new WeightedValue<T>();
+            listEntry.Weight = weight;
+            listEntry.Value = value;
+
+            m_valueList.Add(listEntry);
+
+            m_totalWeight += weight;
+        }
+
+        /// <summary>
+        /// Selects a value from the list based on the input.
+        /// </summary>
+        /// <param name="rngValue">Value 0.0 to 1.0 represending the random value selected by the RNG.</param>
+        /// <returns></returns>
+        public T Select(double rngValue)
+        {
+            double cumulativeChance = 0;
+            foreach (WeightedValue<T> listEntry in m_valueList)
+            {
+                double realChance = listEntry.Weight / m_totalWeight;
+                cumulativeChance += realChance;
+
+                if (rngValue < cumulativeChance)
+                {
+                    return listEntry.Value;
+                }
+            }
+            throw new InvalidOperationException("Failed to choose a random value.");
+        }
+    }
+}
