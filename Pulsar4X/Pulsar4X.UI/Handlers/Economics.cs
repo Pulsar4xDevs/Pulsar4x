b20f5298af3f259a7e4cc311a5f7dba16286653a
Nathan Hittinger <nhdump@gmail.com>
2015-02-28 23:13:25 -0600

    Geosurvey report fix.
    bleh.

--- Pulsar4X/Pulsar4X.UI/Handlers/Economics.cs
+++ Pulsar4X/Pulsar4X.UI/Handlers/Economics.cs
@@ -2077,11 +2077,11 @@ namespace Pulsar4X.UI.Handlers
                     m_oSummaryPanel.SummaryDataGrid.Rows[12 + Adjust1].Cells[1].Value = "";
                     m_oSummaryPanel.SummaryDataGrid.Rows[13 + Adjust1].Cells[0].Value = "Tectonics";
                     m_oSummaryPanel.SummaryDataGrid.Rows[13 + Adjust1].Cells[1].Value = null; //CurrentPopulation.SystemBody.PlanetaryTectonics;
-                    //if (CurrentPopulation.SystemBody.GeoTeamSurvey)
+                    if (CurrentPopulation.Planet.GeoSurveyList.ContainsKey(CurrentFaction) == true)
                     {
                         Entry = "Completed";
                     }
-                    //else
+                    else
                     {
                         Entry = "No";
                     }
