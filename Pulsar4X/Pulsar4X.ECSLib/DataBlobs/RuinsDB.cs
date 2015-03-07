dd9427ba52f491c501a6660b6b4dedc740b82028
Jon <jon.se5a@gmail.com>
2015-03-08 05:44:08 +1300

    RuinsDB: fixed constructor, removed readonly keywords.

--- Pulsar4X/Pulsar4X.ECSLib/DataBlobs/RuinsDB.cs
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/RuinsDB.cs
@@ -7,6 +7,9 @@ namespace Pulsar4X.ECSLib.DataBlobs
 {
     class RuinsDB : BaseDataBlob
     {
+        /// <summary>
+        /// Ruins size descriptors
+        /// </summary>
         public enum RSize
         {
             NoRuins,
@@ -17,6 +20,9 @@ namespace Pulsar4X.ECSLib.DataBlobs
             Count
         }
 
+        /// <summary>
+        /// Ruins Quality descriptors
+        /// </summary>
         public enum RQuality
         {
             Destroyed,
@@ -28,28 +34,38 @@ namespace Pulsar4X.ECSLib.DataBlobs
         }
 
         /// <summary>
+        /// How many ruins are on this world. or something.
+        /// </summary>
+        public uint RuinCount;
+
+        /// <summary>
         /// What kinds of things should be found in this ruin? including sophistication of killbots?
         /// </summary>
-        public readonly int RuinTechLevel;
+        public int RuinTechLevel;
 
         /// <summary>
         /// How big are these ruins?
         /// </summary>
-        public readonly RSize RuinSize;
+        public RSize RuinSize;
 
         /// <summary>
         /// What shape are these ruins in?
         /// </summary>
-        public readonly RQuality RuinQuality;
+        public RQuality RuinQuality;
 
         /// <summary>
-        /// 
+        /// Constructor for RuinsDataBlob
         /// </summary>
-        /// <param name="entityID"></param>
+        /// <param name="ruinCount"></param>
         /// <param name="ruinTechLevel"> What kinds of things should be found in this ruin? including sophistication of killbots?</param>
         /// <param name="ruinSize">How big are these ruins?</param>
         /// <param name="ruinQuality"> What shape are these ruins in?</param>
-        public RuinsDB(int ruinTechLevel, RSize ruinSize, RQuality ruinQuality)
-        { }
+        public RuinsDB(uint ruinCount, int ruinTechLevel, RSize ruinSize, RQuality ruinQuality)
+        {
+            RuinCount = ruinCount;
+            RuinTechLevel = ruinTechLevel;
+            RuinSize = ruinSize;
+            RuinQuality = ruinQuality;
+        }
     }
 }
