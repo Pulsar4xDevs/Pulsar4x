6ddf1276200d8811ad65161c3cd77f6c35d18dfa
Jon <jon.se5a@gmail.com>
2015-03-07 21:20:43 +1300

    RuinsDB.cs added.

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/RuinsDB.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X.ECSLib.DataBlobs
+{
+    struct RuinsDB : IDataBlob
+    {
+        public enum RSize
+        {
+            NoRuins,
+            Outpost,
+            Settlement,
+            Colony,
+            City,
+            Count
+        }
+
+        public enum RQuality
+        {
+            Destroyed,
+            Ruined,
+            PartiallyIntact,
+            Intact,
+            MultipleIntact,
+            Count
+        }
+
+        public int Entity { get { return m_entityID; } }
+        private readonly int m_entityID;
+
+        public IDataBlob UpdateEntityID(int newEntityID)
+        {
+            return new RuinsDB(newEntityID, RuinTechLevel, RuinSize, RuinQuality);
+        }
+
+        /// <summary>
+        /// What kinds of things should be found in this ruin? including sophistication of killbots?
+        /// </summary>
+        public readonly int RuinTechLevel;
+
+        /// <summary>
+        /// How big are these ruins?
+        /// </summary>
+        public readonly RSize RuinSize;
+
+        /// <summary>
+        /// What shape are these ruins in?
+        /// </summary>
+        public readonly RQuality RuinQuality;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entityID"></param>
+        /// <param name="ruinTechLevel"> What kinds of things should be found in this ruin? including sophistication of killbots?</param>
+        /// <param name="ruinSize">How big are these ruins?</param>
+        /// <param name="ruinQuality"> What shape are these ruins in?</param>
+        public RuinsDB(int entityID, int ruinTechLevel, RSize ruinSize, RQuality ruinQuality)
+            : this()
+        { }
+    }
+}
