168b5d697902f7967eab0d9639a072cb96135ec5
Jon <jon.se5a@gmail.com>
2015-03-15 02:56:33 +1300

    the I've no idea what I'm doing wpfui commit.

---
+++ Pulsar4X/Pulsar4X.ECSLib/Engine-Comms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Pulsar4X.ECSLib
+{
+    public class Engine_Comms
+    {
+        Dictionary<Guid, Queue<Message>> _MessagesforUI = new Dictionary<Guid, Queue<Message>>();
+
+        Queue<Message> Messagequeue(Guid factionID)
+        {
+            return _MessagesforUI[factionID];
+        }
+
+        //public Message Messageforfaction(Guid factionID)        
+        //{
+        //    Queue<Message> queue = Messagequeue(factionID);
+        //
+        //    return queue.Take(1);
+        //}
+
+    }
+
+    public class Message
+    {
+
+    }
+}
---
+++ Pulsar4X/WPFUI/UI-Comms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pulsar4X.ECSLib;
+namespace WPFUI
+{
+    class UI_Comms
+    {
+        Guid factionID;
+        //Message message = Engine_Comms.Messageforfaction(factionID);
+
+    }
+}
