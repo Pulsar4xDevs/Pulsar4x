a4c550ecb263fb533d090c4eae931330e3dffb87
Greg Nott <gvnott@gmail.com>
2015-03-31 10:50:51 +1100

    Added a main game loop
    This is a function that can be run on its own thread. it will loop
    checking for messages and processing them until it gets the quit
    message.

--- Pulsar4X/Pulsar4X.ECSLib/Game.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Game.cs
@@ -85,6 +85,91 @@ namespace Pulsar4X.ECSLib
         }
 
         /// <summary>
+        /// Runs the game simulation in a loop. Will check for and process messages from the UI.
+        /// </summary>
+        public void MainGameLoop()
+        {
+            bool quit = false;
+            bool messageProcessed = false;
+
+            while (!quit)
+            {
+                // lets first check if there are things waiting in a queue:
+                if (EngineComms.LibMessagesWaiting() == false)
+                {
+                    // there is nothing from the UI, so lets sleep for a while before checking again.
+                    Wait();
+                    continue; // go back and check again.
+                }
+
+                // loop through all the incoming queues looking for a new message:
+                List<int> factions = m_globalManager.GetAllEntitiesWithDataBlob<PopulationDB>();
+                foreach (int faction in factions)
+                {
+                    // lets just take a peek first:
+                    Message message;
+                    if (EngineComms.LibPeekFactionInQueue(faction, out message) && IsMessageValid(message))
+                    {
+                        // we have a valid message we had better take it out of the queue:
+                        message = EngineComms.LibReadFactionInQueue(faction);
+
+                        // process it:
+                        ProcessMessage(faction, message, ref quit);
+                        messageProcessed = true;
+                    }
+                }
+
+                // lets check if we processed a valid message this time around:
+                if (messageProcessed)
+                {
+                    // so we processed a valid message, better check for a new one right away:
+                    messageProcessed = false;
+                    continue;
+                }
+                else
+                {
+                    // we didn't process a valid message... 
+                    // we should probably wait for a while for the pulse to finish or new stuff to queue up
+                    Wait();
+                }
+            }
+        }
+
+        private bool IsMessageValid(Message message)
+        {
+            return true; // we will do this until we have messages that can be invalid!!
+        }
+
+        private void ProcessMessage(int faction, Message message, ref bool quit)
+        {
+            if (message == null)
+            {
+                return;
+            }
+
+            switch (message._messageType)
+            {
+                case Message.MessageType.Quit:
+                    quit = true;                                        // cause the game to quit!
+                    break;
+                case Message.MessageType.Echo:
+                    EngineComms.LibWriteOutQueue(faction, message);     // echo chamber ;)
+                    break;
+                default:
+                    throw new System.Exception("Message of type: " + message._messageType.ToString() + ", Went unprocessed.");
+            }
+        }
+
+        private void Wait()
+        {
+            // we should have a better way of doing this
+            // is there a way for the EnginComs class to fire an event to wake the thread when 
+            // a new message come is??
+            // that would be the ideal way to do it, no wasted time, no wasted CPU usage.
+            Thread.Sleep(5);  
+        }
+
+        /// <summary>
         /// Time advancement code. Attempts to advance time by the number of seconds
         /// passed to it.
         /// 
---
+++ Pulsar4X/Pulsar4X.Tests/MainGameLoopTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pulsar4X.ECSLib;
+using Pulsar4X.ECSLib.DataBlobs;
+
+namespace Pulsar4X.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture, Description("Basic Tests for the Main Game Loop.")]
+    class MainGameLoopTests
+    {
+        Game game;
+        System.Threading.Thread gameThread;
+
+        [SetUp]
+        public void Init()
+        {
+            game = new Game();
+
+            // add some factions:
+            List<BaseDataBlob> list = new List<BaseDataBlob>();
+            list.Add(new PopulationDB(42));
+            int factionID = game.GlobalManager.CreateEntity(list);
+            game.EngineComms.AddFaction(factionID);
+
+            factionID = game.GlobalManager.CreateEntity(list);
+            game.EngineComms.AddFaction(factionID);
+
+            factionID = game.GlobalManager.CreateEntity(list);
+            game.EngineComms.AddFaction(factionID);
+
+            factionID = game.GlobalManager.CreateEntity(list);
+            game.EngineComms.AddFaction(factionID);
+
+            factionID = game.GlobalManager.CreateEntity(list);
+            game.EngineComms.AddFaction(factionID);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if (gameThread.IsAlive)
+                gameThread.Abort();  // not good, but better safe then sorry :)
+
+            game = null;
+        }
+
+        [Test]
+        public void TestMainGameLoop()
+        {
+             // lets start the main game loop in a different thread:
+             gameThread = new System.Threading.Thread(game.MainGameLoop);
+             gameThread.Start();
+             Assert.AreEqual(true, gameThread.ThreadState != System.Threading.ThreadState.Unstarted); // has it started?
+
+             MessageBook mb0 = game.EngineComms.RequestMessagebook(0); // lets try get the message book for the first faction.
+             Assert.NotNull(mb0);
+
+             // lets try an echo:
+             mb0.InMessageQueue.Enqueue(new Message(Message.MessageType.Echo, 42));
+             System.Threading.Thread.Sleep(100); // give the game time to echo!!
+             Message message = null;
+             Assert.IsTrue(mb0.OutMessageQueue.TryDequeue(out message), "Lib did not return echo message.");
+             Assert.NotNull(message);
+             Assert.AreEqual(42, Convert.ToInt32(message._data));
+
+             // now lets try quiting:
+             mb0.InMessageQueue.Enqueue(new Message(Message.MessageType.Quit, null));
+             gameThread.Join();
+             System.Threading.Thread.Sleep(100); // give the game time to quit!!
+             Assert.AreEqual(true, gameThread.ThreadState == System.Threading.ThreadState.Stopped); // has it stopped?
+        }
+    }
+}
