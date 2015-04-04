56787c9a5bc47bbc83e228fa208891522135e5cb
se5a <jon.se5a@gmail.com>
2015-04-05 07:54:58 +1200

    attempt at commander/leader DBs

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/CommanderDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X.ECSLib.DataBlobs
+{
+    public enum CommanderTypes
+    {
+        Navy,
+        Ground,
+        Civilian
+    }
+
+    public class CommanderDB
+    {
+        public CommanderNameDB Name;
+
+        public int Rank; //maybe rank/title should be part of name
+
+        public CommanderTypes Type;
+
+        public Dictionary<string, int> Bonuses; //bonuses should be blobs of thier own maybe.
+    }
+}
---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/CommanderNameDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pulsar4X.ECSLib.DataBlobs
+{
+    public class CommanderNameDB
+    {
+        public string First;
+        public string Last;
+        public bool IsFemale;
+    }
+}
