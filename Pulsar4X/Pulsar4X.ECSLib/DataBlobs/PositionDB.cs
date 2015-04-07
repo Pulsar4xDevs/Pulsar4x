bbef82ebfe33153da046b37bc5e7ec8bcbb1b1e5
Greg Nott <gvnott@gmail.com>
2015-04-07 12:29:30 +1000

    Changed PositionDB to use the new Vector4 class
    Also added some conversion functions and some Distance Squared
    functions.

--- Pulsar4X/Pulsar4X.ECSLib/DataBlobs/PositionDB.cs
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/PositionDB.cs
@@ -1,23 +1,88 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Pulsar4X.ECSLib
 {
     public class PositionDB : BaseDataBlob
     {
         /// <summary>
+        /// The Position as a Vec4, in AU.
+        /// </summary>
+        public Vector4 Position;
+
+        /// <summary>
         /// System X coordinante in AU
         /// </summary>
-        public double X;
+        [JsonIgnore]
+        public double X
+        {
+            get { return Position.X; }
+            set { Position.X = value; }
+        }
 
         /// <summary>
         /// System Y coordinante in AU
         /// </summary>
-        public double Y;
+        [JsonIgnore]
+        public double Y
+        {
+            get { return Position.Y; }
+            set { Position.Y = value; }
+        }
 
         /// <summary>
         /// System Z coordinate in AU
         /// </summary>
-        public double Z;
+        [JsonIgnore]
+        public double Z
+        {
+            get { return Position.Z; }
+            set { Position.Z = value; }
+        }
+
+        #region Unit Conversion Properties
+
+        /// <summary>
+        /// Position as a vec4. This is a utility property that converts Position to Km on get and to AU on set.
+        /// </summary>
+        [JsonIgnore]
+        public Vector4 PositionInKm
+        {
+            get { return new Vector4(Distance.ToKm(Position.X), Distance.ToKm(Position.Y), Distance.ToKm(Position.Z), 0); }
+            set { Position = new Vector4(Distance.ToAU(value.X), Distance.ToAU(value.Y), Distance.ToAU(value.Z), 0); }
+        }
+
+        /// <summary>
+        /// System X coordinante. This is a utility property that converts the X Coord. to Km on get and to AU on set.
+        /// </summary>
+        [JsonIgnore]
+        public double XInKm
+        {
+            get { return Distance.ToKm(Position.X); }
+            set { Position.X = Distance.ToAU(value); }
+        }
+
+        /// <summary>
+        /// System Y coordinante. This is a utility property that converts the Y Coord. to Km on get and to AU on set.
+        /// </summary>
+        [JsonIgnore]
+        public double YInKm
+        {
+            get { return Distance.ToKm(Position.Y); }
+            set { Position.Y = Distance.ToAU(value); }
+        }
+
+        /// <summary>
+        /// System Z coordinate. This is a utility property that converts the Z Coord. to Km on get and to AU on set.
+        /// </summary>
+        [JsonIgnore]
+        public double ZInKm
+        {
+            get { return Distance.ToKm(Position.Z); }
+            set { Position.Z = Distance.ToAU(value); }
+        }
+
+        #endregion
 
         /// <summary>
         /// Initilized constructor.
@@ -27,34 +92,50 @@ namespace Pulsar4X.ECSLib
         /// <param name="y">Y value.</param>
         public PositionDB(double x, double y, double z)
         {
-            X = x;
-            Y = y;
-            Z = z;
+            Position = new Vector4(x, y, z, 0);
+        }
+
+        public PositionDB(Vector4 pos)
+        {
+            Position = pos;
+        }
+
+        public PositionDB()
+        {
+            Position = Vector4.Zero;
         }
 
         /// <summary>
         /// Static function to find the distance between two positions.
         /// </summary>
-        /// <param name="posA"></param>
-        /// <param name="posB"></param>
-        /// <returns>distance between posA and posB</returns>
+        /// <returns>Distance between posA and posB.</returns>
         public static double GetDistanceBetween(PositionDB posA, PositionDB posB)
         {
-            double distX = (posA.X - posB.X);
-            double distY = (posA.Y - posB.Y);
-            double distZ = (posA.Z - posB.Z);
-
-            return Math.Sqrt((distX * distX) + (distY * distY) + (distZ * distZ));
+            return (posA.Position - posB.Position).Length();
         }
 
         /// <summary>
         /// Instance function for those who don't like static functions.
         /// </summary>
-        /// <param name="otherPos"></param>
-        /// <returns></returns>
         public double GetDistanceTo(PositionDB otherPos)
         {
             return GetDistanceBetween(this, otherPos);
+        }
+
+        /// <summary>
+        /// Static Function to find the Distance Squared betweeen two positions.
+        /// </summary>
+        public static double GetDistanceBetweenSqrd(PositionDB posA, PositionDB posB)
+        {
+            return (posA.Position - posB.Position).LengthSquared();
+        }
+
+        /// <summary>
+        /// Instance function for those who don't like static functions.
+        /// </summary>
+        public double GetDistanceToSqrd(PositionDB otherPos)
+        {
+            return GetDistanceBetweenSqrd(this, otherPos);
         }
 
         /// <summary>
@@ -65,7 +146,7 @@ namespace Pulsar4X.ECSLib
         /// <returns></returns>
         public static PositionDB operator +(PositionDB posA, PositionDB posB)
         {
-            return new PositionDB(posA.X + posB.X, posA.Y + posB.Y, posA.Z + posB.Z);
+            return new PositionDB(posA.Position + posB.Position);
         }
     }
 }
