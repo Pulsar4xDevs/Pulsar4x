12d8ad88c660ed6ff8f66d39d3d184a9a596968b
Rod-Serling <jthomson145@gmail.com>
2015-04-13 17:52:53 -0400

    Unit test for dataBlob deep copy constructors.

--- Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
+++ Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
@@ -10,6 +10,22 @@ using Pulsar4X.ECSLib;
 
 namespace Pulsar4X.Tests
 {
+    class DataBlobTestException : Exception
+    {
+        public DataBlobTestException()
+        {
+        }
+
+        public DataBlobTestException(string message) : base(message)
+        {
+        }
+
+        public DataBlobTestException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
     [TestFixture]
     [Description("DataBlob Tests")]
     class DataBlobTests
@@ -21,9 +37,27 @@ namespace Pulsar4X.Tests
         public void Init()
         {
             _manager = new EntityManager();
-            _dataBlobTypes = new List<Type>(Assembly.GetExecutingAssembly().GetTypes().Where(type => type.IsSubclassOf(typeof(BaseDataBlob)) && !type.IsAbstract));
+            _dataBlobTypes = new List<Type>(Assembly.GetAssembly(typeof(BaseDataBlob)).GetTypes().Where(type => type.IsSubclassOf(typeof(BaseDataBlob)) && !type.IsAbstract));
         }
 
+        [Test]
+        public void TypeCount()
+        {
+            Assert.AreEqual(_dataBlobTypes.Count, EntityManager.BlankDataBlobMask().Length);
+        }
+
+        [Test]
+        public void DeepCopyConstructor()
+        {
+            foreach (Type dbType in _dataBlobTypes)
+            {
+                ConstructorInfo constructor = dbType.GetConstructor(new []{dbType});
+                if (constructor == null)
+                {
+                    throw new DataBlobTestException(dbType.ToString() + " does not have a Deep Copy constructor.");
+                }
+            }
+        }
 
     }
 }
