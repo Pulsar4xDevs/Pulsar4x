fc532e10a6a657c4755dc6b062d4ea3a6d78aa80
Rod-Serling <jthomson145@gmail.com>
2015-04-13 18:51:38 -0400

    Cleanup of DeepCopy unit test.

--- Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
+++ Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
@@ -10,28 +7,11 @@ using Pulsar4X.ECSLib;
 
 namespace Pulsar4X.Tests
 {
-    class DataBlobTestException : Exception
-    {
-        public DataBlobTestException()
-        {
-        }
-
-        public DataBlobTestException(string message) : base(message)
-        {
-        }
-
-        public DataBlobTestException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
-
     [TestFixture]
     [Description("DataBlob Tests")]
-    class DataBlobTests
+    internal class DataBlobTests
     {
-
-        private static readonly List<Type> DataBlobTypes = new List<Type>(Assembly.GetAssembly(typeof(BaseDataBlob)).GetTypes().Where(type => type.IsSubclassOf(typeof(BaseDataBlob)) && !type.IsAbstract)); 
+        private static readonly List<Type> DataBlobTypes = new List<Type>(Assembly.GetAssembly(typeof(BaseDataBlob)).GetTypes().Where(type => type.IsSubclassOf(typeof(BaseDataBlob)) && !type.IsAbstract));
         private static EntityManager _manager = new EntityManager();
 
         [SetUp]
@@ -46,15 +26,15 @@ namespace Pulsar4X.Tests
             Assert.AreEqual(DataBlobTypes.Count, EntityManager.BlankDataBlobMask().Length);
         }
 
-        [Test, TestCaseSource("DataBlobTypes")]
+        [Test]
+        [TestCaseSource("DataBlobTypes")]
         public void DeepCopyConstructor(Type dataBlobType)
         {
-            ConstructorInfo constructor = dataBlobType.GetConstructor(new []{dataBlobType});
+            ConstructorInfo constructor = dataBlobType.GetConstructor(new[] {dataBlobType});
             if (constructor == null)
             {
-                throw new DataBlobTestException(dataBlobType.ToString() + " does not have a Deep Copy constructor.");
+                Assert.Fail(dataBlobType + " does not have a Deep Copy constructor.");
             }
         }
-
     }
 }
