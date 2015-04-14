c4a83c4bd4c39837f116e20b1c93ae68ad57739a
Jon <jon.se5a@gmail.com>
2015-04-15 09:23:41 +1200

    Added NameDB

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/NameDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsar4X.ECSLib.DataBlobs
+{
+    public class NameDB
+    {
+        /// <summary>
+        /// Each faction can have a different name for whatever entity  has this blob.
+        /// </summary>
+        public Dictionary<Entity, string> Name { get; set; }
+
+        /// <summary>
+        ///  Each faction can have a different name for whatever entity  has this blob.
+        /// </summary>
+        /// <param name="primaryFaction">This entities faction</param>
+        /// <param name="primaryName">The name this faction uses for this entity</param>
+        public NameDB(Entity primaryFaction, string primaryName)
+        {
+            Name = new Dictionary<Entity, string>();
+            Name.Add(primaryFaction, primaryName);
+        }
+
+    }
+}
