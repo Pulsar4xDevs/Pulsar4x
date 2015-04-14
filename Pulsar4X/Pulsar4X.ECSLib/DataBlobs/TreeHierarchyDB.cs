e0052002e0b76980fc9bda1b3f18e39a3a061d14
Rod-Serling <jthomson145@gmail.com>
2015-04-13 21:31:34 -0400

    TreeHierarchyDB return.

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/TreeHierarchyDB.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Pulsar4X.ECSLib
+{
+#if DEBUG
+    public
+#else
+    internal abstract
+#endif
+        class TreeHierarchyDB : BaseDataBlob
+    {
+        public Entity Parent { get; set; }
+
+        [JsonIgnore]
+        public TreeHierarchyDB ParentDB
+        {
+            get
+            {
+                if (Parent == null)
+                    return null;
+
+                return GetSameTypeDB(Parent);
+            }
+        }
+
+        public Entity Root
+        {
+            get
+            {
+                if (Parent == null)
+                {
+                    return OwningEntity;
+                }
+
+                return ParentDB.Root;
+            }
+        }
+
+        [JsonIgnore]
+        public TreeHierarchyDB RootDB
+        {
+            get { return GetSameTypeDB(Root); }
+        }
+
+        public List<Entity> Children { get; set; }
+
+        [JsonIgnore]
+        public List<TreeHierarchyDB> ChildrenDBs
+        {
+            get { return Children.Select(GetSameTypeDB).ToList(); }
+        }
+
+        public override Entity OwningEntity
+        {
+            get { return _owningEntity; }
+            set
+            {
+                if (_owningEntity != null && Parent != null)
+                {
+                    ParentDB.RemoveChild(_owningEntity);
+                }
+
+                _owningEntity = value;
+
+                if (Parent != null)
+                {
+                    ParentDB.AddChild(_owningEntity);
+                }
+            }
+        }
+        private Entity _owningEntity;
+
+        public TreeHierarchyDB(Entity parent)
+        {
+            Parent = parent;
+            Children = new List<Entity>();
+        }
+
+        private void AddChild(Entity child)
+        {
+            Children.Add(child);
+        }
+
+        private bool RemoveChild(Entity child)
+        {
+            return Children.Remove(child);
+        }
+
+        private TreeHierarchyDB GetSameTypeDB(Entity entity)
+        {
+            if (!entity.IsValid)
+            {
+                throw new ArgumentException("Invalid Entity");
+            }
+
+            int typeIndex;
+            EntityManager.TryGetTypeIndex(GetType(), out typeIndex);
+
+            return entity.GetDataBlob<TreeHierarchyDB>(typeIndex);
+        }
+    }
+}
--- Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
+++ Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
@@ -36,5 +36,69 @@ namespace Pulsar4X.Tests
                 Assert.Fail(dataBlobType + " does not have a Deep Copy constructor.");
             }
         }
+
+        [Test]
+        public void TreeHierarchyTest()
+        {
+            Entity rootNode = CreateNode(null);
+            TreeHierarchyDB rootDB = rootNode.GetDataBlob<TreeHierarchyDB>();
+
+            Assert.AreSame(rootDB, rootDB.RootDB);
+
+            Assert.IsNull(rootDB.ParentDB);
+
+            Entity Parent1_node = CreateNode(rootNode);
+            Entity Parent1_child1 = CreateNode(Parent1_node);
+            Entity Parent1_child2 = CreateNode(Parent1_node);
+
+            var Parent1_childEntitys = new List<Entity> { Parent1_child1, Parent1_child2 };
+
+            Entity Parent2_node = CreateNode(rootNode);
+            Entity Parent2_child1 = CreateNode(Parent2_node);
+            Entity Parent2_child2 = CreateNode(Parent2_node);
+
+            var Parent2_childEntitys = new List<Entity> { Parent2_child1, Parent2_child2 };
+
+            TreeHierarchyDB Parent1DB = Parent1_node.GetDataBlob<TreeHierarchyDB>();
+            TreeHierarchyDB Parent1_child1DB = Parent1_child1.GetDataBlob<TreeHierarchyDB>();
+            TreeHierarchyDB Parent1_child2DB = Parent1_child2.GetDataBlob<TreeHierarchyDB>();
+
+            TreeHierarchyDB Parent2DB = Parent2_node.GetDataBlob<TreeHierarchyDB>();
+            TreeHierarchyDB Parent2_child1DB = Parent2_child1.GetDataBlob<TreeHierarchyDB>();
+            TreeHierarchyDB Parent2_child2DB = Parent2_child2.GetDataBlob<TreeHierarchyDB>();
+
+            Assert.AreSame(rootDB, Parent1_child1DB.RootDB);
+            Assert.AreSame(rootDB, Parent2_child2DB.RootDB);
+            Assert.AreSame(Parent1DB, Parent1_child1DB.ParentDB);
+            Assert.AreSame(Parent2DB, Parent2_child1DB.ParentDB);
+
+            var Parent1_children = new List<TreeHierarchyDB> { Parent1_child1DB, Parent1_child2DB };
+            var Parent2_children = new List<TreeHierarchyDB> { Parent2_child1DB, Parent2_child2DB };
+
+            Assert.AreEqual(Parent1DB.Children, Parent1_childEntitys);
+            Assert.AreEqual(Parent2DB.Children, Parent2_childEntitys);
+
+            Assert.AreEqual(Parent1_children, Parent1DB.ChildrenDBs);
+            Assert.AreEqual(Parent2_children, Parent2DB.ChildrenDBs);
+
+            Parent2_child1DB.Parent = Parent1DB.OwningEntity;
+            Assert.AreEqual(Parent1DB.OwningEntity, Parent2_child1DB.ParentDB.OwningEntity);
+
+            Entity root2Node = CreateNode(null);
+            TreeHierarchyDB root2DB = root2Node.GetDataBlob<TreeHierarchyDB>();
+
+            Parent2DB.Parent = root2Node;
+            Assert.AreEqual(root2DB.OwningEntity, Parent2_child2DB.RootDB.OwningEntity);
+
+        }
+
+        private Entity CreateNode(Entity parentEntity)
+        {
+            TreeHierarchyDB nodeDB = new TreeHierarchyDB(parentEntity);
+            Entity nodeEntity = Entity.Create(_manager);
+            nodeEntity.SetDataBlob(nodeDB);
+
+            return nodeEntity;
+        }
     }
 }
