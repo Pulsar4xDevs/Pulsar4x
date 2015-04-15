85b77fb9febb4d522636d100d53c7b4bdb8adc78
intercross21 <intercross21@gmail.com>
2015-04-16 01:02:35 +0400

    Added tests for FactionFactory and ColonyFactory. Moved ShipFactory to FactoryTests.

--- Pulsar4X/Pulsar4X.ECSLib/DataBlobs/NameDB.cs
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/NameDB.cs
@@ -26,7 +26,10 @@ namespace Pulsar4X.ECSLib.DataBlobs
 
         public NameDB(NameDB nameDB)
         {
-            Name = (JDictionary<Entity,string>)nameDB.Name.ToDictionary(entry => entry.Key, entry => entry.Value);
+            //Name = (JDictionary<Entity,string>)nameDB.Name.ToDictionary(entry => entry.Key, entry => entry.Value);
+            Name = new JDictionary<Entity, string>();
+            foreach (KeyValuePair<Entity, string> keyValue in nameDB.Name)
+                Name.Add(keyValue.Key, keyValue.Value);
         }
     }
 }
