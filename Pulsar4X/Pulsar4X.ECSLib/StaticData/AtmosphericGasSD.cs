0ae152073bfed7cfaf48df3d34d896163dcb728c
se5a <jon.se5a@gmail.com>
2015-04-16 08:51:43 +1200

    Somehow AtmoGas disapeared... re-added it.

---
+++ Pulsar4X/Pulsar4X.ECSLib/StaticData/AtmosphericGasSD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsar4X.ECSLib
+{
+    public struct AtmosphericGasSD
+    {
+        public string Name;
+        public string ChemicalSymbol;
+        public bool IsToxic;
+        public double BoilingPoint;
+        public double MeltingPoint;
+   }
+}
