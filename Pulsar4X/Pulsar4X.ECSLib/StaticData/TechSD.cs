b440330ab2ac9c86830ebbb6747af42e2d8c521b
se5a <jon.se5a@gmail.com>
2015-04-16 11:34:25 +1200

    Added initial Technolagy SD and DB.

---
+++ Pulsar4X/Pulsar4X.ECSLib/StaticData/TechSD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pulsar4X.ECSLib
+{
+    public struct TechSD
+    {
+        public string Name;
+        public List<TechSD> Reqirements;
+        public int cost;
+    }
+}
