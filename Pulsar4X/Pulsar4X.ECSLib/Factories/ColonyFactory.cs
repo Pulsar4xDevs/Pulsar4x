01a583f1887bb41a3face04979802bd1f41a35ce
Jon <jon.se5a@gmail.com>
2015-04-15 13:15:53 +1200

    Added ECS FAQ.txt with a new Q and A from IRC. Some fixes to ColonyFactory and FactionFactory.

--- Pulsar4X/Pulsar4X.ECSLib/Factories/ColonyFactory.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Factories/ColonyFactory.cs
@@ -12,16 +12,15 @@ namespace Pulsar4X.ECSLib.Factories
         /// <param name="systemEntityManager"></param>
         /// <param name="factionEntity"></param>
         /// <returns></returns>
-        public static Entity CreateColony(EntityManager systemEntityManager, Entity factionEntity, Entity planetEntity)
+        public static Entity CreateColony(Entity factionEntity, Entity planetEntity)
         {
             List<BaseDataBlob> blobs = new List<BaseDataBlob>();
 
-            Entity colonyEntity = systemEntityManager.CreateEntity(blobs);
-
             NameDB name = new NameDB(factionEntity, "somestring");
-            blobs.Add(name);
             ColonyInfoDB colonyInfoDB = new ColonyInfoDB(Entity.GetInvalidEntity(), 0, planetEntity);
             blobs.Add(colonyInfoDB);
+
+            Entity colonyEntity = Entity.Create(planetEntity.Manager, blobs);
 
             return colonyEntity;
         }
--- Pulsar4X/Pulsar4X.ECSLib/Factories/FactionFactory.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Factories/FactionFactory.cs
@@ -7,15 +7,17 @@ namespace Pulsar4X.ECSLib.Factories
 {
     public static class FactionFactory
     {
-        public static Entity CreateFaction(EntityManager systemEntityManager)
+        public static Entity CreateFaction(EntityManager globalManager, string factionName)
         {
 
-            List<BaseDataBlob> blobs = new List<BaseDataBlob>();       
-            Entity factionEntity = systemEntityManager.CreateEntity(blobs);
-
-            NameDB name = new NameDB(factionEntity, "somestring");
+            List<BaseDataBlob> blobs = new List<BaseDataBlob>();
+            NameDB name = new NameDB(Entity.GetInvalidEntity(), factionName);
             blobs.Add(name);
+            Entity factionEntity = Entity.Create(globalManager, blobs);
 
+            //factionEntity didn't exsist when we created the NameDB, so we have to recreate the name dictionary here.
+            name.Name = new Dictionary<Entity, string>() { { factionEntity, factionName } };
+            
             return factionEntity;
         }
     }
