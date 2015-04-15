5ec817a53fe9118fc5dbbf9cb979607dae20d3f0
Rod-Serling <jthomson145@gmail.com>
2015-04-15 19:17:25 -0400

    New test to ensure datablobs can be deserialized.

--- Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
+++ Pulsar4X/Pulsar4X.Tests/DataBlobTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using Pulsar4X.ECSLib;
 
@@ -35,6 +36,39 @@ namespace Pulsar4X.Tests
             {
                 Assert.Fail(dataBlobType + " does not have a Deep Copy constructor.");
             }
+        }
+
+        [Test]
+        [TestCaseSource("DataBlobTypes")]
+        public void JSONConstructor(Type dataBlobType)
+        {
+            ConstructorInfo[] constructors = dataBlobType.GetConstructors();
+            Attribute jsonConstructorAttribute = new JsonConstructorAttribute();
+
+            foreach (ConstructorInfo constructorInfo in constructors.Where(constructorInfo => constructorInfo.GetCustomAttributes().Contains(jsonConstructorAttribute)))
+            {
+                Assert.Pass(dataBlobType.ToString() + " will deserialize with the constructor marked with [JsonConstructor]");
+            }
+
+            foreach (ConstructorInfo constructorInfo in constructors.Where(constructorInfo => constructorInfo.GetParameters().Length == 0 && constructorInfo.IsPublic))
+            {
+                Assert.Pass(dataBlobType.ToString() + " will deserialize with the default parameterless constructor.");
+            }
+
+            if (constructors.Length == 1)
+            {
+                if (constructors[0].GetParameters().Length != 0)
+                {
+                    Assert.Pass(dataBlobType.ToString() + " will deserialize with the only parameterized constructor available. Make sure parameters match the Json property names saved in the Json file.");
+                }
+            }
+
+            foreach (ConstructorInfo constructorInfo in constructors.Where(constructorInfo => constructorInfo.GetParameters().Length == 0 && constructorInfo.IsPrivate))
+            {
+                Assert.Pass(dataBlobType.ToString() + " will deserialize with the private default parameterless constructor.");
+            }
+            Assert.Fail(dataBlobType.ToString() + " does not have a Json constructor");
+
         }
 
         [Test]
