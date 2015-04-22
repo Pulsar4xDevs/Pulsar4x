514a4dd9946094da81ef860bdbfc949d4e4c850f
Greg Nott <gvnott@gmail.com>
2015-04-22 14:56:41 +1000

    Fixed bug causing techSD not to import correctly.
    Json was again having problems importing into a dictionary. Have solved
    the problem by FORCING a JDictionary to serialize as a list of Key/Value
    pairs.

--- Pulsar4X/Pulsar4X.ECSLib/Helpers/JSONHelper.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Helpers/JSONHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -11,12 +9,12 @@ namespace Pulsar4X.ECSLib
 {
     /// <summary>
     /// The following is a special dictionay. It should be used whenever you have a dictionary 
-    /// that will be serilized/deserilized by json and the key is a custom type.
-    /// This is because by default JSON.net will save the key as a string only, this
-    /// Dictionay forces it to save the full type.
+    /// that will be serilized/deserilized by json.
+    /// This is because by default JSON.net will save the key as a string only or will fail to deserilize, 
+    /// this Dictionay forces it to save the full type (if it is a custom type) or to serilize as a Json array of key/value pairs.
     /// </summary>
     [JsonArrayAttribute]
-    public class JDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    public class JDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializable
     {
         /// <summary>
         /// Initializes a new instance of Pulsar4X.ECSLib.JDicitonary<TKey, TValue> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.
@@ -28,11 +26,29 @@ namespace Pulsar4X.ECSLib
         /// <summary>
         /// Initializes a new instance of the Pulsar4X.ESCLib.JDictionary<TKey, TValue> class that contains elements copied from the specified System.Collections.Generic.IDictionary<TKey, TValue> and uses the default equality comparer for the key type.
         /// </summary>
-        /// <exception cref="System.ArgumentNullException"></exception>
-        /// <exception cref="System.ArgumentException"></exception>
-        /// <remarks>Acts as a deep copy constructor.</remarks>
         public JDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary)
         {
+        }
+
+        /// <summary>
+        /// Deserilizes the dictionary from a json list.
+        /// </summary>
+        public JDictionary(SerializationInfo info, StreamingContext context)
+        {
+            var list = (List<KeyValuePair<TKey, TValue>>)info.GetValue("List", typeof(List<KeyValuePair<TKey, TValue>>));
+
+            foreach (var item in list)
+            {
+                this.Add(item.Key, item.Value);
+            }
+        }
+
+        /// <summary>
+        /// Serializes the Dictionary to a json list.
+        /// </summary>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("List", this.ToList());
         }
     }
 
@@ -50,12 +66,6 @@ namespace Pulsar4X.ECSLib
                 return base.CreateContract(objectType);
             }
 
-            // make sure we use default dictionary import converter 
-            // otherwise import will fail.
-            if (typeof(IDictionary).IsAssignableFrom(objectType))
-            {
-                return base.CreateContract(objectType);
-            }
             // now we can force iserialible to be used.
             if (typeof(ISerializable).IsAssignableFrom(objectType))
             {
--- Pulsar4X/Pulsar4X.ECSLib/StaticData/MineralSD.cs
+++ Pulsar4X/Pulsar4X.ECSLib/StaticData/MineralSD.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 
 namespace Pulsar4X.ECSLib
 {
-    public struct MineralSD : ISerializable
+    public struct MineralSD
     {
         public string Name;
         public string Description;
         public Guid ID;
-        public Dictionary<BodyType, double> Abundance;
-
-        public MineralSD(SerializationInfo info, StreamingContext context)
-        {
-            Name = info.GetString("Name");
-            Description = info.GetString("Description");
-            ID = (Guid)info.GetValue("ID", typeof(Guid));
-
-            var list = (List<KeyValuePair<BodyType, double>>)info.GetValue("Abundance", typeof(List<KeyValuePair<BodyType, double>>));
-
-            Abundance = new Dictionary<BodyType, double>();
-            foreach (var item in list)
-            {
-                Abundance.Add(item.Key, item.Value);
-            }
-        }
-
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("Name", Name);
-            info.AddValue("Description", Description);
-            info.AddValue("ID", ID);
-            info.AddValue("Abundance", Abundance.ToList());
-        }
+        public JDictionary<BodyType, double> Abundance;
     }
 }
