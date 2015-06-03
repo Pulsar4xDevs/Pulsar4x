22d3cbdfaa323ca3f1803ad08f803fec28d7fcfa
Jon <jon.se5a@gmail.com>
2015-06-03 18:43:46 +1200

    fixed installationUC namespace

--- Pulsar4X/ModdingTools.JsonDataEditor/UserControls/InstallationUC.cs
+++ Pulsar4X/ModdingTools.JsonDataEditor/UserControls/InstallationUC.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using Pulsar4X.ECSLib;
+
 namespace ModdingTools.JsonDataEditor.UserControls
 {
     public partial class InstallationUC : UserControl
