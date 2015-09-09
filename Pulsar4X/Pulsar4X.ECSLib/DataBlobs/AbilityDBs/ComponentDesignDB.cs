2cff58a50e00619b211c44df153301ac3592d1ba
Jon <jon.se5a@gmail.com>
2015-09-09 14:14:48 +1200

    little bit of tidying up and moving code around.

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/AbilityDBs/ComponentDesignDB.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCalc;
+
+namespace Pulsar4X.ECSLib
+{
+    public enum GuiHint
+    {
+        GuiSelectionList,
+        GuiSelectionMaxMin,
+        GuiTextDisplay,
+        None
+    }
+    public class ComponentDesignDB
+    {
+        public string Name;
+        public string Description;
+
+        internal ParsingProcessor Parser { get; private set; }
+
+        public GuiHint SizeGuiHint { get; private set; }
+        public int SizeValue { get; private set; }
+        internal Expression SizeFormula { get; set; }
+        public void SetSize(int size)
+        {
+            SetMinSize();
+            SetMinSize();
+            if (size < MinSizeValue)
+                SizeValue = MinSizeValue;
+            if (size > MaxSizeValue)
+                SizeValue = MaxSizeValue;
+            else
+                SizeValue = size;
+        }
+
+        public int MaxSizeValue { get; private set; }
+        internal Expression MaxSizeFormula { get; set; }
+        public void SetMaxSize()
+        {
+            object result = MaxSizeFormula.Evaluate();
+            if (result is int)
+                MaxSizeValue = (int)result;
+            else if (result is double)
+                MaxSizeValue = (int)(double)result;
+        }
+
+        public int MinSizeValue { get; private set; }
+        internal Expression MinSizeFormula { get; set; }
+        public void SetMinSize()
+        {
+            object result = MinSizeFormula.Evaluate();
+            if (result is int)
+                MinSizeValue = (int)result;
+            else if (result is double)
+                MinSizeValue = (int)(double)result;
+        }
+
+        public GuiHint HTKGuiHint { get; private set; }
+        public int HTKValue { get; private set; }
+        internal Expression HTKFormula { get; set; }
+        public void SetHTK()
+        {
+            object result = HTKFormula.Evaluate();
+            if (result is int)
+                HTKValue = (int)result;
+            else if (result is double)
+                HTKValue = (int)(double)result;
+        }
+
+        public GuiHint CrewReqGuiHint { get; private set; }
+        public int CrewReqValue { get; private set; }
+        internal Expression CrewFormula { get; set; }
+        public void SetCrew()
+        {
+            object result = CrewFormula.Evaluate();
+            if (result is int)
+                CrewReqValue = (int)result;
+            else if (result is double)
+                CrewReqValue = (int)(double)result;
+        }
+
+        public GuiHint RessearchGuiHint { get; private set; }
+        public int ResearchCostValue { get; private set; }
+        internal Expression ResearchCostFormula { get; set; }
+        public void SetResearchCost()
+        {
+            object result = ResearchCostFormula.Evaluate();
+            if (result is int)
+                ResearchCostValue = (int)result;
+            else if (result is double)
+                ResearchCostValue = (int)(double)result;
+        }
+
+        public GuiHint MineralCostGuiHint { get; private set; }
+        public JDictionary<Guid, int> MineralCostValues { get; private set; }
+        internal Dictionary<Guid, Expression> CostFormulas { get; set; }
+        public void SetCosts()
+        {
+            MineralCostValues = new JDictionary<Guid, int>();
+            foreach (var kvp in CostFormulas)
+            {
+                object value = kvp.Value.Evaluate();
+                int intvalue = (int)(double)value;
+                MineralCostValues.Add(kvp.Key, intvalue);
+            }
+        }
+
+
+        public List<ComponentDesignAbilityDB> ComponentDesignAbilities;
+
+        public ComponentDesignDB(StaticDataStore staticData, TechDB factionTech)
+        {
+            Parser = new ParsingProcessor(staticData, factionTech, this);
+        }
+    }
+
+    public class ComponentDesignAbilityDB
+    {
+        public string Name;
+        public string Description;
+
+        public GuiHint GuiHint;
+        public Type DataBlobType;
+        //public BaseDataBlob DataBlob;
+
+
+        public Dictionary<TechSD, double> SelectionDictionary;
+
+        public void SetValueFromTechList(TechSD tech)
+        {
+            Value = SelectionDictionary[tech];
+        }
+
+        public Expression Formula { get; set; }
+        public void SetValue()
+        {
+            object result = Formula.Evaluate();
+            if (result is int)
+                Value = (double)(int)result;
+            else
+                Value = (double)result;
+        }
+
+        public void SetValueFromInput(double input)
+        {
+            SetMin();
+            SetMax();
+            if (input < MinValue)
+                Value = MinValue;
+            if (input > MaxValue)
+                Value = MaxValue;
+            else
+                Value = input;
+            Formula = new Expression(Value.ToString()); //this will stop it re-evaluating to default if/when SetValue() is called.
+        }
+
+        public double Value;
+
+        public double MinValue;
+        public Expression MinValueFormula { get; set; }
+        public void SetMin()
+        {
+            object result = MinValueFormula.Evaluate();
+            if (result is int)
+                MinValue = (double)(int)result;
+            else
+                MinValue = (double)result;
+
+
+        }
+        public double MaxValue;
+        public Expression MaxValueFormula { get; set; }
+        public void SetMax()
+        {
+            object result = MaxValueFormula.Evaluate();
+            if (result is int)
+                MaxValue = (double)(int)result;
+            else
+                MaxValue = (double)result;
+        }
+
+    }
+
+}
--- Pulsar4X/Pulsar4X.Tests/ShipComponentTests.cs
+++ Pulsar4X/Pulsar4X.Tests/ShipComponentTests.cs
@@ -42,14 +42,10 @@ namespace Pulsar4X.Tests
         [Test]
         public void TestEngineComponentFactory()
         {
-            ComponentSD engine = EngineFactory.engineasComponentSD();
+            ComponentSD engine = EngineComponentSD();
 
             ComponentDesignDB design = GenericComponentFactory.StaticToDesign(engine, _faction.GetDataBlob<TechDB>(), _game.StaticData);
 
-            //design.SetSize();
-            design.SetCrew();
-            design.SetHTK();
-            design.SetCosts();
             foreach (var ability in design.ComponentDesignAbilities)
             {
                 if (ability.GuiHint == GuiHint.GuiSelectionList)
@@ -66,9 +62,95 @@ namespace Pulsar4X.Tests
                     ability.SetValue();
             }
 
-            Entity engineEntity = GenericComponentFactory.DesignToEntity(_game.GlobalManager, design);
+            Entity engineEntity = GenericComponentFactory.DesignToEntity(_game.GlobalManager, design, _faction.GetDataBlob<TechDB>());
+        }
+        
+        public static ComponentSD EngineComponentSD()
+        {
+            ComponentSD component = new ComponentSD();
+            component.Name = "Engine";
+            component.Description = "Moves a ship";
+            component.ID = new Guid();
 
+            component.SizeGuiHint = GuiHint.GuiSelectionList;
+            component.SizeFormula = "5";
+            component.SizeMaxFormula = "50";
+            component.SizeMinFormula = "1";
 
+            component.HTKGuiHint = GuiHint.GuiTextDisplay;
+            component.BaseHTKFormula = "Min(1, [Size] / 25)";
+
+            component.CrewGuiHint = GuiHint.GuiTextDisplay;
+            component.BaseCrewSizeFormula = "[Size] * 8";
+
+            component.ResearchCostGuiHint = GuiHint.None;
+            component.BaseResearchCostFormula = "[Size] * 10";
+
+            component.CostGuiHint = GuiHint.GuiTextDisplay;
+            component.BaseCostFormula = new JDictionary<Guid, string> { { new Guid("2d4b2866-aa4a-4b9a-b8aa-755fe509c0b3"), "[Size] * 0.016" } };
+
+            component.ComponentAbilitySDs = new List<ComponentAbilitySD>();
+
+            ComponentAbilitySD engineTypeAbility = new ComponentAbilitySD();
+            engineTypeAbility.Name = "Engine Type";
+            engineTypeAbility.Description = "Type of engine Tech";
+            engineTypeAbility.GuiHint = GuiHint.GuiSelectionList;
+            engineTypeAbility.AbilityDataBlobType = null;
+            engineTypeAbility.TechList = new List<Guid>
+            {
+                new Guid("35608fe6-0d65-4a5f-b452-78a3e5e6ce2c"),
+                new Guid("c827d369-3f16-43ef-b112-7d5bcafb74c7"),
+                new Guid("db6818f3-99e9-46c1-b903-f3af978c38b2"),
+                new Guid("f3f10e56-9345-40cc-af42-342e7240355d")
+                //new Guid("58d047e6-c567-4db6-8c76-bfd4a201af94"),
+                //new Guid("bd75bf88-1dad-4022-b401-acdf05ab73f8"),
+                //new Guid("042ce9d4-5a2c-4d8e-9ae4-be059920839c"),
+                //new Guid("93611831-9183-484a-9920-13b39d64e272"),
+                //new Guid("32eda0ab-c117-4224-b148-6c9d0e474296"),
+                //new Guid("cbb1a7ce-3c26-4b5b-abd7-9a99c670d68d"),
+                //new Guid("6e34cc46-0693-4676-b0ca-f076fb36acaf"),
+                //new Guid("9bb4d1c4-680f-4c98-b927-337654073575"),
+                //new Guid("c9587310-f7dd-45d0-ac4c-b6f59a1e1897")
+            };
+            engineTypeAbility.AbilityFormula = "TechData('f3f10e56-9345-40cc-af42-342e7240355d')";
+
+            component.ComponentAbilitySDs.Add(engineTypeAbility);
+
+            ComponentAbilitySD enginePowerEfficency = new ComponentAbilitySD();
+            enginePowerEfficency.Name = "Engine Consumption vs Power";
+            enginePowerEfficency.Description = "More Powerfull engines are less efficent for a given size";
+            enginePowerEfficency.GuiHint = GuiHint.GuiSelectionMaxMin;
+            enginePowerEfficency.AbilityDataBlobType = null;
+            enginePowerEfficency.AbilityFormula = "1";
+            enginePowerEfficency.MaxFormula = "TechData('b8ef73c7-2ef0-445e-8461-1e0508958a0e')";
+            enginePowerEfficency.MinFormula = "TechData('08fa4c4b-0ddb-4b3a-9190-724d715694de')";
+            component.ComponentAbilitySDs.Add(enginePowerEfficency);
+
+            ComponentAbilitySD enginePowerAbility = new ComponentAbilitySD();
+            enginePowerAbility.Name = "Engine Power";
+            enginePowerAbility.Description = "Move Power for ship";
+            enginePowerAbility.GuiHint = GuiHint.None;
+            enginePowerAbility.AbilityDataBlobType = typeof(EnginePowerDB);
+            enginePowerAbility.AbilityFormula = "Ability(0) * [Size]";
+            component.ComponentAbilitySDs.Add(enginePowerAbility);
+
+            ComponentAbilitySD fuelConsumptionBase = new ComponentAbilitySD();
+            fuelConsumptionBase.Name = "Fuel Consumption";
+            fuelConsumptionBase.Description = "From Tech";
+            fuelConsumptionBase.GuiHint = GuiHint.None;
+            fuelConsumptionBase.AbilityDataBlobType = null;
+            fuelConsumptionBase.AbilityFormula = "TechData('8557acb9-c764-44e7-8ee4-db2c2cebf0bc') * Pow(Ability(2), 2.25)";
+            component.ComponentAbilitySDs.Add(fuelConsumptionBase);
+
+            ComponentAbilitySD fuelConsumptionSizeMod = new ComponentAbilitySD();
+            fuelConsumptionSizeMod.Name = "Fuel Consumption";
+            fuelConsumptionSizeMod.Description = "Size Mod";
+            fuelConsumptionSizeMod.GuiHint = GuiHint.GuiTextDisplay;
+            fuelConsumptionSizeMod.AbilityDataBlobType = typeof(FuelUseDB);
+            fuelConsumptionSizeMod.AbilityFormula = "Ability(2) - Ability(2) * [Size] * 0.002";
+            component.ComponentAbilitySDs.Add(fuelConsumptionSizeMod);
+
+            return component;
         }
     }
 }
