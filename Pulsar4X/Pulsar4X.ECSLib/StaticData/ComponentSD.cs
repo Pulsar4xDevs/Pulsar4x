9eb74413eb320dd0ae33430fc52060cdbcaafb5b
Jon <jon.se5a@gmail.com>
2015-09-17 17:52:31 +1200

    something didn't commit properly

--- Pulsar4X/Pulsar4X.ECSLib/StaticData/ComponentSD.cs
+++ Pulsar4X/Pulsar4X.ECSLib/StaticData/ComponentSD.cs
@@ -2,23 +2,7 @@
 using System.Collections.Generic;
 
 namespace Pulsar4X.ECSLib
-{
-    [StaticDataAttribute(false)]
-    public struct ComponentAbilitySD
-    {
-        public string Name;
-        public string Description;
-
-        public AbilityType Ability;
-        public List<float> AbilityAmount;
-        public List<float> CrewAmount;
-        public List<float> WeightAmount;
-        public AbilityType AffectsAbility;
-        public List<float> AffectedAmount;
-        public List<Guid> TechRequirements;
-    }
-
-   
+{  
     [StaticDataAttribute(true, IDPropertyName = "ID")]
     public struct ComponentSD
     {
@@ -26,6 +10,47 @@ namespace Pulsar4X.ECSLib
         public string Description;
         public Guid ID;
 
+        public string SizeFormula;
+        public GuiHint SizeGuiHint;
+
+        public string HTKFormula;
+        public GuiHint HTKGuiHint;
+
+        public string CrewReqFormula;
+        public GuiHint CrewReqGuiHint;
+
+        public JDictionary<Guid,string> MineralCostFormula;
+        public GuiHint MineralCostGuiHint;
+
+        public string ResearchCostFormula;
+        public GuiHint ResearchCostGuiHint;
+
+        public string CreditCostFormula;
+        public GuiHint CreditCostGuiHint;
+
+        //if it can be fitted to a ship as a ship component, on a planet as an installation, can be cargo etc.
+        public JDictionary<ComponentMountType, bool> MountType; 
+
         public List<ComponentAbilitySD> ComponentAbilitySDs;
+    }
+
+    [StaticDataAttribute(false)]
+    public struct ComponentAbilitySD
+    {
+        public string Name;
+        public string Description;
+        public GuiHint GuiHint;
+
+        //used if guihint is GuiSelectionList
+        public JDictionary<Guid, string> GuidDictionary;
+
+        //used if GuiHint is GuiMinMax
+        public string MaxFormula;
+        public string MinFormula;
+
+        //if guihint is selection list or minmax, this should point to a default value. 
+        public string AbilityFormula;
+
+        public string AbilityDataBlobType;
     }
 }
