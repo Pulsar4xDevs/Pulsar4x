d637cc99c599e60a7f205d2bf566843897af2fa5
Jon <jon.se5a@gmail.com>
2015-09-30 08:20:57 +1300

    Added New Project for ViewModels.

---
+++ Pulsar4X/ViewModelLib/ViewModels/StarVM.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Pulsar4X.ECSLib;
+
+namespace Pulsar4X.WPFUI.ViewModels
+{
+    public class StarVM : IViewModel
+    {
+        public Entity Entity { get; set; }
+
+        #region Children And Parents
+        // these are the stars children, if any. Mostly used for rendering and/or navigating to children.
+
+        private BindingList<StarVM> _childStars;
+        public BindingList<StarVM> ChildStars { get { return _childStars; }}
+
+
+        private BindingList<PlanetVM> _childPlanets;
+        public BindingList<PlanetVM> ChildPlanets { get { return _childPlanets;}} 
+
+        // the stars parent system and star (if it is not the root/primary star of the system) 
+        private SystemVM _system;
+
+        public StarVM ParentStar
+        {
+            get
+            {
+                if (_parentStar == null && _parentStarGuid != null)
+                    _parentStar = App.Current.GameVM.GetSystem(Entity.Guid).GetStar((Guid)_parentStarGuid);
+                return _parentStar;
+            }
+            set
+            {
+                _parentStar = value;
+                OnPropertyChanged();
+            }
+        }
+        private StarVM _parentStar;
+        private Guid? _parentStarGuid;
+
+        #endregion
+
+        #region Stars Properties
+
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public Guid ID { get { return Entity.Guid; } }
+
+        private string _starClass;
+
+        private int _age;
+
+        private double _ecoSphereRadius;
+
+        private double _luminosity;
+
+        private int _temperature;
+
+        // in solar masses would be best.
+        private double _mass;
+
+        private double _density;
+
+        private double _radius;
+
+        private double _volume;
+
+        private double _surfaceGravity;
+
+        private double _semiMajorAxis;
+
+        private double _apoapsis;
+
+        private double _periapsis;
+
+        private double _eccentricity;
+
+        private double _inclination;
+
+        private TimeSpan _year;
+
+        /// <summary>
+        /// Ralitive to parent orbit.
+        /// </summary>
+        public Vector4 Position
+        {
+            get { return _position; }
+            private set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+        private Vector4 _position;
+
+        /// <summary>
+        /// 2d cartisian position ralitive to ancestor star (ancestor should be 0,0). 
+        /// </summary>
+        public Vector SystemPosition
+        {
+            get
+            {
+                Vector parentPos = new Vector();
+                if (ParentStar != null)
+                {
+                    parentPos = ParentStar.SystemPosition;
+                }
+                return Conversions.VectorFromVector4(Position) + parentPos;
+            }
+        }
+
+        #endregion
+
+        #region Functions
+
+        #region Construction
+
+        private StarVM(Entity entity)
+        {
+            Entity = entity;
+            _name = entity.GetDataBlob<NameDB>().DefaultName;
+        }
+
+        /// <summary>
+        /// Creates and fills out the properties of this ViewModel from the provided entity.
+        /// </summary>
+        public static StarVM Create(Entity entity, SystemVM systemVM)
+        {
+            StarVM newVM = new StarVM(entity);
+
+            // Initialize the data.
+            newVM.Init(systemVM);
+
+            newVM.Refresh();
+
+            return newVM;
+        }
+
+        /// <summary>
+        /// Creates and fills out the properties of this ViewModel from the entity with the provided Guid.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Cannot create a Planet ViewModel without an initialized game.</exception>
+        /// <exception cref="GuidNotFoundException">Thrown when the supplied Guid is not found in the game.</exception>
+        internal static StarVM Create(Guid guid, SystemVM systemVM)
+        {
+            if (App.Current.Game == null)
+            {
+                throw new InvalidOperationException("Cannot create a StarVM without an initialized game.");
+            }
+
+            Entity entity;
+            if (!App.Current.Game.GlobalManager.FindEntityByGuid(guid, out entity))
+            {
+                throw new GuidNotFoundException(guid);
+            }
+
+            return Create(entity, systemVM);
+        }
+
+        #endregion
+
+
+        #region IViewModel
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void Refresh(bool partialRefresh = false)
+        {
+            _position = Entity.GetDataBlob<PositionDB>().Position;   
+            
+        }
+
+        public void Init(SystemVM systemVM)
+        {
+            _system = systemVM;
+            _childStars = new BindingList<StarVM>();
+            _childPlanets = new BindingList<PlanetVM>();
+            if (Entity.GetDataBlob<OrbitDB>().Parent != null)
+                _parentStarGuid = Entity.GetDataBlob<OrbitDB>().Parent.Guid;
+            foreach (var childOrbit in Entity.GetDataBlob<OrbitDB>().Children)
+            {
+                if(childOrbit.HasDataBlob<StarInfoDB>())
+                    _childStars.Add(_system.GetStar(childOrbit.Guid));
+                else if(childOrbit.HasDataBlob<SystemBodyDB>())
+                    _childPlanets.Add(_system.GetPlanet(childOrbit.Guid));
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ Pulsar4X/ViewModelLib/ViewModels/SystemVM.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Pulsar4X.ECSLib;
+
+namespace Pulsar4X.WPFUI.ViewModels
+{
+    public class SystemVM
+    {
+        private BindingList<StarVM> _stars;
+        public BindingList<StarVM> Stars { get { return _stars;} }
+        public List<StarVM> StarList { get { return _starDictionary.Values.ToList(); } } 
+        private StarVM _parentStar;
+        public StarVM ParentStar { get { return _parentStar;} }
+
+        private BindingList<PlanetVM> _planets;
+        public BindingList<PlanetVM> Planets { get { return _planets;} }
+        public List<PlanetVM> PlanetList { get { return _planetDictionary.Values.ToList(); } } 
+        private string _name;
+        public string Name { get { return _name;} }
+
+        public Guid ID { get; set; }
+
+        public StarSystem StarSystem { get; set; }
+        // add list of ships
+
+        // add list of waypoints
+
+        // Add list of colonies? maybe?
+        private Dictionary<Guid, StarVM> _starDictionary;
+
+        private Dictionary<Guid, PlanetVM> _planetDictionary;
+
+        /// <summary>
+        /// returns the StarVM for a given bodies guid.
+        /// </summary>
+        /// <param name="bodyGuid"></param>
+        /// <returns></returns>
+        internal StarVM GetStar(Guid bodyGuid)
+        {
+            Entity bodyEntity;
+            Guid starGuid = new Guid();
+            if (_starDictionary.ContainsKey(bodyGuid))
+                starGuid = bodyGuid;
+
+            else if (App.Current.Game.GlobalManager.FindEntityByGuid(bodyGuid, out bodyEntity))
+            {
+                if (bodyEntity.HasDataBlob<StarInfoDB>())
+                {
+                    starGuid = bodyEntity.Guid;
+                }
+            }
+            else throw new GuidNotFoundException(bodyGuid);
+
+            if (!_starDictionary.ContainsKey(starGuid))
+            {
+                StarVM starVM = StarVM.Create(starGuid, this);
+                _starDictionary.Add(starGuid, starVM);
+                if(!_stars.Contains(starVM))
+                    _stars.Add(starVM);
+            }
+            return _starDictionary[starGuid];
+        }
+
+        internal PlanetVM GetPlanet(Guid bodyGuid)
+        {
+            Entity bodyEntity;
+            Guid planetGuid = new Guid();
+            if (_planetDictionary.ContainsKey(bodyGuid))
+                planetGuid = bodyGuid;
+            else if (App.Current.Game.GlobalManager.FindEntityByGuid(bodyGuid, out bodyEntity))
+            {
+                if (bodyEntity.HasDataBlob<SystemBodyDB>())
+                {
+                    planetGuid = bodyEntity.Guid;
+                }
+            }
+            else throw new GuidNotFoundException(bodyGuid);
+
+            if (!_planetDictionary.ContainsKey(planetGuid))
+            {
+                PlanetVM planetVM = PlanetVM.Create(planetGuid);
+                if (!_planets.Contains(planetVM))
+                    _planets.Add(planetVM);
+                _planetDictionary.Add(planetGuid, planetVM);
+            }
+            return _planetDictionary[planetGuid];
+        }
+
+        #region IViewModel
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region Construction
+
+        private SystemVM(StarSystem starSystem)
+        {
+            _name = starSystem.NameDB.DefaultName;
+            StarSystem = starSystem;
+            _stars = new BindingList<StarVM>();
+            _planets = new BindingList<PlanetVM>();
+            _starDictionary = new Dictionary<Guid, StarVM>();
+            _planetDictionary = new Dictionary<Guid, PlanetVM>();
+            //find most massive star, this is the parent.
+            Entity parentStar = starSystem.SystemManager.GetFirstEntityWithDataBlob<StarInfoDB>();
+            StarVM parentstarVM = StarVM.Create(parentStar, this);
+            foreach (var star in starSystem.SystemManager.GetAllEntitiesWithDataBlob<StarInfoDB>())
+            {
+                StarVM starVM = StarVM.Create(star, this);
+                if(!_stars.Contains(starVM))
+                    _stars.Add(starVM);
+                if(!_starDictionary.ContainsKey(star.Guid))
+                    _starDictionary.Add(star.Guid, starVM);
+                if (star.GetDataBlob<MassVolumeDB>().Mass > parentStar.GetDataBlob<MassVolumeDB>().Mass)
+                {
+                    parentStar = star;
+                    parentstarVM = starVM;
+                }
+            }
+            _parentStar = parentstarVM;
+            ID = _parentStar.Entity.Guid;
+            foreach (var planet in starSystem.SystemManager.GetAllEntitiesWithDataBlob<SystemBodyDB>())
+            {
+                PlanetVM planetVM = PlanetVM.Create(planet);
+                if (!_planets.Contains(planetVM))
+                    _planets.Add(planetVM);
+                if(!_planetDictionary.ContainsKey(planet.Guid))
+                    _planetDictionary.Add(planet.Guid,planetVM);
+               
+            }
+        }
+
+        /// <summary>
+        /// Creates and fills out the properties of this ViewModel from the entity with the provided Guid.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Cannot create a Planet ViewModel without an initialized game.</exception>
+        /// <exception cref="GuidNotFoundException">Thrown when the supplied Guid is not found in the game.</exception>
+        internal static SystemVM Create(Guid guid)
+        {
+            if (App.Current.Game == null)
+            {
+                throw new InvalidOperationException("Cannot create a StarVM without an initialized game.");
+            }
+
+            Entity entity;
+            if (!App.Current.Game.GlobalManager.FindEntityByGuid(guid, out entity))
+            {
+                throw new GuidNotFoundException(guid);
+            }
+            StarSystem starSystem = null;
+            
+            for (int i = 0; i < App.Current.Game.Systems.Count; i++)
+            {
+                if (App.Current.Game.Systems[i].SystemManager.FindEntityByGuid(guid, out entity))
+                {
+                    starSystem = App.Current.Game.Systems[i];
+                    i = App.Current.Game.Systems.Count;
+                }
+            }            
+            return Create(starSystem);
+        }
+
+        /// <summary>
+        /// Creates and fills out the properties of this ViewModel from the provided entity.
+        /// </summary>
+        public static SystemVM Create(StarSystem starSystem)
+        {
+            SystemVM newVM = new SystemVM(starSystem);
+
+            // Initialize the data.
+            newVM.Refresh();
+
+            return newVM;
+        }
+
+
+        #endregion
+
+        public void Refresh(bool partialRefresh = false)
+        {
+            foreach (var star in StarList)
+            {
+                star.Refresh();
+            }
+            foreach (var planet in PlanetList)
+            {
+                planet.Refresh();
+            }
+        }
+    }
+}
