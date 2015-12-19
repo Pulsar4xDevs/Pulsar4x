860c71376337692f44620cb078ac151424292b8b
se5a <jon.se5a@gmail.com>
2015-12-19 19:59:56 +1300

    Some initial component design view work

---
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Views/ComponentDesignView.xeto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+using Pulsar4X.ViewModel;
+
+namespace Pulsar4X.CrossPlatformUI.Views
+{
+    public class ComponentDesignView : Panel
+    {
+        protected ComboBox ComponentSelection {get;set;}
+        protected DynamicLayout AbilitysLayout {get;set;}
+        protected TextBox AbilityStats { get; set; }
+        protected TextBox ComponentStats { get; set; }
+        protected Button Create { get; set; }
+        protected TextBox Name { get; set; }
+
+
+
+        public ComponentDesignView()
+        {
+            XamlReader.Load(this);
+        }
+        public ComponentDesignView(ComponentDesignVM viewmodel) :this()
+        {
+           
+        
+        }
+    }
+}
