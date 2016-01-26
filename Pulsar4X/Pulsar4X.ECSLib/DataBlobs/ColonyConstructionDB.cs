b1866213bb6a4ba6020288412d3e7a2bf7f46e3f
Jon <jon.se5a@gmail.com>
2016-01-27 08:04:02 +1300

    remove an unused using reference that appveyor was complaining about

--- Pulsar4X/Pulsar4X.ECSLib/DataBlobs/ColonyConstructionDB.cs
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/ColonyConstructionDB.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Subjects;
-using System.Runtime.InteropServices.ComTypes;
 using Newtonsoft.Json;
 
 namespace Pulsar4X.ECSLib
