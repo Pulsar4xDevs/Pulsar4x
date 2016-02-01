0ff68c992747d03e52a698e08204b9d99888fcf3
firefly2442 <firefly2442@gmail.com>
2016-02-01 15:45:39 -0600

    update copyright year in projects

--- Pulsar4X/Pulsar4X.CrossPlatformUI/Properties/AssemblyInfo.cs
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
