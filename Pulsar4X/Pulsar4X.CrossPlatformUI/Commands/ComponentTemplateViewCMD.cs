607f9f0b548d5203b7c8d7c1ce39c2908266908c
Jon <jon.se5a@gmail.com>
2016-02-12 14:30:40 +1300

    two files which VS neglected to add last commit.

---
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Commands/ComponentTemplateViewCMD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Forms;
+using Eto.Drawing;
+using Pulsar4X.ViewModel;
+
+namespace Pulsar4X.CrossPlatformUI.Commands
+{
+    class ComponentTemplateViewCMD : Command
+        {
+            private GameVM GameData;
+            public ComponentTemplateViewCMD()
+            {
+                ID = "ComponentTemplateViewCMD";
+                Image = Icon.FromResource("Pulsar4X.CrossPlatformUI.Resources.Icons.ColonyView.ico");
+                MenuText = "Component Template Design View";
+                ToolBarText = "Component Template Design View";
+                //Shortcut = Keys.F5;
+                //GameData = GameVM;
+            }
+
+            protected override void OnExecuted(EventArgs e)
+            {
+                base.OnExecuted(e);
+
+                ComponentTemplateVM designVM = new ComponentTemplateVM();//ComponentTemplateVM.Create(GameData);
+                Application.Instance.MainForm.Content = new Views.ComponentTemplateDesignerView(designVM);
+            }
+        }
+    }
