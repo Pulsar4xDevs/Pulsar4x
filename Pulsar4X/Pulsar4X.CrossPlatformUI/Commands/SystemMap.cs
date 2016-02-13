fd6a0b4cbddfe2e13ca877864ccce4e7f91197a5
Rod Serling <ScottPhelps.usn@gmail.com>
2016-02-12 23:03:19 -0600

    AutoUpdate SystemMap command

--- Pulsar4X/Pulsar4X.CrossPlatformUI/Commands/SystemMap.cs
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Commands/SystemMap.cs
@@ -2,26 +2,38 @@
 using Eto.Forms;
 using Pulsar4X.ViewModel;
 using System;
+using System.ComponentModel;
 
 namespace Pulsar4X.CrossPlatformUI.Commands
 {
-    class SystemMap : Command
+    sealed class SystemMap : Command
     {
-        private GameVM GameData;
-        public SystemMap(GameVM GameVM)
+        private GameVM _gameVM;
+
+        public SystemMap(GameVM gameVM)
         {
+            _gameVM = gameVM;
             ID = "SystemMap";
             Image = Icon.FromResource("Pulsar4X.CrossPlatformUI.Resources.Icons.SystemMap.ico");
             MenuText = "System Map";
             ToolBarText = "System Map";
             //Shortcut = Keys.F5;
-            GameData = GameVM;
+            _gameVM.PropertyChanged += _gameVM_PropertyChanged;
+            Enabled = _gameVM.HasGame;
+        }
+
+        private void _gameVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "HasGame")
+            {
+                Enabled = _gameVM.HasGame;
+            }
         }
 
         protected override void OnExecuted(EventArgs e)
         {
             base.OnExecuted(e);
-            Application.Instance.MainForm.Content = new Views.SystemView(GameData);
+            Application.Instance.MainForm.Content = new Views.SystemView(_gameVM);
         }
     }
 }
