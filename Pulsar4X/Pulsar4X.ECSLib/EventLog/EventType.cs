a09476619d6bf7d452ed772028f7e0e158b9f465
Rod Serling <ScottPhelps.usn@gmail.com>
2016-02-24 14:31:37 -0600

    Events sorted by function

--- Pulsar4X/Pulsar4X.ECSLib/EventLog/EventType.cs
+++ Pulsar4X/Pulsar4X.ECSLib/EventLog/EventType.cs
@@ -4,27 +4,148 @@ namespace Pulsar4X.ECSLib
     public enum EventType
     {
         NoEventType,
-        Production,
+
+        #region Shipyard Events
+
         ShipConstruction,
         ShipRepair,
         ShipRefit,
-        ShipReactivation,
         ShipOverhaul,
         ShipScrapped,
-        ShipMothballed,
+        
+        ShipyardModified,
+        OverhaulComplete,
+        OverhaulAbandoned,
+
+        SlipwayDestroyed,
+        ShipyardDestroyed,
+
+        #endregion
+
+        #region Population Events
+        
+        PlanetBombarded,
+        CollateralDamage,
+        MassDriverAttack,
+
+        POWRescued, // Friendly POW's returned to population
+
+        // Political Status Events
+        PopulationSurrender,
+        PopStatusChange,
+        UnrestIncreasing,
+        UnrestDecreasing,
+
+        // Mining Events
+        MineralExhausted,
+        AllMineralsExhausted,
+        NewMineralDeposit,
+        AccessibilityIncrease,
+        MineralsLocated,
+        GeoSurveyComplete,
+
+        // Research Events
+        ResearchStarted,
+        ResearchCompleted,
+        InactiveLab,
+        OverallocationOfLabs,
+        ReductionInLabs,
+
+        // Industry Events
+        Production,
+        BombardmentLosses,
+        MineralShortage,
+        ComponentsScrapped,
+        FightersScrapped,
+        FuelShortage,
+        CivilianMiningColony,
+        ProductionQueueEmpty,
+
+        // Ground Unit Events
         UnitTrained,
+        GroundCombat,
+        GroundUnitLost,
+        GroundUnitDelivered,
+        MoraleIncrease,
+        MoraleLoss,
+        ReplacementsExpended,
+        EnemyCasualties,
+        ReadinessLoss,
+
+        // Environment Events
+        TerraformingReport,
+        GasRemoved,
+        IceSheetMelted,
+        RadiationIncrease,
+        BreathableAtmosphere,
+
+        // Xenology Events
+        MineRestored,
+        FactoryRestored,
+        RuinsExploited,
+        TechDiscovered,
+        RuinsLocated,
+        AnomalyDiscovered,
+        RoboticGuardians,
+        TechDataLearned,
+
+        #endregion
+
+        #region Leader events
+
+        TeamSkill,
         OfficerUpdate,
-        ShieldRechage,
+        PickupFailed,
+        DropoffFailed,
+        CommanderDelivered,
+        OfficerPromoted,
+        OfficerHealth,
+        CommandAssignment,
+
+        OutstandingNewOfficer,
+        ExceptionalNewOfficer,
+        PromisingNewOfficer,
+        NewOfficer,
+
+        NewAdministrator,
+        NewScientist,
+
+        #endregion
+
+        #region Ship Combat
+
+        ShipSurrender,
+        CrewGradeIncrease,
+
+        // Targeting Events
         Targeting,
         TargetingProblemm,
-        MissileLaunch,
-        OutOfAmmo,
-        NoMissileAssigned,
-        WeaponReloading,
         ChanceToHit,
         TargetHit,
         TargetMissed,
+        NoMissileAssigned,
+        FireDelay,
+        FiringSummary,
+        MissilesDestroyed,
+        MissileIntercepted,
+        InterceptionSummary,
+        MissileSelfDestruct,
+
+        // Weapon Events
         WeaponRecharging,
+        WeaponReloading,
+        SecondStageRelease,
+        MissileLaunch,
+        OutOfAmmo,
+
+        // Shield Events
+        ShieldsInactive,
+        ShieldsDown,
+        ShieldRechage,
+        ShieldFailure,
+        ShieldDeactivation,
+
+        // Damage Events
         DamageAbsorbed,
         SystemDestroyed,
         SystemIntact,
@@ -32,155 +153,98 @@ namespace Pulsar4X.ECSLib
         ShipDestroyed,
         Damage,
         ShieldDamage,
-        ShieldsInactive,
-        ShieldsDown,
-        PlanetBombarded,
         ShipSlowed,
-        TreatyAgreed,
-        TeamSkill,
-        TechExchange,
-        SystemSurveyed,
-        GovernmentChange,
-        AlienFleets,
-        AlienPopulation,
-        OrdersAssigned,
-        OrdersNotPossible,
-        MineralsLocated,
-        RuinsLocated,
-        OrdersCompleted,
-        InsufficientFuel,
-        GroupDestroyed,
-        UnableToLand,
-        GroupLanded,
-        FighterCasualties,
-        Selfdestruct,
-        TargetLost,
-        MissilesLost,
-        TransitFailure,
-        Communication,
-        TechDiscovered,
-        RuinsExploited,
-        NoSpareParts,
-        SuccessfulRepair,
-        ResearchCompleted,
-        TechRemoved,
-        SystemFailure,
-        InactiveLab,
-        MineralShortage,
-        LowFuel,
-        MiuneralExhausted,
-        NewThermalContact,
-        IonStormAhead,
-        FuelExhausted,
-        InternalDamage,
         TargetDestroyed,
-        GroundCombat,
-        GroundUnitLost,
-        GasRemoved,
-        TerraformingReport,
-        DestroyedByStorm,
-        RadiationIncrease,
-        HarvesterCapacity,
-        IceSheetMelted,
-        MoraleIncrase,
-        PickupFailed,
-        DropoffFailed,
-        CommanderDelivered,
-        GroundUnitDelivered,
-        JumpGateUnderway,
-        ResearchStarted,
-        HyperLimitWarning,
-        OfficerPromoted,
-        ConditionalOrder,
-        LifePodExpired,
-        SearchAndRescue,
-        SecondStageRelease,
-        ShieldFailure,
         SystemFailureAlert,
         SystemDamaged,
-        NewOfficer,
-        OfficerHealth,
-        MineRestored,
-        FactoryRestored,
-        BombardmentLosses,
-        ConvoyArrival,
-        Sale,
-        Purchase,
-        IntelligenceUpdate,
-        FleetMessage,
-        WreckSalvaged,
-        MassDriverAttack,
-        ConvoyCreated,
-        ComponentsScrapped,
-        FightersScrapped,
-        ConditionalOrderFailure,
-        GroundUnitDest,
-        PopStatusChange,
-        InsufficientJGC,
-        MessageContinued,
-        TrainingReset,
-        FireDelay,
-        FullSpares,
-        MaintenanceZero,
-        OverhaulComplete,
-        PopulationSurrender,
-        OverhaulAbandoned,
-        ShipyardModified,
-        NegotiationModifier,
-        TechDownloaded,
-        TechDataScanned,
-        CommandAssignment,
-        FiringSummary,
-        TeamDelivered,
-        NoDamage,
-        CivilianConstruction,
-        FireControlLockLost,
-        FuelShortage,
-        AlienClassScanned,
-        HostileTransitDetected,
-        JumpPointDetected,
-        CivilianActivity,
-        IllegalOrder,
-        NewMineralDeposit,
-        AccessibilityIncrease,
-        GeoSurveyComplete,
-        AlienShipRefitted,
-        NoFreighterOverhaul,
+        SystemFailure,
+        PowerExplosionDetected,
+        MgExplosionDetected,
+        ShieldExpDetected,
+        InternalDamage,
+        RammingAttempt,
+        ShockDamage,
+        EnemyShipDestroyed,
+        ShieldPenetrated,
+
+        // Boarding/Troop Deployment
+        BoardingAttempt,
+        BoardingCombat,
+        CrewLosses,
+        ShipCaptured,
+        PrisonersTaken,
+        CombatDrop,
+
+
+
+        #endregion
+
+        // Diplomatic Events
+        TreatyAgreed,
+        TechExchange,
+        Communication,
+        Diplomacy,
+        Reparations,
+        
+        // Ship State Events
+        InsufficientFuel,
+        UnableToLand,
+        Selfdestruct,
+        TransitFailure,
+        NoSpareParts,
+        SuccessfulRepair,
+        LowFuel,
+        FuelExhausted,
         MaintenanceProblem,
+        HarvesterCapacity,
+        WreckSalvaged,
+        SalvageFailed,
+        LoadingProblem,
+        LifeSupportFailure,
+        TractorReleased,
+        // Crew Morale Events
+        CrewMoraleFalling,
+        ShoreLeaveComplete,
+
+        // Order Events
+        ConditionalOrder,
+        ConditionalOrderFailure,
+        OrdersAssigned,
+        OrdersNotPossible,
+        OrdersCompleted,
+
+        // Life Pod Events
+        LifePodExpired,
+        SearchAndRescue,
+        
+        // Intelligence Events
+        IntelligenceUpdate,
         NewAlienRace,
-        UnrestIncreasing,
-        UnrestDecreasing,
-        ShipForSale,
-        PDCLocated,
-        CollateralDamage,
-        WeaponIneffective,
-        ReadinessLoss,
-        MoraleIncrease,
-        MoraleLoss,
+        NewHostileClass,
+        NewHostileShip,
+
+        // Utility Events
+        FleetMessage,
+        MessageContinued,
+        IncrementAdjustment,
+        ProgramError,
+        Opps,
+        
+        // Civilian Events
+        CivilianActivity,
+        CivilianConstruction,
+        NewShippingLine,
+
+        // Team Events
+        TeamDelivered,
+        TeamDisbanded,
         SuccessfulEspionage,
         TeamKilled,
         TeamCaptured,
         EnemyAgentsKilled,
         EnemyAgentsCaptured,
-        UnsuitablePlanet,
-        MissilesInNebula,
-        TargetUndamaged,
-        MissilesDestroyed,
-        SaleOfferWithdrawn,
-        SlipwayDestroyed,
-        ShipyardDestroyed,
-        IncrementAdjustment,
-        Fire,
-        FireContained,
-        ExplosionDetected,
-        Reparations,
-        PlanetLooted,
-        InterceptionSummary,
-        MissileIntercepted,
-        Overkill,
-        ShipSurrender,
-        BreathableAtmosphere,
-        Diplomacy,
+        
+        // Sensor Events
         NewHostileContact,
         NewNeutralContact,
         NewFriendlyContact,
@@ -188,87 +252,121 @@ namespace Pulsar4X.ECSLib
         MissileContact,
         WreckContact,
         MineralPacketContact,
-        JumpGateDetected,
         GroundForcesContact,
         ShipyardContact,
         CivilianContact,
-        SalvageFailed,
-        LoadingProblem,
-        NewJumpConnection,
-        NewSystemDiscovered,
-        ProgramError,
+        HostileTransitDetected,
+        ExplosionDetected,
         ShieldsDetected,
         ActiveSensorDetected,
         PopulationDetected,
-        MissileSelfDestruct,
-        OverhaulClockReduced,
         ActiveContactLost,
         ThermalContactLost,
-        GPDContactLost,
-        ReplacementsExpended,
         HostileContactUpdate,
         NeutralContactUpdate,
         FriendlyContactUpdate,
         AlliedContactUpdate,
         CivilianContactUpdate,
-        CivilianMiningColony,
-        NewShippingLine,
-        ProductionQueueEmpty,
+        EnergyImpactDetected,
+        NewThermalContact,
+        PDCLocated,
+        TargetLost,
+        MissilesLost,
+        NewWreck,
+        WreckDisappeared,
+
+        // Jump Point/Transit Events
+        NewJumpConnection,
+        NewSystemDiscovered,
         JumpPointFound,
-        OutstandingNewOfficer,
-        ExceptionalNewOfficer,
-        PromisingNewOfficer,
-        GeologicalSurveyData,
-        BoardingAttempt,
-        BoardingCombat,
-        CrewLosses,
-        ShipCaptured,
-        CombatDrop,
-        WreckComponents,
-        PrisonersTaken,
-        AllMineralsExhausted,
-        RammingAttempt,
+
+        // Empire Events
         EmpireInDebt,
-        OverallocationOfLabs,
-        ReductionInLabs,
-        ItsLifeJump,
-        NewAliencClass,
-        NewAlienShip,
-        TeamDisbanded,
-        CrewGradeIncrease,
-        EnemyCasualties,
-        ShieldDeactivation,
-        TechDataLearned,
+        SystemSurveyed,
+
+        // Wormhole Events
         NewStableWormhole,
         WormholeMoved,
         WormholeDisappeared,
-        ShieldPenetrated,
-        FighterAssigned,
-        RoboticGuardians,
-        TractorReleased,
-        ParasiteLauncherReady,
-        FireControlPain,
-        Opps,
-        NewReck,
-        WreckDisappeared,
-        EnemyShipDestroyed,
-        CrewMoraleFalling,
-        ShoreLeaveComplete,
-        LifeSupportFailure,
-        ProbeOutOfFuel,
-        NewAdministrator,
-        NewScientist,
-        POWRescued,
-        ShockDamage,
-        AnomalyDiscovered,
-        EnergyImpactDetected,
-        PowerExplosionDetected,
-        MgExplosionDetected,
-        ShieldExpDetected,
+        
+        #region Unknown/Depreciated/Unused
+
+        GeologicalSurveyData, // Depreciated in favor of Mineral Deposit found/accessability increased?
+
+        // Unused Damage events?
+        TargetUndamaged,
+        WeaponIneffective,
+        Overkill,
+        WreckComponents,
+        NoDamage,
+        // Non-implemented/depreciated shipboard fire mechanic?
+        Fire,
+        FireContained,
+        // Non-implemented/depreciated ship exchange?
+        SaleOfferWithdrawn,
+        ShipForSale,
+        Sale,
+        Purchase,
+
+        // Non-implemented/depreciated convoy feature? Pre-taskgroup?
+        ConvoyCreated,
+        ConvoyArrival,
+
+        // Weather events? Are these used in Nebulas?
+        IonStormAhead,
+        MissilesInNebula,
+        DestroyedByStorm,
+
+        // Possible fighter-specific messages. May be depreciated.
+        GroupLanded,
+        GroupDestroyed,
+        FighterCasualties,
+
+        // Likely background events. Never displayed to the player.
         TimeCheck,
         InvalidUnloadSystem,
-        NewHostileClass,
-        NewHostileShip
+        AlienFleets,
+        AlienPopulation,
+        AlienShipRefitted,
 
+        // I have no idea
+        FireControlPaint,
+        FireControlLockLost,
+        JumpGateUnderway,
+        GroundUnitDest,
+        FullSpares,
+        MaintenanceZero,
+        TechDownloaded,
+        TechDataScanned,
+        NoFreighterOverhaul,
+        GPDContactLost,
+        TechRemoved,
+        NegotiationModifier,
+        IllegalOrder,
+        AlienClassScanned,
+        OverhaulClockReduced,
+        TrainingReset,
+        ParasiteLauncherReady,
+        InsufficientJGC,
+        PlanetLooted,
+        UnsuitablePlanet,
+        GovernmentChange, // NPR's don't change government? Player government change isn't recorded?
+        FighterAssigned,
+
+        NewAlienClass, // Possibly depreciated in favor of "NewHostileClass"
+        NewAlienShip, // Possibly depreciated in favor of "NewHostileShip"
+        ProbeOutOfFuel, // Possibly used when missle bouys used fuel?
+
+        JumpGateDetected, // Depreciated for "JumpPointFound"?
+        JumpPointDetected, // Depreciated for "JumpPointFound"?
+
+        // Known Depreciated
+        ShipMothballed,
+        ShipReactivation,
+        HyperLimitWarning,
+
+        ItsLifeJim, // Easter Egg Event?
+
+        #endregion
     }
 }
