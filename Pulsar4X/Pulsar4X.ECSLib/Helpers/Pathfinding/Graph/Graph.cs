efc024b4a5451d225713ee89c410723021047cb2
Rod Serling <ScottPhelps.usn@gmail.com>
2016-02-25 18:00:12 -0600

    More work on Pathfinding

---
+++ Pulsar4X/Pulsar4X.ECSLib/Helpers/Pathfinding/Graph/Graph.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Pulsar4X.ECSLib
+{
+    /// <summary>
+    /// The Graph class represents a graph, which is composed of a collection of nodes and edges. This Graph class
+    /// maintains its collection of nodes using the NodeList class, which is a collection of Node objects.
+    /// It delegates the edge maintenance to the Node class.  The Node class maintains the edge information using
+    /// the adjacency list technique.
+    /// </summary>
+    public class Graph
+    {
+        public virtual NodeList Nodes => _nodes;
+        public virtual int Count => _nodes.Count;
+        private readonly NodeList _nodes;
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor.  Creates a new Graph class instance.
+        /// </summary>
+        public Graph()
+        {
+            _nodes = new NodeList();
+        }
+
+        /// <summary>
+        /// Creates a new graph class instance based on a list of nodes.
+        /// </summary>
+        /// <param name="nodes">The list of nodes to populate the newly created Graph class with.</param>
+        public Graph(NodeList nodes)
+        {
+            _nodes = nodes;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Clears out all of the nodes in the graph.
+        /// </summary>
+        public virtual void Clear()
+        {
+            _nodes.Clear();
+        }
+
+        /// <summary>
+        /// Adds a new node to the graph.
+        /// </summary>
+        /// <param name="u">The node to add.</param>
+        public virtual void AddNode(Node u)
+        {
+            // Make sure this node is unique
+            if (!Contains(u))
+            {
+                _nodes.Add(u);
+
+                // Establish links to neighbors.
+                foreach (Node v in _nodes)
+                {
+                    if (u == v)
+                    {
+                        continue;
+                    }
+
+                    double cost;
+                    if (u.IsNeighbor(v, out cost))
+                    {
+                        AddUndirectedEdge(u, v, cost);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a directed edge from one node to another.
+        /// </summary>
+        /// <param name="u">The node from which the directed edge eminates.</param>
+        /// <param name="v">The node from which the directed edge leads to.</param>
+        /// <exception cref="ArgumentException">Thrown if the provided nodes are not part of this graph.</exception>
+        public virtual void AddDirectedEdge(Node u, Node v)
+        {
+            AddDirectedEdge(u, v, 0);
+        }
+
+        /// <summary>
+        /// Adds a directed, weighted edge from one node to another.
+        /// </summary>
+        /// <param name="u">The node from which the directed edge eminates.</param>
+        /// <param name="v">The node from which the directed edge leads to.</param>
+        /// <param name="cost">The weight of the edge.</param>
+        /// <exception cref="ArgumentException">Thrown if the provided nodes are not part of this graph.</exception>
+        public virtual void AddDirectedEdge(Node u, Node v, double cost)
+        {
+            // get references to uKey and vKey
+            if (Contains(u) && Contains(v))
+            {
+                u.AddDirected(v, cost);
+            }
+            else
+            {
+                throw new ArgumentException("One or both of the nodes supplied were not members of the graph.");
+            }
+        }
+
+        /// <summary>
+        /// Adds an undirected edge from one node to another.
+        /// </summary>
+        /// <param name="u">The node from which the directed edge eminates.</param>
+        /// <param name="v">The node from which the directed edge leads to.</param>
+        /// <param name="cost">The weight of the edge.</param>
+        /// <exception cref="ArgumentException">Thrown if the provided nodes are not part of this graph.</exception>
+        public virtual void AddUndirectedEdge(Node u, Node v, double cost)
+        {
+            // Make sure u and v are Nodes in this graph
+            if (Contains(u) && Contains(v))
+            {
+                // Add an edge from u -> v and from v -> u
+                _nodes[u.Key].AddDirected(v, cost);
+                _nodes[v.Key].AddDirected(u, cost);
+            }
+            else
+            {
+                throw new ArgumentException("One or both of the nodes supplied were not members of the graph.");
+            }
+        }
+
+        /// <summary>
+        /// Determines if a node exists within the graph.
+        /// </summary>
+        /// <param name="n">The node to check for in the graph.</param>
+        /// <returns>True if the node n exists in the graph, False otherwise.</returns>
+        public virtual bool Contains(Node n)
+        {
+            return _nodes.ContainsKey(n.Key);
+        }
+
+        #endregion
+    }
+}
--- Pulsar4X/Pulsar4X.ECSLib/Helpers/Pathfinding/PathfindingManager.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Helpers/Pathfinding/PathfindingManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,13 +8,20 @@ namespace Pulsar4X.ECSLib
     public class PathfindingManager
     {
         private readonly Game _game;
+        private readonly Hashtable _dist = new Hashtable();
+        private readonly Hashtable _path = new Hashtable();
+
+        private readonly object _syncRoot = new object();
 
         public PathfindingManager(Game game)
         {
             _game = game;
         }
 
-        public Graph GetPathfindinGraph()
+        /// <summary>
+        /// Gets a pathfinding graph for the entire universe.
+        /// </summary>
+        public Graph GetPathfindingGraph()
         {
             var pathfindingGraph = new Graph();
             foreach (StarSystem starSystem in _game.StarSystems.Values)
@@ -25,27 +33,17 @@ namespace Pulsar4X.ECSLib
                     var thisTransitableDB = jumpPoint.GetDataBlob<TransitableDB>();
                     Entity destinationJP = thisTransitableDB.Destination;
 
-                    var node = new Node(jumpPoint, destinationJP, new AdjacencyList());
-                    if (pathfindingGraph.Contains(node))
-                    {
-                        continue;
-                    }
-
+                    var node = new JPNode(jumpPoint, destinationJP, new List<EdgeToNeighbor>());
                     pathfindingGraph.AddNode(node);
-                    foreach (Node otherNode in pathfindingGraph.Nodes.Cast<Node>().Where(otherNode => node != otherNode))
-                    {
-                        double weight;
-                        if (node.IsNeighbor(otherNode, out weight))
-                        {
-                            pathfindingGraph.AddUndirectedEdge(node, otherNode, weight);
-                        }
-                    }
                 }
             }
 
             return pathfindingGraph;
         }
 
+        /// <summary>
+        /// Gets a pathfinding graph for objects/systems known by the provided faction.
+        /// </summary>
         public Graph GetPathfindingGraph(Entity faction)
         {
             var factionDB = faction.GetDataBlob<FactionInfoDB>();
@@ -60,36 +58,120 @@ namespace Pulsar4X.ECSLib
                     var thisTransitableDB = jumpPoint.GetDataBlob<TransitableDB>();
                     Entity destinationJP = thisTransitableDB.Destination;
 
-                    var node = new Node(jumpPoint, destinationJP, new AdjacencyList());
-                    if (pathfindingGraph.Contains(node))
-                    {
-                        continue;
-                    }
-
+                    var node = new JPNode(jumpPoint, destinationJP, new List<EdgeToNeighbor>());
                     pathfindingGraph.AddNode(node);
-                    foreach (Node otherNode in pathfindingGraph.Nodes.Cast<Node>().Where(otherNode => node != otherNode))
-                    {
-                        double weight;
-                        if (node.IsNeighbor(otherNode, out weight))
-                        {
-                            pathfindingGraph.AddUndirectedEdge(node, otherNode, weight);
-                        }
-                    }
                 }
             }
 
             return pathfindingGraph;
         }
 
-        public NodeList GetPath(Entity source, Entity destination)
+        /// <summary>
+        /// Gets a stack of nodes representing the path from the source to the destination.
+        /// </summary>
+        public Stack GetPath(Entity source, Entity destination)
         {
-            var nodeList = new NodeList();
+            Graph graph;
+            if (source.HasDataBlob<OwnedDB>())
+            {
+                graph = GetPathfindingGraph(source.GetDataBlob<OwnedDB>().Faction);
+            }
+            else
+            {
+                graph = GetPathfindingGraph();
+            }
 
-            var graph = GetPathfindinGraph();
-            var Q = graph.Nodes.Clone();
-            
+            var sourceNode = new JPNode(source, source, new List<EdgeToNeighbor>());
+            graph.AddNode(sourceNode);
 
-            return nodeList;
+            var destinationNode = new JPNode(destination, destination, new List<EdgeToNeighbor>());
+            graph.AddNode(destinationNode);
+
+            lock (_syncRoot)
+            {
+                _dist.Clear();
+                _path.Clear();
+
+                foreach (JPNode node in graph.Nodes)
+                {
+                    _dist.Add(node.Key, double.MaxValue);
+                    _path.Add(node.Key, null);
+                }
+
+                _dist[sourceNode.Key] = 0;
+
+                NodeList nodes = graph.Nodes; // Nodes == Q
+
+                // [Dijkstra]
+                while (nodes.Count > 0)
+                {
+                    Node u = GetMin(nodes); // Get the Minimum Node
+                    nodes.Remove(u); // Remove it from set Q.
+
+                    foreach (EdgeToNeighbor edge in u.Neighbors)
+                    {
+                        Relax(u, edge.Neighbor, edge.Cost);
+                    }
+                }
+                // [/Dijkstra]
+
+                // Determine if a path exists.
+                double totalDistance = (double)_dist[destinationNode.Key];
+                if (totalDistance == double.MaxValue)
+                {
+                    // No path to target.
+                    return new Stack();
+                }
+
+                // Create the stack from the shortest path.
+                var pathStack = new Stack();
+                Node currentJPNode = destinationNode;
+                pathStack.Push(currentJPNode);
+                do
+                {
+                    Node prevJPNode = currentJPNode;
+                    currentJPNode = (Node)_path[prevJPNode.Key];
+
+                    pathStack.Push(currentJPNode);
+                } while (currentJPNode != sourceNode);
+                return pathStack;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the Node from the passed-in NodeList that has the smallest value in the distance table.
+        /// </summary>
+        private Node GetMin(NodeList nodes)
+        {
+            // find the node in nodes with the smallest distance value
+            double minDist = double.MaxValue;
+            Node minJPNode = null;
+            foreach (Node n in nodes)
+            {
+                if ((double)_dist[n.Key] <= minDist)
+                {
+                    minDist = (double)_dist[n.Key];
+                    minJPNode = n;
+                }
+            }
+
+            return minJPNode;
+        }
+
+        /// <summary>
+        /// Relaxes the edge from the Node uNode to vNode.
+        /// </summary>
+        private void Relax(Node uJPNode, Node vJPNode, double cost)
+        {
+            double distTouNode = (double)_dist[uJPNode.Key];
+            double distTovNode = (double)_dist[vJPNode.Key];
+
+            if (distTovNode > distTouNode + cost)
+            {
+                // update distance and route
+                _dist[vJPNode.Key] = distTouNode + cost;
+                _path[vJPNode.Key] = uJPNode;
+            }
         }
     }
 }
