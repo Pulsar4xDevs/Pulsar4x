75d5cd01e950f7ebecdc22c442e1e73c6ae092d7
Rod Serling <ScottPhelps.usn@gmail.com>
2016-03-02 22:36:16 -0600

    Additional AbilityDB's

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/AbilityDBs/ExplosionChanceAbilityDB.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Pulsar4X.ECSLib
+{
+    class ExplosionChanceAbilityDB : BaseDataBlob
+    {
+        /// <summary>
+        /// Chance this component will causs a secondary explosion when hit.
+        /// </summary>
+        [JsonProperty]
+        public float ExplosionChance { get; internal set; }
+
+        [JsonProperty]
+        public float ExplosionDamage { get; internal set; }
+
+        public override object Clone()
+        {
+            return new ExplosionChanceAbilityDB {ExplosionChance = ExplosionChance, OwningEntity = OwningEntity};
+        }
+    }
+}
