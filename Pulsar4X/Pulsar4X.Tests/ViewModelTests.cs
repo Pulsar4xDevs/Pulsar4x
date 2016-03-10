c4f6a3df6c64b432710e7763f0582e317102175d
Jon <jon.se5a@gmail.com>
2016-03-10 16:04:39 +1300

    Quick test that tests creating a default game via GameVM.

---
+++ Pulsar4X/Pulsar4X.Tests/ViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Pulsar4X.ECSLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pulsar4X.ViewModel;
+
+namespace Pulsar4X.Tests
+{
+    [TestFixture]
+    [Description("ViewModel and Generic Game Tests")]
+    class ViewModelTests
+    {
+        private Game _game;
+        private GameVM _gameVM;
+        private NewGameOptionsVM _newGameOptions;
+
+        [Test]
+        public void NewGame()
+        {
+            _gameVM = new GameVM();
+            _newGameOptions = NewGameOptionsVM.Create(_gameVM);
+            Assert.DoesNotThrow(() => _gameVM.CreateGame(_newGameOptions));
+        }
+
+
+    }
+}
