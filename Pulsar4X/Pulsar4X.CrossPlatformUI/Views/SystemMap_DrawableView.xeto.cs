c2d6ca3c36650561fb8659511333adb4176854a6
Jon <jon.se5a@gmail.com>
2016-03-18 09:12:38 +1300

    Now the transform works as intended.

--- Pulsar4X/Pulsar4X.CrossPlatformUI/Views/SystemMap_DrawableView.xeto.cs
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Views/SystemMap_DrawableView.xeto.cs
@@ -78,32 +78,36 @@ namespace Pulsar4X.CrossPlatformUI.Views
             _pen = new Pen(iconcolor, objectInfo.Pendata.Thickness);
 
         }
+        private float PosXViewAdjusted { get { return _objectData.PosX * _zoom + _parent.Width / 2; } }
+
         private float ViewPosX
         {
             get
             {
-                float posAdjust = _objectData.PosX * _zoom + _parent.Width / 2;   //adjust position for viewscreen.
                 float sizeAdjust = _objectData.Width / 2;               //adjust position for size
                 if (_objectData.SizeAffectedbyZoom)                     //if the size of the vectorimage should be affected by zooming. 
                     sizeAdjust *= _zoom;
-                return posAdjust - sizeAdjust;
+                return PosXViewAdjusted - sizeAdjust;
             }
+
         }
+
+        private float PosYViewAdjusted { get { return _objectData.PosY * _zoom + _parent.Height / 2; } }
+
         private float ViewPosY
         {
             get
-            {
-                float posAdjust = _objectData.PosY * _zoom + _parent.Height / 2;
+            {                
                 float sizeAdjust = _objectData.Height / 2;
                 if (_objectData.SizeAffectedbyZoom)
                     sizeAdjust *= _zoom;
-                return posAdjust - sizeAdjust;                
+                return PosYViewAdjusted - sizeAdjust;                
             }
         }
         public void DrawMe(Graphics g)
         {
             g.SaveTransform();
-            g.MultiplyTransform(Matrix.FromRotationAt(_objectData.Rotation, ViewPosX, ViewPosY));
+            g.MultiplyTransform(Matrix.FromRotationAt(_objectData.Rotation, PosXViewAdjusted, PosYViewAdjusted));
             if (_objectData is IconData)               
                 g.DrawEllipse(_pen, ViewPosX, ViewPosY, _objectData.Width, _objectData.Height);
             else if (_objectData is ArcData)
@@ -112,6 +116,7 @@ namespace Pulsar4X.CrossPlatformUI.Views
                 g.DrawArc(_pen, ViewPosX, ViewPosY, _objectData.Width * _zoom, _objectData.Height * _zoom, arcData.StartAngle, arcData.SweepAngle);
             }
             g.RestoreTransform();
+            
             
         }
     }
