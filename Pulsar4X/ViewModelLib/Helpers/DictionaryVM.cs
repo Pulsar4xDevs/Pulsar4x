ce810d68da50504421707af92a6b0be7cdfdf0be
Jon <jon.se5a@gmail.com>
2016-04-27 17:17:18 +1200

    Minor cleanup in viewmodels

--- Pulsar4X/ViewModelLib/Helpers/DictionaryVM.cs
+++ Pulsar4X/ViewModelLib/Helpers/DictionaryVM.cs
@@ -12,45 +12,9 @@ using Pulsar4X.ECSLib;
 namespace Pulsar4X.ViewModel
 {
 
-    public class RangeEnabledObservableCollection<T> : ObservableCollection<T>
-    {
-        public void AddRange(IEnumerable<T> items)
-        {
-            this.CheckReentrancy();
-            foreach (var item in items)
-                this.Items.Add(item);
-            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-        }
-    }
 
-    public class ItemPair<T1, T2> : INotifyPropertyChanged
-    {
-        private T1 _item1;
-        public T1 Item1
-        {
-            get { return _item1; }
-            set { _item1 = value; OnPropertyChanged(); }
-        }
 
-        private T2 _item2;
-        public T2 Item2
-        {
-            get { return _item2; }
-            set { _item2 = value; OnPropertyChanged(); }
-        }
 
-        public ItemPair(T1 item1, T2 item2)
-        {
-            Item1 = item1;
-            Item2 = item2;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));           
-        }
-    }
 
     public enum DisplayMode
     {
