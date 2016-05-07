2a45d7ed451294fd3fc06fa86ade88ebf0662477
Tim Sonnier <tisonnier2000@gmail.com>
2016-05-07 16:24:11 -0500

    Created SpeciesProcessorTest.cs

---
+++ Pulsar4X/Pulsar4X.Tests/SpeciesProcessorTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Pulsar4X.ECSLib;
+using System.Diagnostics;
+
+namespace Pulsar4X.Tests
+{
+    [TestFixture, Description("Population Growth Test")]
+    class SpeciesProcessorTest
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+
+
+        [Test]
+        public void testColonyGravityIsHabitable()
+        {
+
+        }
+
+        [Test]
+        public void testColonyToxicityCost()
+        {
+
+        }
+
+        [Test]
+        public void testColonyPressureCost()
+        {
+
+        }
+
+        [Test]
+        public void testColonyTemperatureCost()
+        {
+
+        }
+
+        [Test]
+        public void testColonyGasCost()
+        {
+
+        }
+
+        [Test]
+        public void testColonyCost()
+        {
+
+        }
+    }
+}
