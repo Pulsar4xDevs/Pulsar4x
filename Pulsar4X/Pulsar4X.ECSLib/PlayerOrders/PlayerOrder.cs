4a3979358bad2da801f88d49cc0d77523018a387
Tim Sonnier <tisonnier2000@gmail.com>
2016-05-24 07:38:04 -0500

    Created PlayerOrder class

---
+++ Pulsar4X/Pulsar4X.ECSLib/PlayerOrders/PlayerOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsar4X.ECSLib
+{
+    abstract class PlayerOrder
+    {
+        //@todo: flesh out
+        Entity _owningEntity;
+
+        public PlayerOrder()
+        {
+
+        }
+
+        public PlayerOrder(Entity owner)
+        {
+            _owningEntity = owner;
+        }
+    }
+}
