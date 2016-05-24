e428380bef4b270b5182a76eb3777b5c8abb27e7
Shajenko <tisonnier2000@gmail.com>
2016-05-24 16:50:56 -0500

    Reworking Orders

---
+++ Pulsar4X/Pulsar4X.ECSLib/OrderQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulsar4X.ECSLib
+{
+    public class OrderQueue
+    {
+        #region Properties
+
+        List<Entity> _orderList;
+
+        #endregion 
+
+        #region Constructors
+
+        #endregion
+    }
+}
