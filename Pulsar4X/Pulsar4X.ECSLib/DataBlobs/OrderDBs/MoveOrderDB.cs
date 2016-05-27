a1efc98ade4352339383f8831c396e4a68534b2b
Shajenko <tisonnier2000@gmail.com>
2016-05-26 22:19:37 -0500

    Created MoveOrderDB.cs for the new OrderQueue

---
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/OrderDBs/MoveOrderDB.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Pulsar4X.ECSLib.DataBlobs.OrderDBs
+{
+    class MoveOrderDB : BaseDataBlob
+    {
+        [JsonProperty]
+        public Entity Ship { get; internal set; }
+
+        [JsonProperty]
+        public Entity Target { get; internal set; }
+
+
+
+        public MoveOrderDB()
+        {
+
+        }
+
+        public MoveOrderDB(Entity ship, Entity target)
+        {
+            Ship = ship;
+            Target = target;
+        }
+
+        public override object Clone()
+        {
+            return new MoveOrderDB(Ship, Target);
+        }
+    }
+}
--- Pulsar4X/Pulsar4X.ECSLib/OrderQueue.cs
+++ Pulsar4X/Pulsar4X.ECSLib/OrderQueue.cs
@@ -16,6 +16,64 @@ namespace Pulsar4X.ECSLib
 
         #region Constructors
 
+        // @todo: make sure all types of orders are added 
+
+        public OrderQueue()
+        {
+            _orderList = new List<Entity>();
+        }
+
+        public OrderQueue(OrderQueue oq)
+        {
+            _orderList = oq._orderList.Select(item => (Entity)item.Clone()).ToList();
+        }
+
+        #endregion
+
+        #region Public API Functions
+
+        public bool ConstructionOrder(Entity colony, string type, long amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResearchOrder(Entity colony, string type, long researchersAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MiningOrder(Entity colony, string type, long amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ToggleConstruction(Entity colony, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveOrder(Entity ship, Entity System, long x, long y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AttackOrder(Entity ship, Entity target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SurveyOrder(Entity ship, Entity systemBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveCargoOrder(Entity ship, Entity target, string cargo, long amount)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
         #endregion
     }
 }
