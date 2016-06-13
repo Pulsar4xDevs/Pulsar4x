33da014288949a423a55a970609c6ab974ec9b5e
Shajenko <tisonnier2000@gmail.com>
2016-06-12 21:55:31 -0500

    Initial OrderQueueTest

---
+++ Pulsar4X/Pulsar4X.Tests/OrderQueueTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Pulsar4X.ECSLib;
+
+namespace Pulsar4X.Tests
+{
+    [TestFixture, Description("OrderQueue Test")]
+    public class OrderQueueTest
+    {
+        private Game _game;
+        private EntityManager _entityManager;
+        private Entity _faction;
+        // private List<Entity> _planets;
+        private Entity _ship1, _ship2;
+        private GalaxyFactory _galaxyFactory;
+        private StarSystemFactory _starSystemFactory;
+        private StarSystem _starSystem;
+
+        [SetUp]
+        public void Init()
+        {
+            _game = new Game(new NewGameSettings());
+            StaticDataManager.LoadData("Pulsar4x", _game);  
+            _entityManager = new EntityManager(_game);
+            _faction = FactionFactory.CreateFaction(_game, "Terran");
+
+            // Create system
+            _starSystem = _starSystemFactory.CreateSol(_game);
+
+            // _planets = _starSystem.SystemManager.GetAllEntitiesWithDataBlob<SystemBodyDB>();
+
+
+            // Create two ships
+            _ship1 = ShipFactory.
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+        [Test]
+        public void testOrderQueue()
+        {
+        }
+    }
+}
