decca0745f604c2a2989f586336372401569a07e
Tim Sonnier <tisonnier2000@gmail.com>
2016-06-16 23:35:12 -0500

    Adjustment

--- Pulsar4X/Pulsar4X.Tests/OrderQueueTest.cs
+++ Pulsar4X/Pulsar4X.Tests/OrderQueueTest.cs
@@ -13,18 +13,11 @@ namespace Pulsar4X.Tests
 
         private EntityManager _entityManager;
         private Entity _faction;
-        // private List<Entity> _planets;
-        private Entity _ship1, _ship2;
-        private GalaxyFactory _galaxyFactory;
-        private StarSystemFactory _starSystemFactory;
         private StarSystem _starSystem;
         private Player _player;
-        private EntityManager _entityManager;
-        private Entity _faction;
         private Entity _ship, _target;
         private Entity _earth;
         private List<Entity> _planets;
-        private StarSystem _starSystem;
         private List<StarSystem> _systems;
         private OrderProcessor _orderProcessor;
 
@@ -33,37 +26,9 @@ namespace Pulsar4X.Tests
         public void Init()
         {
             _game = new Game(new NewGameSettings());
-
-            StaticDataManager.LoadData("Pulsar4x", _game);  
-            _entityManager = new EntityManager(_game);
-            _faction = FactionFactory.CreateFaction(_game, "Terran");
-
-            // Create system
-            _starSystem = _starSystemFactory.CreateSol(_game);
-
-            // _planets = _starSystem.SystemManager.GetAllEntitiesWithDataBlob<SystemBodyDB>();
-
-
-            // Create two ships
-            _ship1 = ShipFactory.
-        }
-
-        [TearDown]
-        public void Cleanup()
-        {
-
             StaticDataManager.LoadData("Pulsar4x", _game);
-
             _player = _game.AddPlayer("Test Player");
-
             _faction = DefaultStartFactory.DefaultHumans(_game, _player, "Test Faction");
-
-            _systems = new List<StarSystem>();
-
-            foreach(KeyValuePair<Guid, StarSystem> kvp in _game.Systems)
-            {
-                _systems.Add(kvp.Value);
-            }
 
             _starSystem = _game.Systems.First<KeyValuePair<Guid, StarSystem>>().Value;
             _planets = _starSystem.SystemManager.GetAllEntitiesWithDataBlob<SystemBodyDB>();
@@ -74,7 +39,16 @@ namespace Pulsar4X.Tests
             _target = _ship.Clone(_starSystem.SystemManager);
 
             _orderProcessor = new OrderProcessor();
+
+            _systems = new List<StarSystem>();
+
+            foreach (KeyValuePair<Guid, StarSystem> kvp in _game.Systems)
+            {
+                _systems.Add(kvp.Value);
+            }
         }
+
+
 
 
         [TearDown]
