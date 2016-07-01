154f4f31e38658c47bc3921d65385214321b3c6d
Jon <jon.se5a@gmail.com>
2016-07-02 11:11:44 +1200

    Minor tweaks.

--- Pulsar4X/Pulsar4X.ECSLib/Factories/ShipComponents/ComponentInstanceFactory.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Factories/ShipComponents/ComponentInstanceFactory.cs
@@ -22,37 +22,5 @@ namespace Pulsar4X.ECSLib
     }
 
 
-    /// <summary>
-    /// this is used to give an High level entity such as a ship or colony an ability 
-    /// </summary>
-    internal static class AttributeToAbilityMap
-    {
-        //[ThreadStatic]
-        //private static Entity CurrentEntity;
-        internal static Dictionary<Type, Delegate> TypeMap = new Dictionary<Type, Delegate>
-        {
-            { typeof(EnginePowerAtbDB), new Action<Entity, Entity>((shipOrColonyEntity, componentInstanceEntity) => { if (!shipOrColonyEntity.HasDataBlob<PropulsionDB>()) shipOrColonyEntity.SetDataBlob<PropulsionDB>(new PropulsionDB()); }) },
-            { typeof(BeamWeaponAtbDB), new Action<Entity, Entity>((shipOrColonyEntity, componentInstanceEntity) => { if (!componentInstanceEntity.HasDataBlob<BeamWeaponsDB>()) shipOrColonyEntity.SetDataBlob(new BeamWeaponsDB()); }) },
-            { typeof(SimpleBeamWeaponAtbDB), new Action<Entity, Entity>((shipOrColonyEntity, componentInstanceEntity) => { if (!componentInstanceEntity.HasDataBlob<BeamWeaponsDB>()) shipOrColonyEntity.SetDataBlob(new BeamWeaponsDB()); }) },
-        };
 
-        /// <summary>
-        /// adds abilites to a ship or component instance
-        /// </summary>
-        /// <param name="shipOrColony"></param>
-        /// <param name="componentDesign"></param>
-        /// <param name="componentInstance"></param>
-        internal static void AddAbility(Entity shipOrColony, Entity componentDesign, Entity componentInstance)
-        {
-
-            //CurrentEntity = shipOrColony;
-            foreach (var datablob in componentDesign.DataBlobs)
-            {
-                var t = datablob.GetType();
-                if (TypeMap.ContainsKey(t))
-                    TypeMap[t].DynamicInvoke(shipOrColony, componentInstance); // invoke appropriate delegate  
-            }
-            componentInstance.GetDataBlob<ComponentInstanceInfoDB>().ParentEntity = shipOrColony;
-        }
-    }
 }
