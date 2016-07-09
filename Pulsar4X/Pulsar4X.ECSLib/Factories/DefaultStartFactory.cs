58e8cc628d9832631ecfbcaf29b2d8e6a7db4ab5
Jon <jon.se5a@gmail.com>
2016-07-09 20:07:05 +1200

    fixed a merge bug.

--- Pulsar4X/Pulsar4X.ECSLib/Factories/DefaultStartFactory.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Factories/DefaultStartFactory.cs
@@ -66,8 +66,8 @@ namespace Pulsar4X.ECSLib
             //Entity ship = ShipFactory.CreateShip(shipClass, sol.SystemManager, factionEntity, position, sol, "Serial Peacemaker");
             //ship.SetDataBlob(earth.GetDataBlob<PositionDB>()); //first ship reference PositionDB
 
-            Entity ship2 = ShipFactory.CreateShip(shipClass, sol.SystemManager, factionEntity, position, sol, "Ensuing Calm");
-            ship2.SetDataBlob(earth.GetDataBlob<OrbitDB>());//second ship reference earth OrbitDB
+            //Entity ship2 = ShipFactory.CreateShip(shipClass, sol.SystemManager, factionEntity, position, sol, "Ensuing Calm");
+            //ship2.SetDataBlob(earth.GetDataBlob<OrbitDB>());//second ship reference earth OrbitDB
 
             //Entity ship3 = ShipFactory.CreateShip(shipClass, sol.SystemManager, factionEntity, position, sol, "Contiual Pacifier");
             //ship3.SetDataBlob((OrbitDB)earth.GetDataBlob<OrbitDB>().Clone());//second ship clone earth OrbitDB
