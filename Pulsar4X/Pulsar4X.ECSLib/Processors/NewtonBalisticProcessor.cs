01f98bd2cba6911976a788f6fab24918c0ba7a05
NathanH- <nhdump@gmail.com>
2016-11-12 23:02:51 -0600

    Code for hitting a planet added, but better prediction for when planetary impacts is needed.

--- Pulsar4X/Pulsar4X.ECSLib/Processors/NewtonBalisticProcessor.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Processors/NewtonBalisticProcessor.cs
@@ -21,6 +21,9 @@ namespace Pulsar4X.ECSLib
 
             manager.ManagerSubpulses.AddSystemInterupt(toDate + orbitCycle, PulseActionEnum.BalisticMoveProcessor);
 
+            List<Entity> RemoveList = new List<Entity>();
+            List<StarSystem> RemoveSystem = new List<StarSystem>();
+
             foreach (Entity objectEntity in manager.GetAllEntitiesWithDataBlob<NewtonBalisticDB>())
             {
                 NewtonBalisticDB balisticDB = objectEntity.GetDataBlob<NewtonBalisticDB>();
@@ -33,10 +36,32 @@ namespace Pulsar4X.ECSLib
                 if(targetPos.AbsolutePosition == position.AbsolutePosition)
                 {
                     //do something in damage processor for asteroid hitting a planet?
+                    DamageProcessor.OnTakingDamage(myTarget, 1000000); ///one. million. damage points.
 
+                    StarSystem mySystem;
+                    if (!manager.Game.Systems.TryGetValue(position.SystemGuid, out mySystem))
+                        throw new GuidNotFoundException(position.SystemGuid);
+
+                    RemoveList.Add(objectEntity);
+                    RemoveSystem.Add(mySystem);
+
+                    mySystem.SystemManager.RemoveEntity(objectEntity); //get rid of the asteroid
                 }
-
             }
+
+            /// <summary>
+            /// Clean up the asteroids that have hit something and been put in the remove list.
+            /// </summary>
+            for(int removeIterator = 0; removeIterator < RemoveList.Count; removeIterator++)
+            {
+                RemoveSystem[removeIterator].SystemManager.RemoveEntity(RemoveList[removeIterator]);
+            }
+
+            /// <summary>
+            /// This may not be necessary but clear these two lists.
+            /// </summary>
+            RemoveList.Clear();
+            RemoveSystem.Clear();
         }
     }
 }
