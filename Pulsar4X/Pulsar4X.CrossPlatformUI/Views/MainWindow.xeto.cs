cb8152f7020dcb44b006377bf87f1a9d44ad8db4
ghosta0815 <Manuel.Gschrey@gmail.com>
2017-01-21 22:37:23 +0100

    Enables/Disables Time control, when a game is loaded

--- Pulsar4X/Pulsar4X.CrossPlatformUI/Views/MainWindow.xeto.cs
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Views/MainWindow.xeto.cs
@@ -9,8 +9,11 @@ namespace Pulsar4X.CrossPlatformUI.Views {
     public class MainWindow : Panel {
         #region Form Controls
         protected StackLayout TopButtonBar;
-
         protected StackLayout adv_buttons;
+        protected DropDown dd_subpulse;
+        protected TabControl view_tabs;
+        protected TimeControlView TimeControlV;
+
         #region Advance Time Buttons
         protected Button btn_time_5sec;
         protected Button btn_time_30sec;
@@ -24,18 +27,38 @@ namespace Pulsar4X.CrossPlatformUI.Views {
         protected Button btn_time_5day;
         protected Button btn_time_30day;
         #endregion
-
-        protected DropDown dd_subpulse;
-        protected TabControl view_tabs;
         #endregion
 
-        private GameVM _game;
+        private GameVM _gameVM;
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the MainWindow view
+        /// </summary>
         public MainWindow(GameVM game) {
-            _game = game;
-            DataContext = _game;
+            _gameVM = game;
+            DataContext = _gameVM;
             XamlReader.Load(this);
+            _gameVM.PropertyChanged += _gameVM_PropertyChanged;
         }
-        
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Enables/Disables Timing control, when game has loaded/unloaded;
+        /// </summary>
+        private void _gameVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "HasGame")
+            {
+                TimeControlV.Enabled = _gameVM.HasGame;          
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Adds additional Views as Panels to the TabPage
+        /// </summary>
         public void AddOrSelectTabPanel(string title, Container c, bool activate = true, bool force_new = false) {
             if (!force_new) {
                 foreach (TabPage p in view_tabs.Pages) {
