794260b23941daf51e7ee0101edca4cc5fc739bc
ghosta0815 <Manuel.Gschrey@gmail.com>
2017-02-11 20:02:37 +0100

    Rework of TextIcon-Handling to allow stacking the labels

--- Pulsar4X/Pulsar4X.CrossPlatformUI/Views/SystemView/IconCollection.cs
+++ Pulsar4X/Pulsar4X.CrossPlatformUI/Views/SystemView/IconCollection.cs
@@ -7,39 +7,116 @@ namespace Pulsar4X.CrossPlatformUI.Views
 {
     internal class IconCollection
     {
-        public List<IconBase> Icons { get; } = new List<IconBase>();
+        //Main dictionaries for storing Icons
+        //The Guid represents the entity, the Icon is created for
+        public List<OrbitRing> OrbitList { get; } = new List<OrbitRing>();
+        public List<TextIcon> TextIconList { get; } = new List<TextIcon>();
+        public List<EntityIcon> EntityList { get; } = new List<EntityIcon>();
+
+
         public Dictionary<Guid, EntityIcon> IconDict { get; } = new Dictionary<Guid, EntityIcon> ();
+
+        /// <summary>
+        /// Constructs an IconCollection
+        /// </summary>
         public IconCollection()
         {
         }
 
+        /// <summary>
+        /// Initializes the IconCollection
+        /// Creates Orbitrings, TextIcons and Entityicons and adds them to Collection
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <param name="camera"></param>
         public void Init(IEnumerable<Entity> entities, Camera2dv2 camera)
         {
-            Icons.Clear();
+            System.Diagnostics.Debug.WriteLine("Init called");
             IconDict.Clear();
+            OrbitList.Clear();
+            TextIconList.Clear();
+            EntityList.Clear();
+
             foreach (var item in entities)
             {
                 if (item.HasDataBlob<OrbitDB>() && item.GetDataBlob<OrbitDB>().Parent != null)
                 {
                     OrbitRing ring = new OrbitRing(item, camera);
-
-                    Icons.Add(ring);
+                    OrbitList.Add(ring);
                 }
                 if (item.HasDataBlob<NameDB>())
-                    Icons.Add(new TextIcon(item, camera));
+                {
+                    TextIconList.Add(new TextIcon(item, camera));
+                }
+
 
                 EntityIcon entIcon = new EntityIcon(item, camera);
-                Icons.Add(entIcon);
+                EntityList.Add(entIcon);
+
+
                 IconDict.Add(item.Guid, entIcon);
             }
         }
 
+        /// <summary>
+        /// Distributes the TextIcons, when they are overlapping
+        /// </summary>
+        public void TextIconsDistribute()
+        {
+            List<Rectangle> occupiedPosition = new List<Rectangle>();
+            IComparer<Rectangle> byViewPos = new ByViewPosition();
+
+            foreach (var item in TextIconList)
+            {
+                item.ViewOffset = item.DefaultViewOffset;
+            }
+
+            //Placement happens bottom to top, left to right
+            //Each newly placed Texticon is compared to only the Texticons that are placed above its position
+            //Therefore a sorted list of the occupied Positions is maintained
+            TextIconList.Sort();
+            occupiedPosition.Add(TextIconList[0].ViewDisplayRect);
+            for (int i = 1; i < TextIconList.Count; i++)
+            {
+                var lowestPosIndex = occupiedPosition.BinarySearch(TextIconList[i].ViewDisplayRect + new Point(0,(int)TextIconList[i].ViewNameSize.Height) , byViewPos);
+                if (lowestPosIndex < 0) lowestPosIndex = ~lowestPosIndex;
+                
+                for (int j = lowestPosIndex; j < occupiedPosition.Count; j++)
+                {
+                    if (TextIconList[i].ViewDisplayRect.Intersects(occupiedPosition[j]))
+                    {
+                        TextIconList[i].ViewOffset -= new PointF(0,  TextIconList[i].ViewDisplayRect.Bottom - occupiedPosition[j].Top);
+                    }
+                }
+                //Inserts the new label sorted
+                var InsertIndex = occupiedPosition.BinarySearch(TextIconList[i].ViewDisplayRect, byViewPos);
+                if (InsertIndex < 0) InsertIndex = ~InsertIndex;
+                occupiedPosition.Insert(InsertIndex, TextIconList[i].ViewDisplayRect);
+            }
+
+
+        }
+
+        /// <summary>
+        /// Draws all Items in _ItemCollection
+        /// In the following order:
+        ///     Orbitrings
+        ///     EntitySymbols
+        ///     TextLabels
+        /// </summary>
+        /// <param name="g"></param>
         public void DrawMe(Graphics g)
         {
-            foreach (var item in Icons)
+            foreach( var item in OrbitList)
+                item.DrawMe(g);
+            foreach (var item in EntityList)
+                item.DrawMe(g);
+
+            TextIconsDistribute();
+            foreach (var item in TextIconList)
             {
                 item.DrawMe(g);
-            }
+            }            
         }
     }
 
@@ -47,8 +124,26 @@ namespace Pulsar4X.CrossPlatformUI.Views
     {
         //sets the size of the icons
         float Scale { get; set; }
-
         void DrawMe(Graphics g);
     }
 
+
+    /// <summary>
+    /// IComparer for the Texticonrectangles (or any other rectancle)
+    /// Sorts Bottom to top, left to right
+    /// </summary>
+    internal class ByViewPosition : IComparer<Rectangle>
+    {
+        public int Compare(Rectangle r1, Rectangle r2)
+        {
+            if (r1.Bottom > r2.Bottom) return -1;
+            else if (r1.Bottom < r2.Bottom) return 1;
+            else
+            {
+                if (r1.Left > r2.Left) return -1;
+                else if (r1.Left < r2.Left) return 1;
+                else return 0;
+            }
+        }
+    }
 }
