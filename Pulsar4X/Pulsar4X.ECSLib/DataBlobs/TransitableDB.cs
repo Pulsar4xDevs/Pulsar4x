a5313568aec929caeb7486597fcc1f023a74e51b
Rod Serling <ScottPhelps.usn@gmail.com>
2017-05-02 02:37:01 -0500

    DOC: Document TransitableDB/Clean Dead code

--- Pulsar4X/Pulsar4X.ECSLib/DataBlobs/TransitableDB.cs
+++ Pulsar4X/Pulsar4X.ECSLib/DataBlobs/TransitableDB.cs
@@ -5,32 +5,33 @@ using Newtonsoft.Json;
 
 namespace Pulsar4X.ECSLib
 {
+    /// <summary>
+    /// TransitableDB defines an entity as capable of being used as a jump point.
+    /// </summary>
     public class TransitableDB : BaseDataBlob
     {
+        /// <summary>
+        /// Destination that this jump point goes to.
+        /// </summary>
         [JsonProperty]
         public Entity Destination { get; internal set; }
-
-        public ReadOnlyDictionary<Entity, int> BridgeDictionary => new ReadOnlyDictionary<Entity, int>(_bridgeDictionary);
-        [JsonProperty]
-        internal Dictionary<Entity, int> _bridgeDictionary = new Dictionary<Entity, int>();
 
         [JsonProperty]
         public bool IsStabilized { get; internal set; }
 
         public TransitableDB() { }
 
-        public TransitableDB(Entity destination) : this(destination, false, new Dictionary<Entity, int>()) { }
+        public TransitableDB(Entity destination) : this(destination, false) { }
 
-        public TransitableDB(Entity destination, bool isStabilized, IDictionary<Entity, int> bridgeDictionary)
+        public TransitableDB(Entity destination, bool isStabilized)
         {
             Destination = destination;
             IsStabilized = isStabilized;
-            _bridgeDictionary = new Dictionary<Entity, int>(bridgeDictionary);
         }
 
         public override object Clone()
         {
-            return new TransitableDB(Destination, IsStabilized, _bridgeDictionary);
+            return new TransitableDB(Destination, IsStabilized);
         }
     }
 }
