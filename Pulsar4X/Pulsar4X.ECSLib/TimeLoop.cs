dd8e7b7b1dfa9bc302d749956593568350483a68
Rod Serling <ScottPhelps.usn@gmail.com>
2017-05-15 08:20:56 -0500

    Apply formatting/Code style to TimeLoop

--- Pulsar4X/Pulsar4X.ECSLib/TimeLoop.cs
+++ Pulsar4X/Pulsar4X.ECSLib/TimeLoop.cs
@@ -23,19 +23,25 @@ namespace Pulsar4X.ECSLib
         //changes how often the tick happens
         public float TimeMultiplier
         {
-            get {return _timeMultiplier;}
+            get { return _timeMultiplier; }
             set
             {
                 _timeMultiplier = value;
                 _timer.Interval = _tickInterval.TotalMilliseconds * value;
             }
-        } 
+        }
         private float _timeMultiplier = 1f;
 
         private TimeSpan _tickInterval = TimeSpan.FromMilliseconds(250);
-        public TimeSpan TickFrequency { get { return _tickInterval; } set { _tickInterval = value;
-            _timer.Interval = _tickInterval.TotalMilliseconds * _timeMultiplier;
-        } }
+        public TimeSpan TickFrequency
+        {
+            get { return _tickInterval; }
+            set
+            {
+                _tickInterval = value;
+                _timer.Interval = _tickInterval.TotalMilliseconds * _timeMultiplier;
+            }
+        }
 
 
         public TimeSpan Ticklength { get; set; } = TimeSpan.FromSeconds(3600);
@@ -54,7 +60,7 @@ namespace Pulsar4X.ECSLib
         /// <param name="state"></param>
         private void InvokeDateChange(object state)
         {
-            Event logevent = new Event(GameGlobalDateTime, "Game Global Date Changed", null, null, null);
+            var logevent = new Event(GameGlobalDateTime, "Game Global Date Changed", null, null, null);
             logevent.EventType = EventType.GlobalDateChange;
             _game.EventLog.AddEvent(logevent);
 
@@ -70,9 +76,13 @@ namespace Pulsar4X.ECSLib
             {
                 _gameGlobalDateTime = value;
                 if (_game.SyncContext != null)
+                {
                     _game.SyncContext.Post(InvokeDateChange, value); //marshal to the main (UI) thread, so the event is invoked on that thread.
+                }
                 else
+                {
                     InvokeDateChange(value);//if context is null, we're probibly running tests or headless. in this case we're not going to marshal this.    
+                }
             }
         }
         /// <summary>
@@ -87,12 +97,12 @@ namespace Pulsar4X.ECSLib
         /// <param name="game"></param>
         internal TimeLoop(Game game)
         {
-            
+
             _game = game;
             _timer.Interval = _tickInterval.TotalMilliseconds;
             _timer.Enabled = false;
             _timer.Elapsed += Timer_Elapsed;
-            
+
         }
 
         #region Public Time Methods. UI interacts with time here
@@ -135,9 +145,13 @@ namespace Pulsar4X.ECSLib
         internal void AddSystemInteractionInterupt(DateTime datetime, PulseActionEnum action, SystemEntityJumpPair jumpPair)
         {
             if (!EntityDictionary.ContainsKey(datetime))
+            {
                 EntityDictionary.Add(datetime, new Dictionary<PulseActionEnum, List<SystemEntityJumpPair>>());
+            }
             if (!EntityDictionary[datetime].ContainsKey(action))
+            {
                 EntityDictionary[datetime].Add(action, new List<SystemEntityJumpPair>());
+            }
             EntityDictionary[datetime][action].Add(jumpPair);
         }
 
@@ -150,7 +164,7 @@ namespace Pulsar4X.ECSLib
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             if (!_isProcessing)
-            {               
+            {
                 DoProcessing(); //run DoProcessing if we're not already processing
             }
             else
@@ -158,8 +172,6 @@ namespace Pulsar4X.ECSLib
                 _isOvertime = true; //if we're processing, then processing it taking longer than the sim speed
             }
         }
-
-
 
         private void DoProcessing()
         {
@@ -172,14 +184,16 @@ namespace Pulsar4X.ECSLib
             //check for global interupts
             _targetDateTime = GameGlobalDateTime + Ticklength;
 
-         
+
             while (GameGlobalDateTime < _targetDateTime)
             {
                 DateTime nextInterupt = ProcessNextInterupt(_targetDateTime);
                 //do system processors
-              
+
                 if (_game.Settings.EnableMultiThreading == true) //threaded
+                {
                     Parallel.ForEach<StarSystem>(_game.Systems.Values, starSys => starSys.SystemManager.ManagerSubpulses.ProcessSystem(nextInterupt));
+                }
                 //The above 'blocks' till all the tasks are done.
                 else //non threaded
                 {
@@ -211,9 +225,9 @@ namespace Pulsar4X.ECSLib
                 nextInteruptDateTime = EntityDictionary.Keys.Min();
                 if (nextInteruptDateTime <= maxDateTime)
                 {
-                    foreach (var delegateListPair in EntityDictionary[nextInteruptDateTime])
+                    foreach (KeyValuePair<PulseActionEnum, List<SystemEntityJumpPair>> delegateListPair in EntityDictionary[nextInteruptDateTime])
                     {
-                        foreach (var jumpPair in delegateListPair.Value) //foreach entity in the value list
+                        foreach (SystemEntityJumpPair jumpPair in delegateListPair.Value) //foreach entity in the value list
                         {
                             //delegateListPair.Key.DynamicInvoke(_game, jumpPair);
                             PulseActionDictionary.DoAction(delegateListPair.Key, _game, jumpPair);
@@ -223,10 +237,14 @@ namespace Pulsar4X.ECSLib
                     processedTo = nextInteruptDateTime;
                 }
                 else
+                {
                     processedTo = maxDateTime;
+                }
             }
             else
+            {
                 processedTo = maxDateTime;
+            }
 
             return processedTo;
         }
@@ -239,7 +257,9 @@ namespace Pulsar4X.ECSLib
             if (GameGlobalDateTime.Equals(other.GameGlobalDateTime))
             {
                 if (EntityDictionary.Count.Equals(other.EntityDictionary.Count))
+                {
                     equality = true;
+                }
             }
             return equality;
         }
