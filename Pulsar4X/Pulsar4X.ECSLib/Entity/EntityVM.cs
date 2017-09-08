3b38c63e6e643eac93289dddd36e879e2f80b242
se5a <jon.se5a@gmail.com>
2017-09-08 22:09:36 +1200

    a few more missed files not added to git.

---
+++ Pulsar4X/Pulsar4X.ECSLib/Entity/EntityVM.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Pulsar4X.ECSLib
+{
+    public class EntityVM
+    {
+        Game _game;
+        private Entity _entity;
+        public string Guid { get { return _entity.Guid.ToString(); } }
+        public string EntityName { get; set; }
+
+        Dictionary<ICreateViewmodel, IDBViewmodel> _viewmodelDict = new Dictionary<ICreateViewmodel, IDBViewmodel>();
+        internal List<IDBViewmodel> Viewmodels = new List<IDBViewmodel>();
+
+        public EntityVM(Game game, Entity entity)
+        {
+            _game = game;
+            _entity = entity;
+
+        }
+
+        internal void Update()
+        {
+
+            foreach(ICreateViewmodel datablob in _entity.DataBlobs.Where(item => item is ICreateViewmodel))
+            {
+                if(datablob is ICreateViewmodel &!_viewmodelDict.ContainsKey(datablob))
+                {
+                    var newvm = datablob.CreateVM(_game);
+                    Viewmodels.Add(newvm);
+                    _viewmodelDict.Add(datablob, newvm);
+                }
+            }
+            foreach(var datablobAsKey in _viewmodelDict.Keys.ToArray())
+            {
+                if(!_entity.DataBlobs.Contains((BaseDataBlob)datablobAsKey))
+                {
+                    Viewmodels.Remove(_viewmodelDict[datablobAsKey]);
+                    _viewmodelDict.Remove(datablobAsKey); 
+                }
+            }
+
+            foreach(var viewmodel in Viewmodels)
+            {
+                viewmodel.Update();
+            }
+        }
+    }
+
+
+    public interface IDBViewmodel
+    {
+        void Update();
+    }
+
+
+    internal interface ICreateViewmodel
+    {
+        IDBViewmodel CreateVM(Game game);
+    }
+}
---
+++ Pulsar4X/Pulsar4X.ECSLib/ViewModels/MainVM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Pulsar4X.ECSLib
+{
+    public class MainVM
+    {
+
+        Entity _currentFactionEntity;
+        Game Game { get; set; }
+
+        public string CurrentFactionName { get; set; } = "";
+        public string StatusText { get; set; } = "";
+
+        public List<EntityVM> ViewedEntites { get; set; } = new List<EntityVM>(); 
+
+        public MainVM()
+        {
+        }
+
+
+
+
+        public void CreateGame(NewGameOptionsVM options)
+        {
+            StatusText = "Creating Game...";
+
+            // TODO: Databind the GameSettings object in the NewGameOptionsVM
+            var gameSettings = new NewGameSettings
+            {
+                GameName = "Test Game",
+                MaxSystems = options.NumberOfSystems,
+                SMPassword = options.GmPassword,
+                DataSets = options.SelectedModList.Select(dvi => dvi.Directory),
+                CreatePlayerFaction = options.CreatePlayerFaction,
+                DefaultFactionName = options.FactionName,
+                DefaultPlayerPassword = options.FactionPassword,
+                DefaultSolStart = options.DefaultStart,
+            };
+
+            Game = new Game(gameSettings);
+
+            //_currentFactionEntity = roles.FirstOrDefault(role => (role.Value & AccessRole.Owner) != 0).Key;
+
+
+            StatusText = "Game Created.";
+
+            //StarSystemViewModel = new SystemView.StarSystemVM(this, Game, _currentFactionEntity);
+            //StarSystemViewModel.Initialise();
+        }
+
+        public void LoadGame(string pathToFile)
+        {
+            StatusText = "Loading Game...";
+            Game = SerializationManager.ImportGame(pathToFile);
+
+
+            // TODO: Select Default player, generate auth token for them.
+            //CurrentAuthToken = new AuthenticationToken(Game.SpaceMaster);
+            _currentFactionEntity = Game.GameMasterFaction;
+
+            StatusText = "Game Loaded.";
+        }
+
+    }
+}
