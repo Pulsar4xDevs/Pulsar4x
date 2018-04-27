676b7ea695a2b5d0ad425fded0a59fe194ad65c1
se5a <jon.se5a@gmail.com>
2018-04-27 23:05:15 +1200

    first stab at an SDL UI.

---
+++ Pulsar4X/Pulsar4X.SDL2UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SDL2;
+namespace Pulsar4X.SDL2UI
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var window = SDL.SDL_CreateWindow("Pulsar4X Window", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+            var renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+        }
+    }
+}
