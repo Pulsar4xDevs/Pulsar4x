d031523716f7cea01a24ffd5d38277a977bdbc34
se5a <jon.se5a@gmail.com>
2018-05-03 18:33:11 +1200

    Start of an SDL + DearIMGUI-CS version

---
+++ Pulsar4X/Pulsar4X.SDL2UI/NewGameOptions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Pulsar4X;
+using ImGuiNET;
+
+namespace Pulsar4X.SDL2UI
+{
+    public class NewGameOptions: PulsarGuiWindow
+    {
+        GlobalUIState _state;
+        enum gameType { Nethost, Standalone }
+        int gameTypeButtonGrp = 0;
+        gameType selectedGameType = gameType.Standalone;
+        byte[] netPortInputBuffer = new byte[8];
+        string netPortString { get { return System.Text.Encoding.UTF8.GetString(netPortInputBuffer); } }
+        byte[] nameInputBuffer = new byte[16];
+
+        internal NewGameOptions(GlobalUIState state)
+        { _state = state; }
+
+
+
+        ECSLib.NewGameSettings gameSettings = new ECSLib.NewGameSettings();
+        internal override void Display()
+        {
+            ImGui.InputText("Game Name", nameInputBuffer, 16);
+            if (ImGui.RadioButton("Host Network Game", ref gameTypeButtonGrp, 1))
+                selectedGameType = gameType.Nethost;
+            if (ImGui.RadioButton("Start Standalone Game", ref gameTypeButtonGrp, 0))
+                selectedGameType = gameType.Standalone;
+            if (selectedGameType == gameType.Nethost)
+                ImGui.InputText("Network Port", netPortInputBuffer, 8);
+            if (ImGui.Button("Create New Game!"))
+                DoNewGame(System.Text.Encoding.UTF8.GetString(nameInputBuffer));
+
+
+
+        }
+
+        void DoNewGame(string name)
+        {
+            
+            var gameSettings = new Pulsar4X.ECSLib.NewGameSettings
+            {
+                GameName = "name",
+                MaxSystems = 100,
+                SMPassword = "",
+                //DataSets = options.SelectedModList.Select(dvi => dvi.Directory),
+                CreatePlayerFaction = true,
+                DefaultFactionName = "UEF",
+                DefaultPlayerPassword = "",
+                DefaultSolStart = true,
+            };
+            _state.Game = new ECSLib.Game(gameSettings);
+
+
+        }
+
+    }
+
+
+
+}
--- Pulsar4X/Pulsar4X.SDL2UI/Program.cs
+++ Pulsar4X/Pulsar4X.SDL2UI/Program.cs
@@ -1,15 +1,126 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using SDL2;
+using ImGuiNET;
+using ImGuiSDL2CS;
+
+
 namespace Pulsar4X.SDL2UI
 {
     public class Program
     {
+        static SDL2Window Instance;
         [STAThread]
-        public static void Main(string[] args)
+        public static void Main()
         {
-            var window = SDL.SDL_CreateWindow("Pulsar4X Window", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
 
-            var renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+            Instance = new PulsarMainWindow();
+            Instance.Run();
+            Instance.Dispose();
         }
+    }
+
+    public class PulsarMainWindow : ImGuiSDL2CSWindow
+    {
+        private GlobalUIState _state = new GlobalUIState();
+        private TextInputBuffer[] _TextInputBuffers;
+
+        private MemoryEditor _MemoryEditor = new MemoryEditor();
+        private byte[] _MemoryEditorData;
+
+        private FileDialog _Dialog = new FileDialog(false, false, true, false, false, false);
+
+        public PulsarMainWindow()
+            : base("Pulsar4X")
+        {
+
+            // Create any managed resources and set up the main game window here.
+            _MemoryEditorData = new byte[1024];
+            Random rnd = new Random();
+            for (int i = 0; i < _MemoryEditorData.Length; i++)
+            {
+                _MemoryEditorData[i] = (byte)rnd.Next(255);
+            }
+
+        }
+
+        public unsafe override void ImGuiLayout()
+        {
+            
+            // 1. Show a simple window
+            // Tip: if we don't call ImGui.Begin()/ImGui.End() the widgets appears in a window automatically called "Debug"
+            {
+                
+                ImGui.SetNextWindowSize(new ImVec2(200, 100), ImGuiCond.FirstUseEver);
+                if (_state.MainMenu.IsActive)
+                    _state.MainMenu.Display();
+                if (_state.NewGameOptions.IsActive)
+                    _state.NewGameOptions.Display();
+            }
+        }
+    }
+
+    internal class GlobalUIState
+    {
+        internal ECSLib.Game Game;
+        internal bool IsGameLoaded
+        {
+            get
+            {
+                return Game != null;
+            }
+        }
+
+
+        internal MainMenuItems MainMenu { get; }
+        internal NewGameOptions NewGameOptions { get; }
+
+        internal GlobalUIState()
+        {
+            MainMenu = new MainMenuItems(this);
+            NewGameOptions = new NewGameOptions(this);
+        }
+
+    }
+
+    public class MainMenuItems : PulsarGuiWindow
+    {
+
+        bool _saveGame = false;
+        GlobalUIState _state; 
+
+        internal MainMenuItems(GlobalUIState state)
+        {
+            IsActive = true;
+            _state = state;
+        }
+
+        internal override void Display()
+        {
+            
+            ImGui.SetNextWindowSize(new ImVec2(200, 100), ImGuiCond.FirstUseEver);
+
+            ImGui.Begin("Pulsar4X Main Menu", ref IsActive, ImGuiWindowFlags.NoTitleBar);
+
+            if (ImGui.Button("Start a New Game"))
+            {
+                _state.NewGameOptions.IsActive = true;
+                this.IsActive = false;
+            }
+            if (_state.IsGameLoaded)
+                if (ImGui.Button("Save Current Game"))
+                    _saveGame = !_saveGame;    
+            ImGui.Button("Resume a Current Game");
+            ImGui.Button("Connect to a Network Game");
+            //if (_saveGame)
+
+            ImGui.End();
+        }
+    }
+
+    public abstract class PulsarGuiWindow
+    {
+        internal bool IsActive = false;
+        internal abstract void Display();
     }
 }
