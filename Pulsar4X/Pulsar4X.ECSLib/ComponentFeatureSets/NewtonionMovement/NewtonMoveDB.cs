af9d8b980117dd7e3ccba5a845b2402f07fed41f
se5a <jon.se5a@gmail.com>
2019-05-11 07:53:26 +1200

    * NewtonBalisticProcessor.cs: Removed old unused newtonBalistic
    * NewtonMoveDB.cs:
    * NewtonionMovementProcessor.cs: Moved NewtonMoveDB to it's own file

---
+++ Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/NewtonionMovement/NewtonMoveDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pulsar4X.ECSLib
+{
+    public class NewtonMoveDB : BaseDataBlob
+    {
+        //internal DateTime TimeSinceLastRun { get; set; }
+        public Vector4 ThrustVector { get; internal set; } = Vector4.Zero;
+        public Vector4 CurrentVector_kms { get; internal set; }
+
+        public Entity SOIParent { get; internal set; }
+        public double ParentMass { get; internal set; }
+
+        public NewtonMoveDB() { }
+
+        public NewtonMoveDB(Entity sphereOfInfluenceParent)
+        {
+            SOIParent = sphereOfInfluenceParent;
+            ParentMass = SOIParent.GetDataBlob<MassVolumeDB>().Mass;
+        }
+
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
