03c564a62bd892a1461b1efcc76b344d11f39943
agm-114 <agm.114.email@gmail.com>
2020-03-12 09:39:02 -0400

    Fixed ship designer memory leak

--- Pulsar4X/Pulsar4X.ImGuiNetUI/EmpireManagement/ShipDesign.cs
+++ Pulsar4X/Pulsar4X.ImGuiNetUI/EmpireManagement/ShipDesign.cs
@@ -29,7 +29,6 @@ namespace Pulsar4X.SDL2UI
  
         List<(ComponentDesign design, int count)> _shipComponents = new List<(ComponentDesign design, int count)>();
         
-        private EntityDamageProfileDB _profile;
         private RawBmp _rawShipImage;
         private IntPtr _shipImgPtr;
         
@@ -227,14 +226,13 @@ namespace Pulsar4X.SDL2UI
                     string name = _exsistingClasses[i].Name;
                     if (ImGui.Selectable(name))
                     {
+                         
                         _selectedDesign = i;
                         _designName = ImGuiSDL2CSHelper.BytesFromString(_exsistingClasses[i].Name, 32);
                         _shipComponents = _exsistingClasses[i].Components;
                         _armor = _exsistingClasses[i].Armor;
-                        _profile = new EntityDamageProfileDB(_shipComponents, _armor);
-                        _rawShipImage = _profile.DamageProfile;
-                        _shipImgPtr = SDL2Helper.CreateSDLTexture(_state.rendererPtr, _rawShipImage);
-
+                        EntityDamageProfileDB _profile = new EntityDamageProfileDB(_shipComponents, _armor);
+                        _shipImgPtr = SDL2Helper.CreateSDLTexture(_state.rendererPtr, _profile.DamageProfile);
                         _armorNames.Contains(_armor.name);
                         _armorIndex = _armorSelection.FindIndex(foo => foo.name.Equals(_armor.name));
                         designChanged = true;
@@ -445,10 +443,9 @@ namespace Pulsar4X.SDL2UI
             ImGui.Text("Ship Stats");
             if (designChanged)
             {
-                _profile = new EntityDamageProfileDB(_shipComponents, _armor);
-                _rawShipImage = _profile.DamageProfile;
+                EntityDamageProfileDB _profile = new EntityDamageProfileDB(_shipComponents, _armor);
 
-                _shipImgPtr = SDL2Helper.CreateSDLTexture(_state.rendererPtr, _rawShipImage);
+                _shipImgPtr = SDL2Helper.CreateSDLTexture(_state.rendererPtr, _profile.DamageProfile);
 
                 double mass = 0;
                 double fu = 0;
