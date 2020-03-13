3d09c0b13a6112d10c3956f5d10c513897becbac
AGM-114 <agm.114.email@gmail.com>
2020-03-12 20:44:18 -0400

    Changed the armor system to use straight lines

--- Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/Damage/ComponentPlacement.cs
+++ Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/Damage/ComponentPlacement.cs
@@ -8,18 +8,18 @@ namespace Pulsar4X.ECSLib.ComponentFeatureSets.Damage
 {
     public static class ComponentPlacement
     {
+
         public static RawBmp CreateShipBmp(EntityDamageProfileDB shipProfile)
         {
             byte armorID = 255;//shipProfile.Armor.IDCode;
             var po = shipProfile.PlacementOrder;
             
             List<(Guid typeID, RawBmp bmp)> typeBitmaps = shipProfile.TypeBitmaps;
+            List<(int width, int height)> partsize = new List<(int width, int height)>();
             int componentWidthNum = 0;
 
             int totalLen = 0;
             var totalWidth = 0;
-            int widestPoint = 0;
-            int widestLen = 0;
 
             byte componentInstance = 0;
             
@@ -40,8 +40,6 @@ namespace Pulsar4X.ECSLib.ComponentFeatureSets.Damage
                 if (width > totalWidth)
                 {
                     totalWidth = width;
-                    widestPoint = totalLen;
-                    widestLen = typeBmp.Width;
                 }
             }
 
@@ -79,7 +77,9 @@ namespace Pulsar4X.ECSLib.ComponentFeatureSets.Damage
                 var typeid = po[i].id;
                 var count = po[i].count;
                 int maxPixHeight = typeBmp.Height * count;
-                
+
+                partsize.Add(((int)typeBmp.Width, (int)typeBmp.Height * count));
+
                 for (int o = 0; o < count; o++)
                 {
 
@@ -121,17 +121,7 @@ namespace Pulsar4X.ECSLib.ComponentFeatureSets.Damage
             //shipBmp.ByteArray = shipByteArray;
 
             //TODO: somehow make lines thicker without crashing
-           
-
-            //below is a failed attempt at trying to make lines thicker
-            //int bezzierMargin = 50;
-            //widestPoint -= bezzierMargin;
-            //widestLen -= bezzierMargin
-            //bool canvasWidthIsBigEnough = canvasWidth > bezzierMargin;
-            //bool canvasLenIsBigEnough = canvasLen > bezzierMargin;
-            //canvasWidth = (canvasWidthIsBigEnough ? canvasWidth - bezzierMargin : canvasWidth);
-            //canvasLen = (canvasLenIsBigEnough?canvasLen-bezzierMargin:canvasLen);
-            //((canvasWidthIsBigEnough&&canvasLenIsBigEnough)?4:0);
+          
 
 
             float addedLineThickness = 5;
@@ -150,90 +140,67 @@ namespace Pulsar4X.ECSLib.ComponentFeatureSets.Damage
             }
 
             shipBmp = finalShipBmp;
-           
-
-
-            //create bezzier control points for front and rear armor/skin
-
-            (int x, int y)[] controlPointsFore = new (int x, int y)[4];
-
-            controlPointsFore[0] = (0, canvasWidth / 2);
-            controlPointsFore[1] = (0, 2);
-            controlPointsFore[2] = (0, 2);
-            controlPointsFore[3] = (widestPoint - widestLen, 2);
-            
-            (int x, int y)[] controlPointsAft = new (int x, int y)[4];
-            int lastbmpHeight = typeBitmaps[po.Count -1].bmp.Height * po[po.Count -1].count / 2;
-            int lastbmpLen = typeBitmaps[po.Count -1].bmp.Width ;
-            
-            controlPointsAft[0] = (widestPoint + 3 , 2); //add three for the extra canvas size
-            controlPointsAft[1] = (totalLen - lastbmpLen, 2 + lastbmpHeight);
-            controlPointsAft[2] = (totalLen - lastbmpLen, 2 + lastbmpHeight);
-            controlPointsAft[3] = (canvasLen, canvasWidth / 2 -  lastbmpHeight);
-            
+          
             List<(int x, int y)> linePoints = new List<(int x, int y)>();
-            float dt = 0.01f;
-            for (float t = 0.0f; t < 1.0; t += dt)
-            {
-                var x = BezCalc(t, controlPointsFore[0].x, controlPointsFore[1].x, controlPointsFore[2].x, controlPointsFore[3].x);
-                var y = BezCalc(t, controlPointsFore[0].y, controlPointsFore[1].y, controlPointsFore[2].y, controlPointsFore[3].y);
-                
-                linePoints.Add(((int)x, (int)y));
-                
-            }
+            List<int> lineHeight = new List<int>();
             
-            for (float t = 0.0f; t < 1.0; t += dt)
+
+            int spacer = -5;
+
+            int numparts = partsize.Count();
+
+
+            //Grabs the height of each transtion between parts
+            for (int partnum = 0; partnum < numparts - 1; partnum++)
             {
-                var x = BezCalc(t, controlPointsAft[0].x, controlPointsAft[1].x, controlPointsAft[2].x, controlPointsAft[3].x);
-                var y = BezCalc(t, controlPointsAft[0].y, controlPointsAft[1].y, controlPointsAft[2].y, controlPointsAft[3].y);
-                
-                linePoints.Add(((int)x, (int)y));
-                
+                if (partsize[partnum].height > partsize[partnum + 1].height)
+                    lineHeight.Add(partsize[partnum].height / 2);
+                else
+                    lineHeight.Add(partsize[partnum + 1].height / 2);
             }
-            
-            //set the pixels in teh bitmap for the bezier curves.
-            var coordStart = linePoints[0];
+            lineHeight.Add(partsize[numparts - 1].height / 2);
+
+            linePoints.Add((0, 0));
+
+            //Adds a point at each transtions
+            int currentx = 0;
+            for (int partnum = 0; partnum < numparts; partnum++)
+            {
+                currentx += partsize[partnum].width;
+                linePoints.Add((currentx, lineHeight[partnum] + spacer));        
+            }
+
+
+            var halfwidth = canvasWidth / 2;
+
+            var bottomcoordStart = linePoints[0];
+            var topcoordStart = linePoints[0];
+            bottomcoordStart = (bottomcoordStart.x, halfwidth + bottomcoordStart.y);
+            topcoordStart = (topcoordStart.x, halfwidth - topcoordStart.y);
             for (int i = 1; i < linePoints.Count; i++)
             {
-                var coordEnd = linePoints[i];
-                ThickLine(shipBmp, coordStart, coordEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
-                //Mirror:
-                coordStart = (coordStart.x, canvasWidth - coordStart.y);
-               
-                coordEnd = (coordEnd.x, canvasWidth - coordEnd.y);
-                
-                ThickLine(shipBmp, coordStart, coordEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
-                coordStart = linePoints[i];
+                //Draws the bottom line
+                var bottomcoordEnd = linePoints[i];
+                bottomcoordEnd = (bottomcoordEnd.x, halfwidth + bottomcoordEnd.y);
+
+                ThickLine(shipBmp, bottomcoordStart, bottomcoordEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
+                bottomcoordStart = bottomcoordEnd;
+
+                //Draws the top line
+                var topcoordEnd = linePoints[i];
+                topcoordEnd = (topcoordEnd.x, halfwidth - topcoordEnd.y);
+
+                ThickLine(shipBmp, topcoordStart, topcoordEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
+                topcoordStart = topcoordEnd;
             }
             
-            //connect the front and rear armor/skin.
-            //TODO: handle angles, currently expects start and end y to be the same.
-            (int x, int y) straightStart = controlPointsFore[3];
-            (int x, int y) straightEnd = controlPointsAft[0];
-            
-            ThickLine(shipBmp, straightStart, straightEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
-            //Mirror:
-            straightStart = (straightStart.x, canvasWidth - straightStart.y);
-            straightEnd = (straightEnd.x, canvasWidth - straightEnd.y);
-            ThickLine(shipBmp, straightStart, straightEnd,  shipProfile.Armor.thickness / 10 + addedLineThickness, 255, 255, 255, 255, shipbmpMargins);
 
-
-            
              
 
             shipProfile.DamageProfile = shipBmp;
             return shipBmp;
         }
-        
-        private static double BezCalc(double t, double a0, double a1, double a2, double a3)
-        {
-            double foo = a0 * Math.Pow((1 - t), 3); 
-            foo += a1 * 3 * t * Math.Pow((1 - t), 2); 
-            foo += a2 * 3 * Math.Pow(t, 2) * (1 - t); 
-            foo += a3 * Math.Pow(t, 3);
-            return foo;
-        }
-
+       
 
 
         static void ThickLine(RawBmp bmp, (int x, int y) coordStart, (int x, int y) coordEnd, float wd, byte r, byte g, byte b, byte a, Vector2 margins)
