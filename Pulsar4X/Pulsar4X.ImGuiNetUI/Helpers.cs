47bf36d5d3e1a30034e05b90f293beeb24e81a9a
se5a <jon.se5a@gmail.com>
2020-04-22 09:59:04 +1200

    Nested BoarderGroup.

--- Pulsar4X/Pulsar4X.ImGuiNetUI/Helpers.cs
+++ Pulsar4X/Pulsar4X.ImGuiNetUI/Helpers.cs
@@ -28,19 +28,26 @@ namespace Pulsar4X.SDL2UI
 
     public class BorderGroup
     {
-        private static Vector2 _startPos;
-        private static Vector2 _labelSize;
-        private static uint _colour;
+        private static Vector2[] _startPos = new Vector2[8];
+        private static Vector2[] _labelSize = new Vector2[8];
+        private static uint[] _colour = new uint[8];
+        private static byte _nestIndex = 0;
+        private static float _dentMulitpier = 3;
         public static void BeginBorder(string label, uint colour)
         {
             ImGui.PushID(label);
-            _colour = colour;
-            _startPos = ImGui.GetCursorScreenPos();
-            _startPos.X -= 3;
-            _startPos.Y += ImGui.GetTextLineHeight() * 0.5f;
+            
+            _colour[_nestIndex] = colour;
+            _startPos[_nestIndex] = ImGui.GetCursorScreenPos();
+            _startPos[_nestIndex].X -= 3;
+            _startPos[_nestIndex].Y += ImGui.GetTextLineHeight() * 0.5f;
             ImGui.Text(label);
-            _labelSize = ImGui.GetItemRectSize();
+            _labelSize[_nestIndex] = ImGui.GetItemRectSize();
+            _nestIndex++;
+            ImGui.Indent(_dentMulitpier * _nestIndex);
         }
+
+
 
         public static void BeginBorder(string label)
         {
@@ -54,20 +61,26 @@ namespace Pulsar4X.SDL2UI
 
         public static void EndBoarder()
         { 
-            Vector2 size = new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetCursorScreenPos().Y - _startPos.Y);
+            ImGui.Unindent(_dentMulitpier * _nestIndex);
+            _nestIndex--;
+            Vector2 size = new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetCursorScreenPos().Y - _startPos[_nestIndex].Y);
             //ImGui.get
             ImDrawListPtr wdl = ImGui.GetWindowDrawList();
 
+            float by = _startPos[_nestIndex].Y + size.Y -_dentMulitpier * _nestIndex;
+            float rx = _startPos[_nestIndex].X + size.X - _dentMulitpier * _nestIndex;
+            
             Vector2[] pts = new Vector2[6];
-            pts[0] = new Vector2(_startPos.X + 2, _startPos.Y);
-            pts[1] = _startPos; //top left
-            pts[2] = new Vector2(_startPos.X, _startPos.Y + size.Y); //bottom left
-            pts[3] = new Vector2(_startPos.X + size.X + 3, _startPos.Y + size.Y); //bottom right
-            pts[4] = new Vector2(_startPos.X + size.X + 3, _startPos.Y);
-            pts[5] = new Vector2(_startPos.X + _labelSize.X + 3, _startPos.Y);
-            wdl.AddPolyline(ref pts[0], pts.Length, _colour, false, 1.0f);
+            pts[0] = new Vector2(_startPos[_nestIndex].X + _dentMulitpier, _startPos[_nestIndex].Y);
+            pts[1] = _startPos[_nestIndex]; //top left
+            pts[2] = new Vector2(_startPos[_nestIndex].X, by); //bottom left
+            pts[3] = new Vector2(rx, by); //bottom right
+            pts[4] = new Vector2(rx, _startPos[_nestIndex].Y); //top right
+            pts[5] = new Vector2(_startPos[_nestIndex].X + _labelSize[_nestIndex].X + _dentMulitpier, _startPos[_nestIndex].Y);
+            wdl.AddPolyline(ref pts[0], pts.Length, _colour[_nestIndex], false, 1.0f);
+            
             ImGui.PopID();
-
+            
         }
     }
     
