ddc08c413bc856f0934bd716fbaad83bcc5c7319
se5a <jon.se5a@gmail.com>
2022-06-06 16:34:01 +1200

    Updated windows SDL2 libraries to 2.0.22

--- Pulsar4X/Pulsar4X.ImGuiNetUI/IMGUISDL/SDL2.cs
+++ Pulsar4X/Pulsar4X.ImGuiNetUI/IMGUISDL/SDL2.cs
@@ -1124,7 +1124,7 @@ namespace SDL2
 		 */
 		public const int SDL_MAJOR_VERSION =	2;
 		public const int SDL_MINOR_VERSION =	0;
-		public const int SDL_PATCHLEVEL =	12;
+		public const int SDL_PATCHLEVEL =	22;
 
 		public static readonly int SDL_COMPILEDVERSION = SDL_VERSIONNUM(
 			SDL_MAJOR_VERSION,
