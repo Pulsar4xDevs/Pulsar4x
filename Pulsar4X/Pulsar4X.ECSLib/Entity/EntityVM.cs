56190dab21005071bdc07081aaaf7dcdd55e3674
Justin Brown <behindcurtain3@gmail.com>
2023-08-14 18:54:54 -0700

    Remove OldViewModels

--- Pulsar4X/Pulsar4X.ECSLib/Entity/EntityVM.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Entity/EntityVM.cs
@@ -1,131 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Collections.ObjectModel;
+// using System.Linq;
 
-namespace Pulsar4X.ECSLib
-{    
-    public class EntityVM : ViewModelBase
-    {
-        Game _game;
-        private Entity _entity;
-        private bool _hasEntity = false;
-        public bool HasEntity 
-        {   get {return _hasEntity;}
-            set {
-                if(_hasEntity != value)
-                {
-                    _hasEntity = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+// namespace Pulsar4X.ECSLib
+// {    
+//     public class EntityVM : ViewModelBase
+//     {
+//         Game _game;
+//         private Entity _entity;
+//         private bool _hasEntity = false;
+//         public bool HasEntity 
+//         {   get {return _hasEntity;}
+//             set {
+//                 if(_hasEntity != value)
+//                 {
+//                     _hasEntity = value;
+//                     OnPropertyChanged();
+//                 }
+//             }
+//         }
 
-        public string Guid { get { return _entity.Guid.ToString(); } }
-        public string EntityName { get; set; }
+//         public string Guid { get { return _entity.Guid.ToString(); } }
+//         public string EntityName { get; set; }
 
-        Dictionary<ICreateViewmodel, IDBViewmodel> _viewmodelDict = new Dictionary<ICreateViewmodel, IDBViewmodel>();
-        public ObservableCollection<IDBViewmodel> Viewmodels = new ObservableCollection<IDBViewmodel>();
+//         Dictionary<ICreateViewmodel, IDBViewmodel> _viewmodelDict = new Dictionary<ICreateViewmodel, IDBViewmodel>();
+//         public ObservableCollection<IDBViewmodel> Viewmodels = new ObservableCollection<IDBViewmodel>();
 
-        public DictionaryVM<Guid, string> SelectableEntites { get; } = new DictionaryVM<Guid, string>();
-        private Dictionary<Guid, Entity> _selectableEntitys { get; } = new Dictionary<Guid, Entity>();
+//         public DictionaryVM<Guid, string> SelectableEntites { get; } = new DictionaryVM<Guid, string>();
+//         private Dictionary<Guid, Entity> _selectableEntitys { get; } = new Dictionary<Guid, Entity>();
 
-        internal CommandReferences CmdRef { get; private set; }
+//         internal CommandReferences CmdRef { get; private set; }
 
-        public EntityVM(GameVM gamevm) 
-        {
-            _game = gamevm.Game;
-            if(gamevm.CurrentFaction != null && gamevm.CurrentFaction.IsValid)
-                Init(gamevm.CurrentFaction);
-        }
+//         public EntityVM(GameVM gamevm) 
+//         {
+//             _game = gamevm.Game;
+//             if(gamevm.CurrentFaction != null && gamevm.CurrentFaction.IsValid)
+//                 Init(gamevm.CurrentFaction);
+//         }
 
-        public void Init(Entity faction)
-        {
-            var possibleEntites = faction.GetDataBlob<FactionOwnerDB>().OwnedEntities.Values;
-            SelectableEntites.Clear();
-            _selectableEntitys.Clear();
-            foreach(var entity in possibleEntites)
-            {
-                if (entity.HasDataBlob<NameDB>())
-                {
-                    string name = entity.GetDataBlob<NameDB>().DefaultName;
-                    SelectableEntites.Add(entity.Guid, name);
-                    _selectableEntitys.Add(entity.Guid, entity);
-                }
-            }
-            SelectableEntites.DisplayMode = DisplayMode.Value;
-            SelectableEntites.SelectionChangedEvent += OnEntitySelected;
-            SelectableEntites.SelectedIndex = 0;
-            //HasEntity = true;
-        }
+//         public void Init(Entity faction)
+//         {
+//             var possibleEntites = faction.GetDataBlob<FactionOwnerDB>().OwnedEntities.Values;
+//             SelectableEntites.Clear();
+//             _selectableEntitys.Clear();
+//             foreach(var entity in possibleEntites)
+//             {
+//                 if (entity.HasDataBlob<NameDB>())
+//                 {
+//                     string name = entity.GetDataBlob<NameDB>().DefaultName;
+//                     SelectableEntites.Add(entity.Guid, name);
+//                     _selectableEntitys.Add(entity.Guid, entity);
+//                 }
+//             }
+//             SelectableEntites.DisplayMode = DisplayMode.Value;
+//             SelectableEntites.SelectionChangedEvent += OnEntitySelected;
+//             SelectableEntites.SelectedIndex = 0;
+//             //HasEntity = true;
+//         }
 
-        private void OnEntitySelected(int oldindex, int newindex)
-        {
-            Viewmodels.Clear();
-            _viewmodelDict.Clear();
-            Guid key = SelectableEntites.GetKey(newindex);
-            _entity = _selectableEntitys[key];
-            CmdRef = new CommandReferences(_entity.FactionOwnerID, _entity.Guid, _game.OrderHandler, _entity.Manager.ManagerSubpulses);
-            HasEntity = true;
-            _entity.Manager.ManagerSubpulses.SystemDateChangedEvent += OnSystemDateChange;
-            Update();
+//         private void OnEntitySelected(int oldindex, int newindex)
+//         {
+//             Viewmodels.Clear();
+//             _viewmodelDict.Clear();
+//             Guid key = SelectableEntites.GetKey(newindex);
+//             _entity = _selectableEntitys[key];
+//             CmdRef = new CommandReferences(_entity.FactionOwnerID, _entity.Guid, _game.OrderHandler, _entity.Manager.ManagerSubpulses);
+//             HasEntity = true;
+//             _entity.Manager.ManagerSubpulses.SystemDateChangedEvent += OnSystemDateChange;
+//             Update();
 
-        }
+//         }
 
-        private void OnSystemDateChange(DateTime newDate)
-        {
-            Update();
-        }
+//         private void OnSystemDateChange(DateTime newDate)
+//         {
+//             Update();
+//         }
 
-        internal void Update()
-        {
+//         internal void Update()
+//         {
 
-            foreach(ICreateViewmodel datablob in _entity.DataBlobs.Where(item => item is ICreateViewmodel))
-            {
-                if(datablob is ICreateViewmodel &!_viewmodelDict.ContainsKey(datablob))
-                {
-                    var newvm = datablob.CreateVM(_game, CmdRef);
+//             foreach(ICreateViewmodel datablob in _entity.DataBlobs.Where(item => item is ICreateViewmodel))
+//             {
+//                 if(datablob is ICreateViewmodel &!_viewmodelDict.ContainsKey(datablob))
+//                 {
+//                     var newvm = datablob.CreateVM(_game, CmdRef);
 
-                    Viewmodels.Add(newvm);
-                    _viewmodelDict.Add(datablob, newvm);
-                }
-            }
-            foreach(var datablobAsKey in _viewmodelDict.Keys.ToArray())
-            {
-                if(!_entity.DataBlobs.Contains((BaseDataBlob)datablobAsKey))
-                {
-                    Viewmodels.Remove(_viewmodelDict[datablobAsKey]);
-                    _viewmodelDict.Remove(datablobAsKey); 
-                }
-            }
+//                     Viewmodels.Add(newvm);
+//                     _viewmodelDict.Add(datablob, newvm);
+//                 }
+//             }
+//             foreach(var datablobAsKey in _viewmodelDict.Keys.ToArray())
+//             {
+//                 if(!_entity.DataBlobs.Contains((BaseDataBlob)datablobAsKey))
+//                 {
+//                     Viewmodels.Remove(_viewmodelDict[datablobAsKey]);
+//                     _viewmodelDict.Remove(datablobAsKey); 
+//                 }
+//             }
 
-            foreach(var viewmodel in Viewmodels)
-            {
-                viewmodel.Update();
-            }
-            OnPropertyChanged(nameof(HasEntity));
-        }
-    }
+//             foreach(var viewmodel in Viewmodels)
+//             {
+//                 viewmodel.Update();
+//             }
+//             OnPropertyChanged(nameof(HasEntity));
+//         }
+//     }
 
-    public class EntityNameVM
-    {
-        private Entity _entity;
-        internal Entity GetEntity { get { return _entity; } }
-        internal System.Guid GetEntityGuid {get {return _entity.Guid;}}
-        public string Name { get { return _entity.GetDataBlob<NameDB>().DefaultName; } }
-        internal EntityNameVM(Entity entity)
-        { _entity = entity; }
+//     public class EntityNameVM
+//     {
+//         private Entity _entity;
+//         internal Entity GetEntity { get { return _entity; } }
+//         internal System.Guid GetEntityGuid {get {return _entity.Guid;}}
+//         public string Name { get { return _entity.GetDataBlob<NameDB>().DefaultName; } }
+//         internal EntityNameVM(Entity entity)
+//         { _entity = entity; }
 
-    }
+//     }
 
-    public interface IDBViewmodel
-    {
-        void Update();
-    }
+//     public interface IDBViewmodel
+//     {
+//         void Update();
+//     }
 
 
-    internal interface ICreateViewmodel
-    {
-        IDBViewmodel CreateVM(Game game, CommandReferences cmdRef);
-    }
-}
+//     internal interface ICreateViewmodel
+//     {
+//         IDBViewmodel CreateVM(Game game, CommandReferences cmdRef);
+//     }
+// }
--- Pulsar4X/Pulsar4X.Tests/NetworkTests.cs
+++ Pulsar4X/Pulsar4X.Tests/NetworkTests.cs
@@ -1,38 +1,38 @@
-﻿using NUnit.Framework;
-using Pulsar4X.ECSLib;
-using System;
+﻿// using NUnit.Framework;
+// using Pulsar4X.ECSLib;
+// using System;
 
-namespace Pulsar4X.Tests
-{
-    [Description("Network Tests")]
-    public class NetworkTests
-    {
-        TestGame _host;
+// namespace Pulsar4X.Tests
+// {
+//     [Description("Network Tests")]
+//     public class NetworkTests
+//     {
+//         TestGame _host;
 
-        Game _client;
-        Networking.NetworkClient _netClient;
-        private DateTime _currentDateTime { get { return StaticRefLib.CurrentDateTime; } }
+//         Game _client;
+//         Networking.NetworkClient _netClient;
+//         private DateTime _currentDateTime { get { return StaticRefLib.CurrentDateTime; } }
 
 
 
-        [SetUp]
-        public void Init()
-        {
-            _host = new TestGame(1);
-            //_host.Game.OrderHandler = new ServerOrderHandler(_host.Game, 4888);
+//         [SetUp]
+//         public void Init()
+//         {
+//             _host = new TestGame(1);
+//             //_host.Game.OrderHandler = new ServerOrderHandler(_host.Game, 4888);
 
-            _client = new Game();
-            _netClient = new Networking.NetworkClient("localhost", 4888, new GameVM());//ugly, refactor so GameVM isn't required and or network isnt needed to be created here?
-            //_client.OrderHandler = new ClientOrderHandler(_client, _netClient); 
-        }
+//             _client = new Game();
+//             _netClient = new Networking.NetworkClient("localhost", 4888, new GameVM());//ugly, refactor so GameVM isn't required and or network isnt needed to be created here?
+//             //_client.OrderHandler = new ClientOrderHandler(_client, _netClient); 
+//         }
 
-        //[Test] 
-        public void TestFactionConnect()
-        {
-            _netClient.ClientConnect();
-            //TODO: set up events so we can listen for updates. 
-            //_netClient.Messages.Contains(
-            //_netClient.SendFactionDataRequest("New Terran Utopian Empire", "");//name is hardcoded in TestGame.cs
-        }
-    }
-}
+//         //[Test] 
+//         public void TestFactionConnect()
+//         {
+//             _netClient.ClientConnect();
+//             //TODO: set up events so we can listen for updates. 
+//             //_netClient.Messages.Contains(
+//             //_netClient.SendFactionDataRequest("New Terran Utopian Empire", "");//name is hardcoded in TestGame.cs
+//         }
+//     }
+// }
