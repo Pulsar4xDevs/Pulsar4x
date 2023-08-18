d39147f798f473e28ba12c82b3ced98088b2b9c1
Justin Brown <behindcurtain3@gmail.com>
2023-08-18 12:26:31 -0700

    Add Installations to EconomicsWindow

--- Pulsar4X/Pulsar4X.ImGuiNetUI/DisplayExtensions/ComponentInstancesDBDisplay.cs
+++ Pulsar4X/Pulsar4X.ImGuiNetUI/DisplayExtensions/ComponentInstancesDBDisplay.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Numerics;
 using ImGuiNET;
 using Pulsar4X.ECSLib;
 
@@ -5,40 +7,75 @@ namespace Pulsar4X.SDL2UI
 {
     public static class ComponentInstancesDBDisplay
     {
+        public static Vector4 GoodColor = new (0.25f, 1f, 0.25f, 0.9f);
+        public static Vector4 OkColor = new (1.0f, 1.0f, 0.25f, 0.9f);
+        public static Vector4 BadColor = new (1.0f, 0.25f, 0.25f, 0.9f);
         public static void Display(this ComponentInstancesDB db, EntityState entityState, GlobalUIState uiState)
         {
-            ImGui.Columns(3); 
+            ImGui.Columns(3);
             ImGui.SetColumnWidth(0, 164);
             ImGui.Text("Type");
             ImGui.NextColumn();
-            ImGui.SetColumnWidth(1, 62);
-            ImGui.Text("Health");
+            ImGui.SetColumnWidth(1, 48);
+            ImGui.Text("#");
             ImGui.NextColumn();
-            ImGui.SetColumnWidth(2, 62);
-            ImGui.Text("Enabled");
+            ImGui.SetColumnWidth(2, 100);
+            ImGui.Text("Status");
             ImGui.NextColumn();
+            ImGui.Separator();
 
-            foreach(var listPerDesign in db.ComponentsByDesign.Values)
+            foreach(var (designID, listPerDesign) in db.ComponentsByDesign)
             {
-                foreach(var instance in listPerDesign)
+                if(listPerDesign.Count == 0) continue;
+
+                ImGui.Text(listPerDesign[0].Name);
+                ImGui.NextColumn();
+                ImGui.Text(listPerDesign.Count.ToString());
+                ImGui.NextColumn();
+
+                var onCount = listPerDesign.Where(x => x.IsEnabled).Count();
+                var offCount = listPerDesign.Where(x => !x.IsEnabled).Count();
+
+                if(onCount > 0 && offCount > 0)
                 {
-                    ImGui.Text(instance.Name);
-
-                    ImGui.NextColumn();
-                    ImGui.Text((100 * instance.HealthPercent()) + "%%");
-
-                    ImGui.NextColumn();
-                    if(instance.IsEnabled)
-                    {
-                        ImGui.Text("On");
-                    }
-                    else
-                    {
-                        ImGui.Text("Off");
-                    }
-
-                    ImGui.NextColumn();
+                    ImGui.PushStyleColor(ImGuiCol.Text, OkColor);
+                    ImGui.Text("Degraded");
+                    ImGui.PopStyleColor();
                 }
+                else if(onCount == 0)
+                {
+                    ImGui.PushStyleColor(ImGuiCol.Text, BadColor);
+                    ImGui.Text("Disabled");
+                    ImGui.PopStyleColor();
+                }
+                else
+                {
+                    ImGui.PushStyleColor(ImGuiCol.Text, GoodColor);
+                    ImGui.Text("Operational");
+                    ImGui.PopStyleColor();
+                }
+
+                ImGui.NextColumn();
+
+                // foreach(var instance in listPerDesign)
+                // {
+                //     ImGui.Text(instance.Name);
+
+                //     ImGui.NextColumn();
+                //     ImGui.Text((100 * instance.HealthPercent()) + "%%");
+
+                //     ImGui.NextColumn();
+                //     if(instance.IsEnabled)
+                //     {
+                //         ImGui.Text("On");
+                //     }
+                //     else
+                //     {
+                //         ImGui.Text("Off");
+                //     }
+
+                //     ImGui.NextColumn();
+                // }
             }
             ImGui.Columns(1);
         }
--- Pulsar4X/Pulsar4X.ImGuiNetUI/EmpireManagement/EconomicsWindow.cs
+++ Pulsar4X/Pulsar4X.ImGuiNetUI/EmpireManagement/EconomicsWindow.cs
@@ -86,7 +86,7 @@ namespace Pulsar4X.SDL2UI
                         selectedEntity.Entity.DisplaySummary(selectedEntity, _uiState);
                         ImGui.EndTabItem();
                     }
-                    if(ImGui.BeginTabItem("Industry"))
+                    if(ImGui.BeginTabItem("Construction"))
                     {
                         selectedEntity.Entity.DisplayIndustry(selectedEntity, _uiState);
                         ImGui.EndTabItem();
