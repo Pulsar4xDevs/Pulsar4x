c4144bac218766e37081ea3e32358a325b48c306
Justin Brown <behindcurtain3@gmail.com>
2023-08-30 22:28:08 -0700

    Move StandAloneOrderHandler to its own file

--- Pulsar4X/Pulsar4X.ECSLib/Orders/IOrderHandler.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Orders/IOrderHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Pulsar4X.ECSLib
+﻿namespace Pulsar4X.ECSLib
 {
     public interface IOrderHandler
     {
@@ -8,39 +6,4 @@ namespace Pulsar4X.ECSLib
 
         void HandleOrder(EntityCommand entityCommand);
     }
-
-
-    internal class StandAloneOrderHandler:IOrderHandler
-    {
-        internal StandAloneOrderHandler(Game game)
-        {
-            Game = game;
-            game.OrderHandler = this;
-        }
-
-        public Game Game { get; private set; }
-
-        public void HandleOrder(EntityCommand entityCommand)
-        {
-            if (entityCommand.IsValidCommand(Game))
-            {
-                if (entityCommand.UseActionLanes)
-                {
-                    var orderableDB = entityCommand.EntityCommanding.GetDataBlob<OrderableDB>();
-                    orderableDB.AddCommandToList(entityCommand);
-                    StaticRefLib.ProcessorManager.GetInstanceProcessor(nameof(OrderableProcessor)).ProcessEntity(orderableDB.OwningEntity, StaticRefLib.CurrentDateTime);
-                }
-                else
-                {
-                    if(entityCommand.EntityCommanding.StarSysDateTime >= entityCommand.ActionOnDate)
-                        entityCommand.ActionCommand(entityCommand.EntityCommanding.StarSysDateTime);
-                    else
-                    {
-                        entityCommand.EntityCommanding.Manager.ManagerSubpulses.AddEntityInterupt(entityCommand.ActionOnDate, nameof(OrderableProcessor), entityCommand.EntityCommanding);
-                    }
-                }
-            }
-        }
-    }
-
 }
