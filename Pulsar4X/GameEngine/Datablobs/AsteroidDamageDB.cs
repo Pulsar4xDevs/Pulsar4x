6c0421fd653b10aa9dd454a5a507550aac083e53
DifferentLevelDan <DifferentLevelDan@users.noreply.github.com>
2023-11-01 18:03:10 -0500

    Add 62 new failing tests for IEquatable DataBlob implementation. Autogenerated AsteroidDamageDB's for review.

--- Pulsar4X/GameEngine/Datablobs/AsteroidDamageDB.cs
+++ Pulsar4X/GameEngine/Datablobs/AsteroidDamageDB.cs
@@ -9,7 +9,7 @@ using Pulsar4X.DataStructures;
 
 namespace Pulsar4X.Datablobs
 {
-    public class AsteroidDamageDB : BaseDataBlob
+    public class AsteroidDamageDB : BaseDataBlob, IEquatable<AsteroidDamageDB>
     {
         /// <summary>
         /// Asteroids are damageable and need to store their health value.
@@ -48,6 +48,41 @@ namespace Pulsar4X.Datablobs
         public override object Clone()
         {
             return new AsteroidDamageDB(this);
+        }
+
+        public bool Equals(AsteroidDamageDB? other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return _health == other._health && FractureChance.Equals(other.FractureChance);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((AsteroidDamageDB)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_health, FractureChance);
+        }
+
+        public static bool operator ==(AsteroidDamageDB? left, AsteroidDamageDB? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(AsteroidDamageDB? left, AsteroidDamageDB? right)
+        {
+            return !Equals(left, right);
         }
     }
 }
