719895f3604c68476bd6921c96cd6442b4fbb75a
Justin Brown <behindcurtain3@gmail.com>
2023-12-10 21:37:53 -0800

    Split GeoSurveyAbilityAtbDB into separate Atb and DB classes

---
+++ Pulsar4X/GameEngine/Atb/GeoSurveyAtb.cs
@@ -0,0 +1,52 @@
+using Newtonsoft.Json;
+using Pulsar4X.Components;
+using Pulsar4X.Datablobs;
+using Pulsar4X.Engine;
+using Pulsar4X.Interfaces;
+
+namespace Pulsar4X.Atb;
+
+public class GeoSurveyAtb : IComponentDesignAttribute
+{
+    [JsonProperty]
+    public int Speed { get; set; } = 1;
+
+    public GeoSurveyAtb(int speed)
+    {
+        Speed = speed;
+    }
+
+    public void OnComponentInstallation(Entity parentEntity, ComponentInstance componentInstance)
+    {
+        if(parentEntity.TryGetDatablob<GeoSurveyAbilityDB>(out var geoSurveyAbilityDB))
+        {
+            geoSurveyAbilityDB.Speed += Speed;
+        }
+        else
+        {
+            parentEntity.SetDataBlob<GeoSurveyAbilityDB>(new GeoSurveyAbilityDB() { Speed = Speed });
+        }
+    }
+
+    public void OnComponentUninstallation(Entity parentEntity, ComponentInstance componentInstance)
+    {
+        if(parentEntity.TryGetDatablob<GeoSurveyAbilityDB>(out var geoSurveyAbilityDB))
+        {
+            geoSurveyAbilityDB.Speed -= Speed;
+            if(geoSurveyAbilityDB.Speed <= 0)
+            {
+                parentEntity.RemoveDataBlob<GeoSurveyAbilityDB>();
+            }
+        }
+    }
+
+    public string AtbName()
+    {
+        return "Geological Surveyor";
+    }
+
+    public string AtbDescription()
+    {
+        return $"Speed {Speed}";
+    }
+}
