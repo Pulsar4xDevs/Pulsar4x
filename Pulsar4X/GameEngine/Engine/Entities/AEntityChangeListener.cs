6de709257c5398c85da209470c56f58ff10a444b
Justin Brown <behindcurtain3@gmail.com>
2024-04-16 22:11:37 -0700

    Add default enqueue for EntityChangeListener

--- Pulsar4X/GameEngine/Engine/Entities/AEntityChangeListener.cs
+++ Pulsar4X/GameEngine/Engine/Entities/AEntityChangeListener.cs
@@ -174,6 +174,7 @@ namespace Pulsar4X.Engine
                     OnDBRemoved(changeData);
                     break;
                 default:
+                    EntityChanges.Enqueue(changeData);
                     break;
             }
         }
