ca04db53abb93625b29ca40dace2968c13cab247
Justin Brown <behindcurtain3@gmail.com>
2025-03-14 13:32:48 -0700

    Clean up Program.cs

--- Pulsar4X/Pulsar4X.Client/Program.cs
+++ Pulsar4X/Pulsar4X.Client/Program.cs
@@ -1,33 +1,7 @@
-﻿using System;
-using System.Threading;
+﻿using Pulsar4X.Client;
 
-namespace Pulsar4X.Client
+// Run the game
+using (var pulsar = new PulsarMainWindow())
 {
-    public class Program
-    {
-        static PulsarMainWindow? Instance;
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            // Webhook URL to the #crash-reports channel
-            //var crashLogger = new DiscordCrashLogger("https://discord.com/api/webhooks/1313608706172125305/pE4jhTyUviwomqfmZcJB-QWayFBwgTVR_o_6SSO_q91c1TI0QKTKNmuBgJl1o0Q7S7Vy");
-
-            try
-            {
-                SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
-                Instance = new PulsarMainWindow();
-                Instance.Run();
-                Instance.Dispose();
-            }
-            catch (Exception e)
-            {
-                // Log the crash
-                //await crashLogger.LogCrashAsync(e, $"Git Hash: {AssemblyInfo.GetGitHash()}");
-
-                // Throw again to allow the local debugger to handle the exception
-                //throw;
-            }
-
-        }
-    }
+    pulsar.Run();
 }
