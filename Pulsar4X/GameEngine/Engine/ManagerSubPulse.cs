ed8541150c1e13d3ec895ebb60d1410d3ac41299
Jouni <jouni.juhani@protonmail.com>
2025-12-30 19:05:52 +0200

    Don't allow nullable Entity

--- Pulsar4X/GameEngine/Engine/ManagerSubPulse.cs
+++ Pulsar4X/GameEngine/Engine/ManagerSubPulse.cs
@@ -123,9 +123,8 @@ namespace Pulsar4X.Engine
         /// <param name="nextDateTime"></param>
         /// <param name="action"></param>
         /// <param name="entity"></param>
-        internal void AddEntityInterupt(DateTime nextDateTime, string actionProcessor, Entity? entity)
+        internal void AddEntityInterupt(DateTime nextDateTime, string actionProcessor, Entity entity)
         {
-            if(entity == null) throw new ArgumentNullException("Entity cannot be null");
             if(nextDateTime < StarSysDateTime) throw new Exception("Trying to add an interrupt in the past");
             if (nextDateTime < _subStepDateTime)
                 _subStepDateTime = nextDateTime;
