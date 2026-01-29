193ba07cd70840750cbe356c51f1b3a26ae805b2
Justin Brown <behindcurtain3@gmail.com>
2026-01-28 22:30:30 -0800

    Fix race condition on PerformanceStopwatch

--- Pulsar4X/GameEngine/Engine/PerformanceStopwatch.cs
+++ Pulsar4X/GameEngine/Engine/PerformanceStopwatch.cs
@@ -14,6 +14,7 @@ public class PerformanceStopwatch
     string _currentId;
     SafeList<PerformanceData> _history = new ();
     PerformanceData _currentData = new PerformanceData();
+    private readonly object _lock = new object();
 
     public struct PerformanceData
     {
@@ -35,19 +36,22 @@ public class PerformanceStopwatch
     public void Stop(string id)
     {
         _stopwatch.Stop();
+        var elapsed = _stopwatch.Elapsed.TotalMilliseconds;
 
-        if(!_currentData.TimesById.ContainsKey(id))
+        lock (_lock)
         {
-            var newList = new List<double> { _stopwatch.Elapsed.TotalMilliseconds };
-            var sum = _stopwatch.Elapsed.TotalMilliseconds;
-            _currentData.TimesById.Add(id, (newList, sum));
-        }
-        else
-        {
-            _currentData.TimesById[id].times.Add(_stopwatch.Elapsed.TotalMilliseconds);
-            double sum = _currentData.TimesById[id].sum + _stopwatch.Elapsed.TotalMilliseconds;
-            (List<double> times, double sum) entry = (_currentData.TimesById[id].times, sum);
-            _currentData.TimesById[id] = entry;
+            if(!_currentData.TimesById.ContainsKey(id))
+            {
+                var newList = new List<double> { elapsed };
+                _currentData.TimesById.Add(id, (newList, elapsed));
+            }
+            else
+            {
+                _currentData.TimesById[id].times.Add(elapsed);
+                double sum = _currentData.TimesById[id].sum + elapsed;
+                (List<double> times, double sum) entry = (_currentData.TimesById[id].times, sum);
+                _currentData.TimesById[id] = entry;
+            }
         }
     }
 
