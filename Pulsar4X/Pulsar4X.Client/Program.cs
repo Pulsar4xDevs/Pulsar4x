b55bdc44c1b76cef424b429f611cecbb225533a9
Jouni <jouni.juhani@protonmail.com>
2026-02-06 12:59:58 +0200

    Move Trace.Listeners.Add before PulsarMainWindow

--- Pulsar4X/Pulsar4X.Client/Program.cs
+++ Pulsar4X/Pulsar4X.Client/Program.cs
@@ -2,16 +2,16 @@
 using System.Diagnostics;
 using Pulsar4X.Client;
 
+#if TRACE
+Trace.Listeners.Add(new ConsoleTraceListener());
+#endif
+// dotnet core doesn't have Debug.Listeners for some reason...
+// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.debug?view=net-10.0
+// https://github.com/dotnet/dotnet-api-docs/issues/4866
+
 // Run the game
 using (var pulsar = new PulsarMainWindow(args))
 {
-#if TRACE
-    Trace.Listeners.Add(new ConsoleTraceListener());
-#endif
-    // dotnet core doesn't have Debug.Listeners for some reason...
-    // https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.debug?view=net-10.0
-    // https://github.com/dotnet/dotnet-api-docs/issues/4866
-
     pulsar.Run();
     /*
     try
